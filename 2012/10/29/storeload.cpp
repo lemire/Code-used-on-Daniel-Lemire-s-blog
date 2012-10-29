b74420fe9f1fdaa151f2292ed350bf43de53c288
Daniel Lemire <lemire@gmail.com>
2012-10-29 13:15:02 -0400

    More test...

--- 2012/10/29/storeload.cpp
+++ 2012/10/29/storeload.cpp
@@ -120,7 +120,7 @@ int testStoreLoadC(size_t M =  2048 * 4, size_t N = 2048 * 8, size_t repeat = 1)
 	for(size_t i = 0; i<M; ++i)
 	  data.push_back(i);
 	vector<int> bigdata;
-	bigdata.reserve(M * N);
+	bigdata.resize(M * N);
 	for(size_t t = 0; t< 3; ++t) {
 		timer.reset();
 		for (size_t r = 0; r < repeat; ++r)
@@ -130,14 +130,68 @@ int testStoreLoadC(size_t M =  2048 * 4, size_t N = 2048 * 8, size_t repeat = 1)
 		for (size_t r = 0; r < repeat; ++r)
 	    	bogus += loadTestC(&data[0],&bigdata[0],N,M);
 		if(t>0) cout<<" load time = "<<timer.split()<<endl;
-		bigdata.clear();
 		for(int i = 0; i<M; ++i)
 	  		assert(data[i] == i);
 	}
     return bogus;
 }
 
+
+__attribute__ ((noinline)) 
+void pack(bool * uncompressed, char * compressed, size_t N) {
+	int bogus = 0;
+	for(size_t x = 0; x+7<N; x+=8) {
+		compressed[x] = uncompressed[x] | 
+		uncompressed[x+1]<<1 |
+		uncompressed[x+2]<<2 |
+		uncompressed[x+3]<<3 |
+		uncompressed[x+4]<<4 |
+		uncompressed[x+5]<<5 |
+		uncompressed[x+6]<<6 |
+		uncompressed[x+7]<<7 ;
+	}
+}
+
+__attribute__ ((noinline)) 
+void unpack(char * compressed, bool * uncompressed, size_t N) {
+	int bogus = 0;
+	for(size_t x = 0; x+7<N; x+=8) {
+		uncompressed[x] = compressed[x] & 1;
+		uncompressed[x+1] = compressed[x] & (1<<1);
+		uncompressed[x+2] = compressed[x] & (1<<2);
+		uncompressed[x+3] = compressed[x] & (1<<3);
+		uncompressed[x+4] = compressed[x] & (1<<4);
+		uncompressed[x+5] = compressed[x] & (1<<5);
+		uncompressed[x+6] = compressed[x] & (1<<6);
+		uncompressed[x+7] = compressed[x] & (1<<7);
+	}
+}
+
+
+void testPackUnpackC(size_t N =  2048 * 32 * 2048) {
+	WallClockTimer timer;
+	bool* data = new bool[N];
+	for(size_t i = 0; i<N; ++i)
+	  data[i] = static_cast<bool>(i & 1);
+	vector<char> comp(N/8);
+	for(size_t t = 0; t< 3; ++t) {
+		timer.reset();
+		pack(data,&comp[0],N/8);
+		cout<<" pack time = "<<timer.split()<<endl;
+		timer.reset();
+		unpack(&comp[0],data,N/8);
+		cout<<" unpack time = "<<timer.split()<<endl;
+		for(size_t i = 0; i<N; ++i) 
+			assert(data[i] == static_cast<bool>(i & 1));	  
+	}
+	delete[] data;
+}
+
+
 int main() {
+	cout<<"pack-unpack test:"<<endl;
+	testPackUnpackC();
+	
 	cout<<"cache-cache test:"<<endl;
 
 	cout<<"ignore:"<<testStoreLoad(2048,32,2048)<<endl;
