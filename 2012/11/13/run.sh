b9ad5414cb1e38e763615c37c802e20c5786e601
Daniel Lemire <lemire@lemire.me>
2012-11-13 12:50:15 -0500

    Updated results for desktop

--- 2012/11/13/run.sh
+++ 2012/11/13/run.sh
@@ -1,2 +1,2 @@
 javac -sourcepath src -cp ./src:lib/SparseBitmap-0.0.3.jar:lib/trove-3.0.3.jar ./src/Benchmark.java
-java -cp ./src/:lib/SparseBitmap-0.0.3.jar:lib/trove-3.0.3.jar Benchmark
+java -server -cp ./src/:lib/SparseBitmap-0.0.3.jar:lib/trove-3.0.3.jar Benchmark
