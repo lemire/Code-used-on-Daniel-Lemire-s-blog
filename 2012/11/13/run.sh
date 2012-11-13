17ee396d1ff5a6c09ecb11b7bdb413717fb26243
Daniel Lemire <lemire@gmail.com>
2012-11-13 12:35:40 -0500

    Preparing new blog post

---
+++ 2012/11/13/run.sh
@@ -0,0 +1,2 @@
+javac -sourcepath src -cp ./src:lib/SparseBitmap-0.0.3.jar:lib/trove-3.0.3.jar ./src/Benchmark.java
+java -cp ./src/:lib/SparseBitmap-0.0.3.jar:lib/trove-3.0.3.jar Benchmark
--- extra/fastsum.cpp
+++ extra/fastsum.cpp
@@ -1,8 +1,40 @@
-// fun test. Requires AVX2 but I lack a compatible processor
 
-#include <cassert>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <immintrin.h>
+#include <cassert>
 #include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+class WallClockTimer {
+public:
+    struct timeval t1, t2;
+    WallClockTimer() :
+        t1(), t2() {
+        gettimeofday(&t1, 0);
+        t2 = t1;
+    }
+    void reset() {
+        gettimeofday(&t1, 0);
+        t2 = t1;
+    }
+    int elapsed() {
+        return ((t2.tv_sec - t1.tv_sec) * 1000) + ((t2.tv_usec - t1. tv_usec)
+                / 1000);
+    }
+    int split() {
+        gettimeofday(&t2, 0);
+        return elapsed();
+    }
+};
+
+
+
 
 int sum(int *  data, size_t length) {
 	int answer = 0;
@@ -11,8 +43,52 @@ int sum(int *  data, size_t length) {
 	}
 	return answer;
 }
+#ifdef __SSE
 
+template <class T>
+string stringSIMD(__m128i x, bool hexflag=false) {
+    typedef union {
+        __m128i v;
+        T a[16/sizeof(T)];
+   } U;
+   U y = {x};
+   ostringstream convert;
+   if(hexflag) convert<<hex;
+   convert<<"{";
+   for(size_t i = 0; i < 16/sizeof(T);++i) {
+       if(sizeof(T)==1)
+           convert<<(hexflag?"0x":"")<<static_cast<int>(y.a[i]);
+       else
+           convert<<(hexflag?"0x":"")<<y.a[i];
+       if(i+1!=16/sizeof(T)) convert<<",";
+   }
+   convert<<"}";
+   return convert.str();
+}
 
+static  const __m128i shufflemask =  _mm_set_epi8 (128,128,128,128,128,128,128,128,128,128,128,128,15,14,13,12);
+
+int SSEsum(int * data, size_t length) {
+	assert(sizeof(int) == 4);// hidden assumption
+	assert(length %  4 == 0);// for simplicity
+	int answer = 0;
+	__m128i* p = reinterpret_cast<__m128i*>(data);
+	__m128i  u = _mm_setzero_si128 ();
+	for(size_t i = 0; i < length / 4 ; ++i) {
+		u = _mm_add_epi32(_mm_load_si128(p++),u);
+		u = _mm_add_epi32( u , _mm_slli_si128(u,4));
+		u = _mm_add_epi32( u , _mm_slli_si128(u,8));
+		//cout<<"before "<<stringSIMD<int>(u)<<endl;
+		
+		u = _mm_shuffle_epi8(u,shufflemask);
+		//cout<<"after "<<stringSIMD<int>(u)<<endl;
+	}
+	return _mm_extract_epi32 (u,0);
+}
+#endif
+
+/*
+// fun test. Requires AVX2 but I lack a compatible processor
 int AVXsum(int * data, size_t length) {
 	assert(sizeof(int) == 4);// hidden assumption
 	assert(length %  8 == 0);// for simplicity
@@ -30,12 +106,24 @@ int AVXsum(int * data, size_t length) {
 	int tmp[8];
 	_mm256_storeu_si256(reinterpret_cast<__m256i*>(tmp),u);
 	return tmp[7];
-}
+}*/
 
 int main() {
-	int x[8];
-	for( size_t i = 0; i < 8; ++i)
+	const size_t N = 1024*1024*32;
+	vector<int> x(N);
+	for( size_t i = 0; i < N; ++i)
 	  x[i] = i;
-	std::cout<<sum(x,8)<<std::endl;
-	std::cout<<AVXsum(x,8)<<std::endl;
+	WallClockTimer t;
+	t.reset();
+	int a1 = sum(&x[0],N);
+	cout<<"time = "<<t.split()<<endl;
+	cout<<"a1 = "<<a1<<endl;
+	t.reset();
+#ifdef __SSE
+	int a2 = SSEsum(&x[0],N);
+	cout<<"time = "<<t.split()<<endl;
+	cout<<"a2 = "<<a2<<endl;
+	if(a1!=a2) cerr<<"bug"<<endl;
+#endif
+
 }
