c3f492f510579bff2f60e25544c578b0c7adc726
Daniel Lemire <lemire@lemire.me>
2013-01-31 16:46:14 -0500

    Minor edit

--- 2013/01/31/dynarray.cpp
+++ 2013/01/31/dynarray.cpp
@@ -1,9 +1,10 @@
 
-
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <iostream>
+#include <climits>
 #include <limits>
 #include <vector>
 
@@ -51,6 +52,7 @@ int testManual(size_t N, const int multiplier, const int div = 1, const int cons
 			delete[] x;
 			x = nx;
 			s = multiplier * s / div + constant;
+                        if( s <= i) cerr << "bug" << endl;
 		}
 		x[i] = i;
 	}
@@ -68,9 +70,13 @@ int overall(size_t N) {
     cout << "STL vector " << N /(delay * 1000.0) << endl;
     
     for(size_t factor = 1; factor <= 6; ++ factor) {
-		t.reset();
-    	bogus += testManual(N,2+factor,2,factor&1==0 ? 0 : 1);
-    	delay = t.split();
+        delay = INT_MAX;
+        for(size_t T = 0 ; T < 10 ; ++T ) {
+          t.reset();
+    	  bogus += testManual(N,2+factor,2,factor&1==0 ? 0 : 1);
+    	  int tdelay = t.split();
+          if(tdelay < delay) delay = tdelay;
+        }
     	cout << "pointer-based "<< (factor +2)/2.0<< " : " << N /(delay * 1000.0) << endl;
     }
     return bogus;
