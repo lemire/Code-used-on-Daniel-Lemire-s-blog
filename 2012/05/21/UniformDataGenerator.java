613739bfb9f1f62b3522c4431065e1428e2e042a
Daniel Lemire <lemire@gmail.com>
2013-04-23 08:32:20 -0400

    Fixing type in license

--- 2012/05/21/UniformDataGenerator.java
+++ 2012/05/21/UniformDataGenerator.java
@@ -1,7 +1,7 @@
 /**
  * (c) Daniel Lemire, http://lemire.me
  * 
- * This is code is released under the
+ * This code is released under the
  * Apache License Version 2.0 http://www.apache.org/licenses/.
  */
 
