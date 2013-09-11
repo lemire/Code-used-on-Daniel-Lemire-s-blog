2e8b8e92c60994ca39276a376f7c04dc71f18637
Daniel Lemire <lemire@gmail.com>
2013-09-11 12:13:59 -0400

    In java now

---
+++ extra/bytevsintcounters.java
@@ -0,0 +1,26 @@
+public class bytevsintcounters {
+	
+	public static void main(String[] args) {
+		int S = 1024;
+		
+		byte[] counter8 = new byte[S];
+		int[] counter32 = new int[S];
+		
+		
+		long bef,aft;
+		bef = System.currentTimeMillis();
+		for(int init = 0; init < 1024; ++init) 
+		for(int i = init; i < 3*S; ++i)
+		  counter8[i%S] += 1;
+		aft = System.currentTimeMillis();
+		long t1 = aft-bef;
+		bef = System.currentTimeMillis();
+		for(int init = 0; init < 1024; ++init) 
+		for(int i = init; i < 3*S; ++i)
+		  counter32[i%S] += 1;
+		aft = System.currentTimeMillis();
+		long t2 = aft-bef;
+		
+		System.out.println(t1+" "+t2);
+	}
+}
