8989309d910fb50ca4e569fc8e3d07e2fb162278
Daniel Lemire <dlemire@fastpfor.(none)>
2013-09-11 12:17:36 -0400

    With longs now

--- extra/bytevsintcounters.java
+++ extra/bytevsintcounters.java
@@ -5,7 +5,8 @@ public class bytevsintcounters {
 		
 		byte[] counter8 = new byte[S];
 		int[] counter32 = new int[S];
-		
+		long[] counter64 = new long[S];
+			
 		
 		long bef,aft;
 		bef = System.currentTimeMillis();
@@ -20,7 +21,13 @@ public class bytevsintcounters {
 		  counter32[i%S] += 1;
 		aft = System.currentTimeMillis();
 		long t2 = aft-bef;
-		
-		System.out.println(t1+" "+t2);
+		bef = System.currentTimeMillis();
+		for(int init = 0; init < 1024; ++init) 
+		for(int i = init; i < 3*S; ++i)
+		  counter64[i%S] += 1;
+		aft = System.currentTimeMillis();
+		long t3  = aft-bef;
+			
+		System.out.println(t1+" "+t2+" "+t3);
 	}
 }
