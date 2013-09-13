c00eb2001a1348ffb450a0ad5195184b36bc9ebc
Daniel Lemire <lemire@gmail.com>
2013-09-13 19:30:36 -0400

    Saving.

--- extra/bytecounting/bytevsintcounters.cpp
+++ extra/bytecounting/bytevsintcounters.cpp
@@ -1,15 +1,15 @@
+// g++-fsf-4.7 -O3 -o bytevsintcounters  bytevsintcounters.cpp
+
 #include <sys/types.h>
 #include <stdint.h>
 #include <sys/time.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-
 #include <iomanip>
-
-
 #include <iostream>
 #include <vector>
 
+using namespace std;
 
 
 class WallClockTimer {
@@ -34,35 +34,54 @@ public:
 };
 
 
+template <class uword>
+int counter(uint32_t S) {
+     vector<uword> counter(S);	    
+     uint32_t gap = 3 * S/(64*1024);
+     for(uint32_t init = 1; init < 1024; ++init) 
+        for (uint32_t i=init; i<S*3; i+=gap) {
+        	counter[i%S] += 1;// ignore overflows
+        }
+     int c = 0;
+     for(uint32_t i = 0; i < S; ++i)
+        if(counter[i]>2) ++c;
+     return c;
+
+}
+
+
+// ideally S should be a power of two
+void test(uint32_t S) {
+	    cout<<" Number of counters = "<<S<<endl;
+
+	    int bogus = 0;
+        WallClockTimer t;
+        bogus += counter<uint8_t>(S);
+        int dt1 = t.split();
+        t.reset();
+        bogus += counter<uint16_t>(S);
+        int dt2 = t.split();
+        t.reset();
+        bogus += counter<uint32_t>(S);
+        int dt3 = t.split();
+        t.reset();
+        bogus += counter<uint64_t>(S);
+        int dt4 = t.split();
+        uint32_t gap = 3 * S/(64*1024);
+        
+        double number = (1024.0 * S)/gap;
+         cout<<std::setprecision(4)<<number/dt1 <<"\t\t"<<number/dt2<<"\t\t"<<number/dt3<<"\t\t"<<number/dt3<<endl;
+        cout<<"ignore: "<<bogus<<endl;
+}
+
 
 
 using namespace std;
 int main() {
-	   size_t S = 1024 * 64;
-	   vector<uint8_t> counter8(S);
-	   vector<uint32_t> counter32(S);
-	   vector<uint64_t> counter64(S);
-		
-        WallClockTimer t;
-        for(size_t init = 1; init < 1024; ++init) 
-        for (size_t i=init; i<S*3; i+=3) {
-        	counter8[i%S] += 1;
-        }
-        int dt1 = t.split();
-        t.reset();
-        for(size_t init = 1; init < 1024; ++init) 
-        for (size_t i=init; i<S*3; i+=3) {
-        	counter32[i%S] += 1;
-        }
-        int dt2 = t.split();
-        t.reset();
-        for(size_t init = 1; init < 1024; ++init) 
-        for (size_t i=init; i<S*3; i+=3) {
-        	counter64[i%S] += 1;
-        }
-        int dt3 = t.split();
-         cout<<std::setprecision(4)<<dt1 <<"\t\t"<<dt2<<"\t\t"<<dt3<<endl;
-        cout<<"ignore: "<<counter8[12]+counter32[32]+counter64[444]<<endl;
+   cout<<" speeds for 8-bit, 16-bit, 32-bit and 64-bit counters"<<endl;
+   test(1024*64);	
+   test(1024*1024*8);	
+   test(1024*1024*32);	
+   test(1024*1024*64);	
 
-	
 }
--- extra/bytecounting/bytevsintcounters.java
+++ extra/bytecounting/bytevsintcounters.java
@@ -1,33 +1,98 @@
+import java.text.*;
 public class bytevsintcounters {
 	
-	public static void main(String[] args) {
-		int S = 1024;
-		
-		byte[] counter8 = new byte[S];
-		int[] counter32 = new int[S];
-		long[] counter64 = new long[S];
-			
-		
+	
+  static int counter8(int S) {
+     byte[]  counter = new byte[S];	    
+     int gap = 3 * S/(64*1024);
+     for(int init = 1; init < 1024; ++init) 
+        for (int i=init; i<S*3; i+=gap) {
+        	counter[i%S] += 1;// ignore overflows
+        }
+     int c = 0;
+     for(int i = 0; i < S; ++i)
+        if(counter[i]>2) ++c;
+     return c;
+
+   }
+   
+  static int counter16(int S) {
+     short[]  counter = new short[S];	    
+     int gap = 3 * S/(64*1024);
+     for(int init = 1; init < 1024; ++init) 
+        for (int i=init; i<S*3; i+=gap) {
+        	counter[i%S] += 1;// ignore overflows
+        }
+     int c = 0;
+     for(int i = 0; i < S; ++i)
+        if(counter[i]>2) ++c;
+     return c;
+
+   }
+   
+  static int counter32(int S) {
+     int[]  counter = new int[S];	    
+     int gap = 3 * S/(64*1024);
+     for(int init = 1; init < 1024; ++init) 
+        for (int i=init; i<S*3; i+=gap) {
+        	counter[i%S] += 1;// ignore overflows
+        }
+     int c = 0;
+     for(int i = 0; i < S; ++i)
+        if(counter[i]>2) ++c;
+     return c;
+
+   }
+   
+  static int counter64(int S) {
+     long[]  counter = new long[S];	    
+     int gap = 3 * S/(64*1024);
+     for(int init = 1; init < 1024; ++init) 
+        for (int i=init; i<S*3; i+=gap) {
+        	counter[i%S] += 1;// ignore overflows
+        }
+     int c = 0;
+     for(int i = 0; i < S; ++i)
+        if(counter[i]>2) ++c;
+     return c;
+
+   }
+   public static void test(int S) {
+   	    System.out.println("Number of counters = "+S);
+        int bogus = 0;
 		long bef,aft;
 		bef = System.currentTimeMillis();
-		for(int init = 0; init < 1024; ++init) 
-		for(int i = init; i < 3*S; ++i)
-		  counter8[i%S] += 1;
+		bogus += counter8(S);
 		aft = System.currentTimeMillis();
 		long t1 = aft-bef;
 		bef = System.currentTimeMillis();
-		for(int init = 0; init < 1024; ++init) 
-		for(int i = init; i < 3*S; ++i)
-		  counter32[i%S] += 1;
+		bogus += counter16(S);
+		aft = System.currentTimeMillis();
 		aft = System.currentTimeMillis();
 		long t2 = aft-bef;
 		bef = System.currentTimeMillis();
-		for(int init = 0; init < 1024; ++init) 
-		for(int i = init; i < 3*S; ++i)
-		  counter64[i%S] += 1;
+		bogus += counter32(S);
 		aft = System.currentTimeMillis();
 		long t3  = aft-bef;
+		bef = System.currentTimeMillis();
+		bogus += counter32(S);
+		aft = System.currentTimeMillis();
+		long t4  = aft-bef;
 			
-		System.out.println(t1+" "+t2+" "+t3);
+        int gap = 3 * S/(64*1024);
+		
+        double number = (1024.0 * S)/gap;
+		DecimalFormat df = new DecimalFormat( "#.#" );
+
+		System.out.println(df.format(number*0.001/t1)+" "+df.format(number*0.001/t2)+" "+df.format(number*0.001/t3)+" "+df.format(number*0.001/t4));
+		System.out.println("ignore :"+bogus);
+   }
+	
+	public static void main(String[] args) {
+	   System.out.println(" speeds for 8-bit, 16-bit, 32-bit and 64-bit counters");
+	   test(1024*64);	
+	   test(1024*1024*8);	
+	   test(1024*1024*32);	
+	   test(1024*1024*64);	
 	}
 }
