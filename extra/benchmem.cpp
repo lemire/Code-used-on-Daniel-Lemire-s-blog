53d84adb95c208943233ce10b171ec1bd7e7d980
Daniel Lemire <lemire@gmail.com>
2013-09-27 12:19:26 -0400

    Testing whether memcpy is really fast

--- extra/benchmem.cpp
+++ extra/benchmem.cpp
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <iostream>
 #include <limits>
-
+#include <immintrin.h>
 using namespace std;
 
 class WallClockTimer {
@@ -34,16 +34,30 @@ public:
     }
 };
 
+void fastcopy(const int * in , int * out, size_t N) {// assumes N is a multiple of 4
+	const __m128i * min = reinterpret_cast<const __m128i *>(in); 
+	__m128i * mout = reinterpret_cast<__m128i *>(out);
+	
+	for(size_t k = 0; k < N/4; ++k) {
+		      _mm_store_si128(mout++,  _mm_load_si128(min++));
+	}
+}
+
 int main() {
-	const size_t N = 5 * 1000 * 1000;
+	const size_t N = 4 * 1000 * 1000;
 	int *  a = new int[N];
 	int *  b = new int[N];
 
 	WallClockTimer t;
 	int total = 0;
+	double besttime0 = numeric_limits<double>::max();
 	double besttime1 = numeric_limits<double>::max();
 	double besttime2 = numeric_limits<double>::max();
 	for(int k = 0; k<10;++k) {
+		t.reset();
+		fastcopy(b,a,N);
+		double thistime0 = t.split()/1000.0;
+		if(thistime0 < besttime0) besttime0 = thistime0;
 		t.reset();
 		memset(a,k,N*sizeof(int));
 		double thistime1 = t.split()/1000.0;
@@ -54,6 +68,8 @@ int main() {
 		if(thistime2 < besttime2) besttime2 = thistime2;
 		total += b[N-2];
 	}
+        cout<<" SIMD memcpy speed = "<<N/(1000.0*1000.0*besttime0) <<" mis or "<< N*4/(1024.0*1024.0*besttime0)<<" MB/s"<<endl;
+
         cout<<" memset speed = "<<N/(1000.0*1000.0*besttime1) <<" mis or "<< N*4/(1024.0*1024.0*besttime1)<<" MB/s"<<endl;
         cout<<" memcpy speed = "<<N/(1000.0*1000.0*besttime2) <<" mis or "<< N*4/(1024.0*1024.0*besttime2)<<" MB/s"<<endl;
         return total;
