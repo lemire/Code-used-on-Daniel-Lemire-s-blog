71c0a9f3c062db33daf9ca3b8cfb968768752d3f
Daniel Lemire <lemire@gmail.com>
2013-10-11 13:59:01 -0400

    Faster copy

--- extra/benchmem.cpp
+++ extra/benchmem.cpp
@@ -1,7 +1,7 @@
 /**
 * reports transfert rate.
 *
-*  g++ -O0 -g3  -o benchmem benchmem.cpp
+*  g++-fsf-4.7 -Ofast -mavx -g3  -o benchmem benchmem.cpp
 */
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -35,49 +35,71 @@ public:
 };
 
 void fastcopy(const int * in , int * out, size_t N) {// assumes N is a multiple of 4
-	const __m128i * min = reinterpret_cast<const __m128i *>(in); 
-	__m128i * mout = reinterpret_cast<__m128i *>(out);
-	
-	for(size_t k = 0; k < N/4; ++k) {
-		      _mm_store_si128(mout++,  _mm_load_si128(min++));
-	}
+    const __m128i * min = reinterpret_cast<const __m128i *>(in); 
+    __m128i * mout = reinterpret_cast<__m128i *>(out);
+    
+    for(size_t k = 0; k < N/4; ++k) {
+              _mm_store_si128(mout++,  _mm_load_si128(min++));
+    }
+}
+
+void ufastcopy(const int * in , int * out, size_t N) {// assumes N is a multiple of 4
+  const __m128i * min = reinterpret_cast<const __m128i *>(in);
+  const __m128i * minMax = min + N/4;
+  const __m128i * minMax4 = min + 4*(N/16);
+  __m128i * mout = reinterpret_cast<__m128i *>(out);
+
+  while (min < minMax4) {
+          _mm_store_si128(mout++,  _mm_load_si128(min++));
+          _mm_store_si128(mout++,  _mm_load_si128(min++));
+          _mm_store_si128(mout++,  _mm_load_si128(min++));
+          _mm_store_si128(mout++,  _mm_load_si128(min++));
+  }
+  while (min < minMax) {
+          _mm_store_si128(mout++,  _mm_load_si128(min++));
+  }
 }
 
 int run(const size_t N) {
-	cout<<"N = "<<N<<endl;
-	int *  a = new int[N];
-	int *  b = new int[N];
+    cout<<"N = "<<N<<endl;
+    int *  a = new int[N];
+    int *  b = new int[N];
 
-	WallClockTimer t;
-	int total = 0;
-	double besttime0 = numeric_limits<double>::max();
-	double besttime1 = numeric_limits<double>::max();
-	double besttime2 = numeric_limits<double>::max();
-	for(int k = 0; k<10;++k) {
-		t.reset();
-		fastcopy(b,a,N);
-		double thistime0 = t.split()/1000.0;
-		if(thistime0 < besttime0) besttime0 = thistime0;
-		t.reset();
-		memset(a,k,N*sizeof(int));
-		double thistime1 = t.split()/1000.0;
-		if(thistime1 < besttime1) besttime1 = thistime1;
-		t.reset();
-		memcpy(b,a,N*sizeof(int));
-		double thistime2 = t.split()/1000.0;
-		if(thistime2 < besttime2) besttime2 = thistime2;
-		total += b[N-2];
-	}
-        cout<<" SIMD memcpy speed = "<<N/(1000.0*1000.0*besttime0) <<" mis or "<< N*4/(1024.0*1024.0*besttime0)<<" MB/s"<<endl;
-
-        cout<<" memset speed = "<<N/(1000.0*1000.0*besttime1) <<" mis or "<< N*4/(1024.0*1024.0*besttime1)<<" MB/s"<<endl;
-        cout<<" memcpy speed = "<<N/(1000.0*1000.0*besttime2) <<" mis or "<< N*4/(1024.0*1024.0*besttime2)<<" MB/s"<<endl;
-        return total;
-
+    WallClockTimer t;
+    int total = 0;
+    double besttime0 = numeric_limits<double>::max();
+    double besttime1 = numeric_limits<double>::max();
+    double besttime2 = numeric_limits<double>::max();
+    double besttime3 = numeric_limits<double>::max();
+    
+    for(int k = 0; k<10;++k) {
+        t.reset();
+        fastcopy(b,a,N);
+        double thistime0 = t.split()/1000.0;
+        if(thistime0 < besttime0) besttime0 = thistime0;
+        t.reset();
+        memset(a,k,N*sizeof(int));
+        double thistime1 = t.split()/1000.0;
+        if(thistime1 < besttime1) besttime1 = thistime1;
+        t.reset();
+        memcpy(b,a,N*sizeof(int));
+        double thistime2 = t.split()/1000.0;
+        if(thistime2 < besttime2) besttime2 = thistime2;
+        total += b[N-2];
+        t.reset();
+        ufastcopy(b,a,N);
+        double thistime3 = t.split()/1000.0;
+        if(thistime3 < besttime3) besttime3 = thistime3;
+    }
+    cout<<" Fast SIMD memcpy speed = "<<N/(1000.0*1000.0*besttime3) <<" mis or "<< N*4/(1024.0*1024.0*besttime3)<<" MB/s"<<endl;
+    cout<<" SIMD memcpy speed = "<<N/(1000.0*1000.0*besttime0) <<" mis or "<< N*4/(1024.0*1024.0*besttime0)<<" MB/s"<<endl;
+    cout<<" memset speed = "<<N/(1000.0*1000.0*besttime1) <<" mis or "<< N*4/(1024.0*1024.0*besttime1)<<" MB/s"<<endl;
+    cout<<" memcpy speed = "<<N/(1000.0*1000.0*besttime2) <<" mis or "<< N*4/(1024.0*1024.0*besttime2)<<" MB/s"<<endl;
+    return total;
 }
 
 int main() {
-	run(4000000);
-	run(20000000);
-	return 0;
+    run(4000000);
+    run(20000000);
+    return 0;
 }
