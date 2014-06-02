2acf40d6f86257dd76015cc7b04e6c11b96cf192
Daniel Lemire <lemire@gmail.com>
2014-06-02 10:46:37 -0400

    Minor edit

--- extra/sum64.cpp
+++ extra/sum64.cpp
@@ -7,7 +7,7 @@ g++ -O3 -o sum64 sum64.cpp && ./sum64
 #include <sys/time.h>
 #include <sys/types.h>
 #include <iostream>
-
+#include <stdint.h>
 using namespace std;
 
 
@@ -29,7 +29,7 @@ void asm_mult64to128thenaddto192(uint64_t u, uint64_t v,  uint64_t &rhh, uint64_
              "addq %%rax,  %[rl]\n"
               "adcq %%rdx,  %[rh]\n"
              "adcq $0,  %[rhh]\n"
-             :  [rh] "+r" (rh), [rhh] "+r" (rhh) , [rl] "+r" (rl)  : [u] "a" (u), [v] "rm" (v)  :"rdx","cc" );
+             :  [rh] "+r" (rh), [rhh] "+r" (rhh) , [rl] "+r" (rl)  : [u] "a" (u), [v] "r" (v)  :"rdx","cc" );
 
 }
 
@@ -67,14 +67,64 @@ uint192 asmscalarproduct(size_t length, const uint64_t * a, const uint64_t * x) 
              "addq %%rax,  %[rl]\n"
         "adcq %%rdx,  %[rh]\n"
              "adcq $0,  %[rhh]\n"
-             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i]), [v] "rm" (x[i])  :"rdx","cc");
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i]), [v] "r" (x[i])  :"rdx","cc");
 	}
 	return s;
 }
 
 
-void check() {
+uint192 asm2scalarproduct(size_t length, const uint64_t * a, const uint64_t * x) {
+	uint192 s;
+	s.low = 0;
+	s.high = 0;
+	s.vhigh = 0;
+	for(size_t i = 0; i+1<length; i+=2) {
+    __asm__ ("mulq %[v]\n"
+             "addq %%rax,  %[rl]\n"
+        "adcq %%rdx,  %[rh]\n"
+             "adcq $0,  %[rhh]\n"
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i]), [v] "r" (x[i])  :"rdx","cc");
+    __asm__ ("mulq %[v]\n"
+             "addq %%rax,  %[rl]\n"
+        "adcq %%rdx,  %[rh]\n"
+             "adcq $0,  %[rhh]\n"
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i+1]), [v] "r" (x[i+1])  :"rdx","cc");
+		}
+	return s;
+}
+uint192 asm4scalarproduct(size_t length, const uint64_t * a, const uint64_t * x) {
+	uint192 s;
+	s.low = 0;
+	s.high = 0;
+	s.vhigh = 0;
+	for(size_t i = 0; i+3<length; i+=4) {
+    __asm__ ("mulq %[v]\n"
+             "addq %%rax,  %[rl]\n"
+        "adcq %%rdx,  %[rh]\n"
+             "adcq $0,  %[rhh]\n"
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i]), [v] "r" (x[i])  :"rdx","cc");
+    __asm__ ("mulq %[v]\n"
+             "addq %%rax,  %[rl]\n"
+        "adcq %%rdx,  %[rh]\n"
+             "adcq $0,  %[rhh]\n"
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i+1]), [v] "r" (x[i+1])  :"rdx","cc");
+    __asm__ ("mulq %[v]\n"
+             "addq %%rax,  %[rl]\n"
+        "adcq %%rdx,  %[rh]\n"
+             "adcq $0,  %[rhh]\n"
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i+2]), [v] "r" (x[i+2])  :"rdx","cc");
+    __asm__ ("mulq %[v]\n"
+             "addq %%rax,  %[rl]\n"
+        "adcq %%rdx,  %[rh]\n"
+             "adcq $0,  %[rhh]\n"
+             :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i+3]), [v] "r" (x[i+3])  :"rdx","cc");
+			}
+	return s;
+}
 
+
+
+void check() {
     uint64_t l1 = 0;
     uint64_t h1 = 0;
     uint64_t vh1 = 0;
@@ -104,6 +154,7 @@ void check() {
 }
 
 int main() {
+    check();
 	const size_t N = 100000;
 	uint64_t a[N];
 	uint64_t x[N];
@@ -124,8 +175,19 @@ int main() {
 	  s2 = asmscalarproduct(N, a, x);
 	}
     const clock_t S3 = clock();
-    cout<<"GCC time="<<(double)(S2-S1)/ CLOCKS_PER_SEC<<endl;
+	for(int T=0; T<10000;++T) {
+	  s2 = asm2scalarproduct(N, a, x);
+	}
+   	const clock_t S4 = clock();
+	for(int T=0; T<10000;++T) {
+	  s2 = asm4scalarproduct(N, a, x);
+	}
+   	const clock_t S5 = clock();
+      cout<<"GCC time="<<(double)(S2-S1)/ CLOCKS_PER_SEC<<endl;
     cout<<"asm time="<<(double)(S3-S2)/ CLOCKS_PER_SEC<<endl;
+ cout<<"asm2 time="<<(double)(S4-S3)/ CLOCKS_PER_SEC<<endl;
+ cout<<"asm4 time="<<(double)(S5-S4)/ CLOCKS_PER_SEC<<endl;
+
 
 	cout<<s1.low<<" "<<s1.high<<" "<<s1.vhigh<<endl;
 	cout<<s2.low<<" "<<s2.high<<" "<<s2.vhigh<<endl;
