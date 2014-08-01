2e906e36a753651f440414d7c5898b3dc52826de
Daniel Lemire <lemire@gmail.com>
2014-08-01 18:33:50 -0400

    Saving..

--- extra/counters.java
+++ extra/counters.java
@@ -35,6 +35,24 @@ public class counters {
         return answer;
     }
     
+    
+    public static int[] merge3(int N, int T, int[][] arrays) {
+        char[] C= new char[N];
+        for(int k = 0; k < arrays.length; ++k) {
+            for(int v : arrays[k]) {
+                C[v]++;
+            }
+        }
+        ArrayList<Integer> s = new ArrayList<Integer>();
+        for(int k = 0; k<N;++k)
+          if(C[k]>T) s.add(k);
+        int[] answer = new int[s.size()];
+        int pos = 0;
+        for(int v: s)
+        answer[pos++] = v;
+        return answer;
+    }
+    
     public static void main(String[] args) {
         int N = 1000*1000*100;
         Random r = new Random();
@@ -44,14 +62,19 @@ public class counters {
               arrays[k][l] = r.nextInt(N);
         }
         long t1 = System.currentTimeMillis();
-        for(int k = 0; k<20; ++k)
+        for(int k = 0; k<5; ++k)
         System.out.println(merge1(N,2,arrays).length);
         long t2 = System.currentTimeMillis();
-        for(int k = 0; k<20; ++k)
+        for(int k = 0; k<5; ++k)
         System.out.println(merge2(N,2,arrays).length);
         long t3 = System.currentTimeMillis();
+        for(int k = 0; k<5; ++k)
+        System.out.println(merge3(N,2,arrays).length);
+        long t4 = System.currentTimeMillis();
+
         System.out.println("timings using hash "+(t2-t1));
         System.out.println("timings using naive "+(t3-t2));
+        System.out.println("timings using packed naive "+(t4-t3));
 
     }
 }
