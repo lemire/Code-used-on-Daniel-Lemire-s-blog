a91a46a3073397fa3506634ed09e33c8743033bf
Daniel Lemire <lemire@gmail.com>
2014-12-22 13:04:17 -0500

    Saving...

--- extra/costofremainder.cpp
+++ extra/costofremainder.cpp
@@ -36,45 +36,51 @@ struct uint192 {
     uint64_t vhigh;
 } ;
 
-////// completesum_alt stuff begin /////
 
-#define COMPLETESUM_ALT_stuff_TYPE 1
+////// completesum_alt STUFF begin /////
 
-#define COMPLETESUM_ALT_stuff_DECLARE \
+#define COMPLETESUM_ALT_STUFF_TYPE 1
+#define USE_SSE
+
+#define COMPLETESUM_ALT_STUFF_DECLARE \
 	  uint64_t ctr0 = 0, ctr1 = 0, ctr2 = 0; \
+	  uint64_t ctr1_0 = 0, ctr1_1 = 0, ctr1_2 = 0, ctr1_3 = 0; \
 	  uint64_t ctr2_0 = 0, ctr2_1 = 0, ctr2_2 = 0, ctr2_3 = 0; \
 	  uint64_t mulLow, mulHigh;
 
-#define COMPLETESUM_ALT_stuff_T1( ii ) { \
+#define COMPLETESUM_ALT_STUFF_T_BASE( ii ) { \
 uint64_t rhi;  /*Dummy variable to tell the compiler that the register rax is input and clobbered but not actually output; see assembler code below. Better syntactic expression is very welcome.*/ \
 __asm__( "mulq %5\n" \
         "addq %%rax, %0\n" \
         "adcq %%rdx, %1\n" \
         "adcq $0, %2\n" \
         : "+g" (ctr0), "+g" (ctr1), "+g" (ctr2), "=a" (rhi) \
-        :"a"(x[ii]), "g"(a[ ii ]) : "rdx", "cc" ); \
+        :"a"(x[ii]), "g"(coeff[ ii ]) : "rdx", "cc" ); \
 }
 
-#if COMPLETESUM_ALT_stuff_TYPE == 0
+#if COMPLETESUM_ALT_STUFF_TYPE == 0
 
-#define COMPLETESUM_ALT_stuff_T2 COMPLETESUM_ALT_stuff_T1
-#define COMPLETESUM_ALT_stuff_FINALIZE
+#define COMPLETESUM_ALT_STUFF_T1 COMPLETESUM_ALT_STUFF_T_BASE
+#define COMPLETESUM_ALT_STUFF_T2 COMPLETESUM_ALT_STUFF_T_BASE
+#define COMPLETESUM_ALT_STUFF_FINALIZE
 
-#elif COMPLETESUM_ALT_stuff_TYPE == 1
+#elif COMPLETESUM_ALT_STUFF_TYPE == 1
 
-#define COMPLETESUM_ALT_stuff_T2( ii ) { \
+#define COMPLETESUM_ALT_STUFF_T1 COMPLETESUM_ALT_STUFF_T_BASE
+
+#define COMPLETESUM_ALT_STUFF_T2( ii ) { \
 uint64_t rhi;  /*Dummy variable to tell the compiler that the register rax is input and clobbered but not actually output; see assembler code below. Better syntactic expression is very welcome.*/ \
 __asm__( "mulq %6\n" \
+        "addq %%rax, %0\n" \
         "addq %%rdx, %1\n" \
         "adcq $0, %2\n" \
-        "addq %%rax, %0\n" \
         "shrq $32, %%rax\n" \
         "addq %%rax, %3\n" \
         : "+g" (ctr2_0), "+g" (ctr1), "+g" (ctr2), "+g" (ctr2_1), "=a" (rhi) \
-        :"a"(x[ii]), "g"(a[ ii ]) : "rdx", "rcx", "cc" ); \
+        :"a"(x[ii]), "g"(coeff[ ii ]) : "rdx", "cc" ); \
 }
 
-#define COMPLETESUM_ALT_stuff_FINALIZE { \
+#define COMPLETESUM_ALT_STUFF_FINALIZE { \
 	uint32_t xx = ctr2_0 >> 32; \
 	uint32_t yy = ctr2_1; \
 	uint64_t zz = xx - yy; \
@@ -89,10 +95,12 @@ __asm__("addq %3, %0\n" \
         : "g" (ctr2_0), "g" (xyz) : "cc" ); \
 }
 
-#elif COMPLETESUM_ALT_stuff_TYPE == 2
+#elif COMPLETESUM_ALT_STUFF_TYPE == 2
 
-#define COMPLETESUM_ALT_stuff_T2( ii ) { \
-        MUL64( mulHigh, mulLow, x[ii], a[ ii ] ); \
+#define COMPLETESUM_ALT_STUFF_T1 COMPLETESUM_ALT_STUFF_T_BASE
+
+#define COMPLETESUM_ALT_STUFF_T2( ii ) { \
+        MUL64( mulHigh, mulLow, x[ii], coeff[ ii ] ); \
         ctr2_0 += mulLow; \
         mulLow >>= 32; \
         ctr2_1 += mulLow; \
@@ -101,7 +109,7 @@ __asm__("addq %3, %0\n" \
         ctr2_3 += mulHigh; \
 }
 
-#define COMPLETESUM_ALT_stuff_FINALIZE { \
+#define COMPLETESUM_ALT_STUFF_FINALIZE { \
 	uint32_t xx = ctr2_0 >> 32; \
 	uint32_t yy = ctr2_1; \
 	uint64_t zz = xx - yy; \
@@ -127,26 +135,172 @@ __asm__("addq %2, %0\n" \
         : "g" (ctr2_2), "g" (xyz) : "cc" ); \
 }
 
+#elif COMPLETESUM_ALT_STUFF_TYPE == 3
+
+#define COMPLETESUM_ALT_STUFF_T1 COMPLETESUM_ALT_STUFF_T_BASE
+
+#define COMPLETESUM_ALT_STUFF_T2( ii ) { \
+uint64_t rhi;  /*Dummy variable to tell the compiler that the register rax is input and clobbered but not actually output; see assembler code below. Better syntactic expression is very welcome.*/ \
+__asm__( "mulq %6\n" \
+        "addq %%rdx, %1\n" \
+        "addq %%rax, %0\n" \
+        "shrq $32, %%rdx\n" \
+        "addq %%rdx, %2\n" \
+        "shrq $32, %%rax\n" \
+        "addq %%rax, %3\n" \
+        : "+g" (ctr2_0), "+g" (ctr2_2), "+g" (ctr2_3), "+g" (ctr2_1), "=a" (rhi) \
+        :"a"(x[ii]), "g"(coeff[ ii ]) : "rdx", "cc" ); \
+}
+
+#define COMPLETESUM_ALT_STUFF_FINALIZE { \
+	uint32_t xx = ctr2_0 >> 32; \
+	uint32_t yy = ctr2_1; \
+	uint64_t zz = xx - yy; \
+	ctr2_1 += zz; \
+	ctr2_0 = (uint32_t)ctr2_1; \
+	ctr2_0 += ((uint64_t)(uint32_t)ctr2_1) << 32; \
+    uint64_t xyz = (ctr2_1)>>32; \
+__asm__("addq %3, %0\n" \
+        "adcq %4, %1\n" \
+        "adcq $0, %2\n" \
+        : "+g" (ctr0), "+g" (ctr1), "+g" (ctr2) \
+        : "g" (ctr2_0), "g" (xyz) : "cc" ); \
+	xx = ctr2_2 >> 32; \
+	yy = ctr2_3; \
+	zz = xx - yy; \
+	ctr2_3 += zz; \
+	ctr2_2 = (uint32_t)ctr2_3; \
+	ctr2_2 += ((uint64_t)(uint32_t)ctr2_3) << 32; \
+    xyz = ctr2_3>>32; \
+__asm__("addq %2, %0\n" \
+        "adcq %3, %1\n" \
+        : "+g" (ctr1), "+g" (ctr2) \
+        : "g" (ctr2_2), "g" (xyz) : "cc" ); \
+}
+
+#elif COMPLETESUM_ALT_STUFF_TYPE == 4
+
+#define COMPLETESUM_ALT_STUFF_T1( ii ) { \
+uint64_t rhi;  /*Dummy variable to tell the compiler that the register rax is input and clobbered but not actually output; see assembler code below. Better syntactic expression is very welcome.*/ \
+__asm__( "mulq %6\n" \
+        "addq %%rdx, %1\n" \
+        "addq %%rax, %0\n" \
+        "shrq $32, %%rdx\n" \
+        "shrq $32, %%rax\n" \
+        "addq %%rdx, %2\n" \
+        "addq %%rax, %3\n" \
+        : "+g" (ctr1_0), "+g" (ctr1_2), "+g" (ctr1_3), "+g" (ctr1_1), "=a" (rhi) \
+        :"a"(x[ii]), "g"(coeff[ ii ]) : "rdx", "cc" ); \
+}
+
+#define COMPLETESUM_ALT_STUFF_T2( ii ) { \
+uint64_t rhi;  /*Dummy variable to tell the compiler that the register rax is input and clobbered but not actually output; see assembler code below. Better syntactic expression is very welcome.*/ \
+__asm__( "mulq %6\n" \
+        "addq %%rdx, %1\n" \
+        "addq %%rax, %0\n" \
+        "shrq $32, %%rdx\n" \
+        "shrq $32, %%rax\n" \
+        "addq %%rdx, %2\n" \
+        "addq %%rax, %3\n" \
+        : "+g" (ctr2_0), "+g" (ctr2_2), "+g" (ctr2_3), "+g" (ctr2_1), "=a" (rhi) \
+        :"a"(x[ii]), "g"(coeff[ ii ]) : "rdx", "cc" ); \
+}
+
+#define COMPLETESUM_ALT_STUFF_FINALIZE { \
+	uint32_t xx = ctr1_0 >> 32; \
+	uint32_t yy = ctr1_1; \
+	uint64_t zz = xx - yy; \
+	ctr1_1 += zz; \
+	ctr1_0 = (uint32_t)ctr1_1; \
+	ctr1_0 += ((uint64_t)(uint32_t)ctr1_1) << 32; \
+    uint64_t xyz = (ctr1_1)>>32; \
+__asm__("addq %3, %0\n" \
+        "adcq %4, %1\n" \
+        "adcq $0, %2\n" \
+        : "+g" (ctr0), "+g" (ctr1), "+g" (ctr2) \
+        : "g" (ctr1_0), "g" (xyz) : "cc" ); \
+	xx = ctr1_2 >> 32; \
+	yy = ctr1_3; \
+	zz = xx - yy; \
+	ctr1_3 += zz; \
+	ctr1_2 = (uint32_t)ctr1_3; \
+	ctr1_2 += ((uint64_t)(uint32_t)ctr1_3) << 32; \
+    xyz = ctr1_3>>32; \
+__asm__("addq %2, %0\n" \
+        "adcq %3, %1\n" \
+        : "+g" (ctr1), "+g" (ctr2) \
+        : "g" (ctr1_2), "g" (xyz) : "cc" ); \
+	xx = ctr2_0 >> 32; \
+	yy = ctr2_1; \
+	zz = xx - yy; \
+	ctr2_1 += zz; \
+	ctr2_0 = (uint32_t)ctr2_1; \
+	ctr2_0 += ((uint64_t)(uint32_t)ctr2_1) << 32; \
+    xyz = (ctr2_1)>>32; \
+__asm__("addq %3, %0\n" \
+        "adcq %4, %1\n" \
+        "adcq $0, %2\n" \
+        : "+g" (ctr0), "+g" (ctr1), "+g" (ctr2) \
+        : "g" (ctr2_0), "g" (xyz) : "cc" ); \
+	xx = ctr2_2 >> 32; \
+	yy = ctr2_3; \
+	zz = xx - yy; \
+	ctr2_3 += zz; \
+	ctr2_2 = (uint32_t)ctr2_3; \
+	ctr2_2 += ((uint64_t)(uint32_t)ctr2_3) << 32; \
+    xyz = ctr2_3>>32; \
+__asm__("addq %2, %0\n" \
+        "adcq %3, %1\n" \
+        : "+g" (ctr1), "+g" (ctr2) \
+        : "g" (ctr2_2), "g" (xyz) : "cc" ); \
+}
+
 #endif
 
-void completesum_alt(const uint64_t* a, const uint64_t *  x, const size_t length, uint64_t * out) 
+void completesum_alt(const uint64_t* coeff, const uint64_t *  x, const size_t length, uint64_t * out) 
 {
-	COMPLETESUM_ALT_stuff_DECLARE
+	COMPLETESUM_ALT_STUFF_DECLARE
 	
     size_t i = 0;
-    for(; i<length*8/8; i+= 8) {
+    for(; i<length*32/32; i+= 32) {
+//    for(; i<length*16/16; i+= 16) {
+//    for(; i<length*8/8; i+= 8) {
 #ifdef IACA
         IACA_START;
 #endif
 
-	COMPLETESUM_ALT_stuff_T2( i + 0 )
-	COMPLETESUM_ALT_stuff_T1( i + 1 )
-	COMPLETESUM_ALT_stuff_T2( i + 2 )
-	COMPLETESUM_ALT_stuff_T1( i + 3 )
-	COMPLETESUM_ALT_stuff_T2( i + 4 )
-	COMPLETESUM_ALT_stuff_T1( i + 5 )
-	COMPLETESUM_ALT_stuff_T2( i + 6 )
-	COMPLETESUM_ALT_stuff_T1( i + 7 )
+	COMPLETESUM_ALT_STUFF_T2( i + 0 )
+	COMPLETESUM_ALT_STUFF_T1( i + 1 )
+	COMPLETESUM_ALT_STUFF_T2( i + 2 )
+	COMPLETESUM_ALT_STUFF_T1( i + 3 )
+	COMPLETESUM_ALT_STUFF_T2( i + 4 )
+	COMPLETESUM_ALT_STUFF_T1( i + 5 )
+	COMPLETESUM_ALT_STUFF_T2( i + 6 )
+	COMPLETESUM_ALT_STUFF_T1( i + 7 )
+	COMPLETESUM_ALT_STUFF_T2( i + 8 )
+	COMPLETESUM_ALT_STUFF_T1( i + 9 )
+	COMPLETESUM_ALT_STUFF_T2( i + 10 )
+	COMPLETESUM_ALT_STUFF_T1( i + 11 )
+	COMPLETESUM_ALT_STUFF_T2( i + 12 )
+	COMPLETESUM_ALT_STUFF_T1( i + 13 )
+	COMPLETESUM_ALT_STUFF_T2( i + 14 )
+	COMPLETESUM_ALT_STUFF_T1( i + 15 )
+	COMPLETESUM_ALT_STUFF_T2( i + 16 )
+	COMPLETESUM_ALT_STUFF_T1( i + 17 )
+	COMPLETESUM_ALT_STUFF_T2( i + 18 )
+	COMPLETESUM_ALT_STUFF_T1( i + 19 )
+	COMPLETESUM_ALT_STUFF_T2( i + 20 )
+	COMPLETESUM_ALT_STUFF_T1( i + 21 )
+	COMPLETESUM_ALT_STUFF_T2( i + 22 )
+	COMPLETESUM_ALT_STUFF_T1( i + 23 )
+	COMPLETESUM_ALT_STUFF_T2( i + 24 )
+	COMPLETESUM_ALT_STUFF_T1( i + 25 )
+	COMPLETESUM_ALT_STUFF_T2( i + 26 )
+	COMPLETESUM_ALT_STUFF_T1( i + 27 )
+	COMPLETESUM_ALT_STUFF_T2( i + 28 )
+	COMPLETESUM_ALT_STUFF_T1( i + 29 )
+	COMPLETESUM_ALT_STUFF_T2( i + 30 )
+	COMPLETESUM_ALT_STUFF_T1( i + 31 )
 
 #ifdef IACA
         IACA_END;
@@ -155,10 +309,10 @@ void completesum_alt(const uint64_t* a, const uint64_t *  x, const size_t length
 	
     for(; i<length; ++i) 
 	{
-	COMPLETESUM_ALT_stuff_T1( i )
+	COMPLETESUM_ALT_STUFF_T1( i )
     }
 	
-	COMPLETESUM_ALT_stuff_FINALIZE
+	COMPLETESUM_ALT_STUFF_FINALIZE
 	
     out[0] = ctr0;
     out[1] = ctr1;
@@ -216,6 +370,195 @@ void completesum(const uint64_t* a, const uint64_t *  x, const size_t length, ui
             "adcq %%rdx,  %[rh]\n"
             "adcq $0,  %[rhh]\n"
             "mulq 56(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "r" (a+i), [v] "r" (x+i)  :"rdx","rax","memory","cc");
+#ifdef IACA
+        IACA_END;
+#endif
+    }
+    for(; i<length; ++i) {
+        __asm__ ("mulq %[v]\n"
+                 "addq %%rax,  %[rl]\n"
+                 "adcq %%rdx,  %[rh]\n"
+                 "adcq $0,  %[rhh]\n"
+                 :  [rh] "+r" (s.high), [rhh] "+r" (s.vhigh) , [rl] "+r" (s.low)  : [u] "a" (a[i]), [v] "r" (x[i])  :"rdx","cc");
+    }
+    out[0] = s.low;
+    out[1] = s.high;
+    out[2] = s.vhigh;
+}
+
+
+void completesum32blocks(const uint64_t* a, const uint64_t *  x, const size_t length, uint64_t * out) {
+    uint192 s;
+    s.low = 0;
+    s.high = 0;
+    s.vhigh = 0;
+    size_t i = 0;
+    for(; i<length*32/32; i+= 32) {
+#ifdef IACA
+        IACA_START;
+#endif
+        __asm__ (
+            "movq (%[u]),%%rax\n"
+            "mulq (%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 8(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 8(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 16(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 16(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 24(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 24(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 32(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 32(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 40(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 40(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 48(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 48(%[v])\n"
+            "addq %%rax,  %[rl]\n"			
+            "movq 56(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 56(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 64(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 64(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 72(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 72(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 80(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 80(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 88(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 88(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 96(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 96(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 104(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 104(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 112(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 112(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 120(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 120(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 128(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 128(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 136(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 136(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 144(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 144(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 152(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 152(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 160(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 160(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 168(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 168(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 176(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 176(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 184(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 184(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 192(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 192(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 200(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 200(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 208(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 208(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 216(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 216(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 224(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 224(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 232(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 232(%[v])\n"
+            "addq %%rax,  %[rl]\n"
+            "movq 240(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 240(%[v])\n"
+            "addq %%rax,  %[rl]\n"			
+            "movq 248(%[u]),%%rax\n"
+            "adcq %%rdx,  %[rh]\n"
+            "adcq $0,  %[rhh]\n"
+            "mulq 248(%[v])\n"
             "addq %%rax,  %[rl]\n"
             "adcq %%rdx,  %[rh]\n"
             "adcq $0,  %[rhh]\n"
@@ -437,11 +780,19 @@ int main() {
     cout<<out[0]<<" "<<out[1]<<" "<<out[2]<<endl;
     out[0]=0; out[1]=0; out[2]=0;  
     const clock_t S4 = clock();
+    out[0]=0; out[1]=0; out[2]=0;   
+    for(int T=0; T<repeat; ++T) {
+        completesum32blocks( a, x, N,out);
+    }
+    cout<<out[0]<<" "<<out[1]<<" "<<out[2]<<endl;
+    out[0]=0; out[1]=0; out[2]=0;  
+    const clock_t S5 = clock();
     double numberofinputpairs = (double) N * repeat/1000000.0;
     cout<<"We report the number of millions of input pairs processed per second"<<endl;
-    cout<<"complete_alt sumtime="<<numberofinputpairs/((double)(S1-S0)/ CLOCKS_PER_SEC)<<endl;
-    cout<<"complete sum time="<<numberofinputpairs/((double)(S2-S1)/ CLOCKS_PER_SEC)<<endl;
+    cout<<"complete_alt sum ="<<numberofinputpairs/((double)(S1-S0)/ CLOCKS_PER_SEC)<<endl;
+    cout<<"complete sum ="<<numberofinputpairs/((double)(S2-S1)/ CLOCKS_PER_SEC)<<endl;
     cout<<"MMH sum ="<<numberofinputpairs/((double)(S3-S2)/ CLOCKS_PER_SEC)<<endl;
     cout<<"NH sum ="<<numberofinputpairs/((double)(S4-S3)/ CLOCKS_PER_SEC)<<endl;
+    cout<<"complete sum (32 ints)="<<numberofinputpairs/((double)(S5-S4)/ CLOCKS_PER_SEC)<<endl;
 
 }
