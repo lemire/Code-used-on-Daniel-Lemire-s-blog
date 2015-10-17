1e9ebb8961ec2c7e08fce6dec83835c4a48c434d
Daniel Lemire <lemire@gmail.com>
2015-10-16 23:02:39 -0400

    Simplifying code.

--- extra/batchbinary.c
+++ extra/batchbinary.c
@@ -93,12 +93,12 @@ __m128i branchfree_search4_avx(int* source, size_t n, int target1, int target2, 
     size_t oldn = n;
     __m128i ha = _mm_set1_epi32(n>>1);
     while(n>1) {
+        n >>= 1;
         __m128i offsetsplushalf = _mm_add_epi32(offsets,ha);
+        ha = _mm_srli_epi32(ha,1);
         __m128i keys = _mm_i32gather_epi32(source,offsetsplushalf,4);
         __m128i lt = _mm_cmplt_epi32(keys,target);
         offsets = _mm_blendv_epi8(offsets,offsetsplushalf,lt);
-        n -= (n >> 1);
-        ha = _mm_srli_epi32(ha,1);
     }
     __m128i lastkeys = _mm_i32gather_epi32(source,offsets,4);
     __m128i lastlt = _mm_cmplt_epi32(lastkeys,target);
