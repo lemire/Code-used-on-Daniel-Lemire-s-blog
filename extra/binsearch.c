d943f0a8fac6b8e02eaab9bfdb78dbc36bb653a0
Daniel Lemire <lemire@gmail.com>
2015-10-20 09:35:40 -0400

    With portable version.

--- extra/binsearch.c
+++ extra/binsearch.c
@@ -4,7 +4,7 @@
 //
 // -DPOWER_OF_TWO to see the bad effects of cache line aliasing
 // -DLINEAR_REFERENCE to compare against a slow reference implementation
-// -DDEBUG_PRINT to add verbose debugging from within routines 
+// -DDEBUG_PRINT to add verbose debugging from within routines
 
 #include <sys/types.h>
 #include <stdint.h>
@@ -28,12 +28,12 @@ void parallel_search(uint32_t *array, size_t length,
 
     for (size_t half = length/2; half; length -= half, half = length/2) {
         DEBUG_PRINT("\n%zd\n", half);
-        for (size_t c = 0; c < count; c++) { 
+        for (size_t c = 0; c < count; c++) {
             size_t i = indexes[c];    // load stored index for this target
             size_t test = i + half;   // test index will be halfway point
-            if (targets[c] > array[test]) i = test; // update index with CMOV 
+            if (targets[c] > array[test]) i = test; // update index with CMOV
             indexes[c] = i;           // always resave whether updated or not
-            DEBUG_PRINT("%d %d %zu %zu %zu\n", 
+            DEBUG_PRINT("%d %d %zu %zu %zu\n",
                         targets[c], array[test], c, i, test);
         }
     }
@@ -50,6 +50,38 @@ void parallel_search(uint32_t *array, size_t length,
     // indexes matching-or-less-than targets ready for caller
 }
 
+
+// parallel binary search for the 'count' 'targets' in a sorted 'array'
+// matching 'indexes' are first element equal or greater than each target
+// this version does not assume cmov
+void portable_parallel_search(uint32_t *array, size_t length,
+                     uint32_t *targets, size_t *indexes, size_t count) {
+    memset(indexes, 0, count * sizeof(*indexes)); // set indexes to zero
+
+    for (size_t half = length/2; half; length -= half, half = length/2) {
+        DEBUG_PRINT("\n%zd\n", half);
+        for (size_t c = 0; c < count; c++) {
+            size_t i = indexes[c];    // load stored index for this target
+            i += ( ((int32_t)array[i+half]-(int32_t)targets[c])>>31) & half;
+            indexes[c] = i;           // always resave whether updated or not
+            DEBUG_PRINT("%d %d %zu %zu %zu\n",
+                        targets[c], array[test], c, i, test);
+        }
+    }
+
+    // FUTURE: may be possible to integrate 'adjustment' into loop above?
+    uint32_t min = array[0];
+    for (size_t c = 0; c < count; c++) {
+        uint32_t target = targets[c];
+        size_t adjusted = indexes[c] + 1;
+        if (target <= min) adjusted = 0;
+        indexes[c] = adjusted;
+    }
+
+    // indexes matching-or-less-than targets ready for caller
+}
+
+
 // reference implementation equivalent to parallel_search
 // NB: if target > max(array), matched index = length (out-of-bounds)
 void linear_search(uint32_t *array, size_t length,
@@ -59,9 +91,9 @@ void linear_search(uint32_t *array, size_t length,
         size_t n = 0;
         // save index of first element in array >= target
         while (n < length && array[n] < target) n++;
-        DEBUG_PRINT("linear_search: %zd %u %zd (%u)\n", c, target, n, 
+        DEBUG_PRINT("linear_search: %zd %u %zd (%u)\n", c, target, n,
                     n < length ? array[n] : UINT32_MAX);
-        indexes[c] = n; 
+        indexes[c] = n;
     }
 }
 
@@ -84,11 +116,11 @@ uint32_t *create_search_targets(size_t count, size_t max) {
 size_t verify_indexes(size_t *reference, size_t *test, size_t count,
                          uint32_t *array, uint32_t *targets, size_t length) {
     for (size_t i = 0; i < count; i++) {
-        if (reference[i] != test[i]) { 
+        if (reference[i] != test[i]) {
             // avoid read-past-end when printing error details
-            printf(" MISMATCH %zd (%u): %zd (%u) %zd (%u)", 
-                   i, targets[i], reference[i], 
-                   reference[i] < length ? array[reference[i]] : UINT32_MAX, 
+            printf(" MISMATCH %zd (%u): %zd (%u) %zd (%u)",
+                   i, targets[i], reference[i],
+                   reference[i] < length ? array[reference[i]] : UINT32_MAX,
                    test[i], test[i] < length ? array[test[i]] : UINT32_MAX);
             return 1;
         }
@@ -129,18 +161,18 @@ int main(int argc, char **argv) {
 
 #ifdef POWER_OF_TWO
     size_t lengths[] = {
-        256, 1024, 256 * 256, 256 * 1024, 
+        256, 1024, 256 * 256, 256 * 1024,
         1024*1024, 16*1024*1024, 256*1024*1024
     };
 #else
     size_t lengths[] = {
-        100, 1000, 10*1000, 100*1000, 
+        100, 1000, 10*1000, 100*1000,
         1000*1000, 10*1000*1000, 100*1000*1000
     };
 #endif
-    
+
     size_t batches[] = { // batch sizes reduced to target count as needed
-        1, 2, 4, 6, 8, 10, 12, 14, 16, 20, 
+        1, 2, 4, 6, 8, 10, 12, 14, 16, 20,
         40, 60, 100, 200, 400, 1000, UINT32_MAX
     };
 
@@ -157,10 +189,10 @@ int main(int argc, char **argv) {
 
         size_t *reference_indexes = malloc(count * sizeof(size_t));
 #ifdef LINEAR_REFERENCE
-        linear_search(array, length, 
+        linear_search(array, length,
                       targets, reference_indexes, count);
 #else
-        parallel_search(array, length, 
+        parallel_search(array, length,
                         targets, reference_indexes, count);
 #endif
 
@@ -175,17 +207,30 @@ int main(int argc, char **argv) {
             RDTSC_START(cycles_start);
             for (size_t c = 0; c < count; c += batch) {
                 // possible smaller batch for last iteration
-                if (c > count - batch) batch = count % batch; 
-                parallel_search(array, length, targets + c, 
+                if (c > count - batch) batch = count % batch;
+                parallel_search(array, length, targets + c,
                                 indexes + c, batch);
             }
             RDTSC_FINAL(cycles_final);
-            float cycles_per_search = 
+            float cycles_per_search =
                 (cycles_final - cycles_start) / (float) count;
-            printf(" %.2f cycles/search", cycles_per_search);
+            printf("cmov?: %.2f cycles/search ", cycles_per_search);
             verify_indexes(reference_indexes, indexes, count,
                            array, targets, length);
+
+            RDTSC_START(cycles_start);
+            for (size_t c = 0; c < count; c += batch) {
+                // possible smaller batch for last iteration
+                if (c > count - batch) batch = count % batch;
+                portable_parallel_search(array, length, targets + c,
+                                indexes + c, batch);
+            }
+            RDTSC_FINAL(cycles_final);
+            cycles_per_search =
+                (cycles_final - cycles_start) / (float) count;
+            printf(", portable: %.2f cycles/search", cycles_per_search);
             printf("\n");
+
         }
         free(array);
         free(targets);
@@ -194,4 +239,3 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 }
-
