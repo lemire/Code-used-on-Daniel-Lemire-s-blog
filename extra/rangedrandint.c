fdb8806f47b24ae27827275bb35cf91243c7549e
Daniel Lemire <lemire@gmail.com>
2015-11-17 19:47:47 -0500

    Saving...

--- extra/rangedrandint.c
+++ extra/rangedrandint.c
@@ -21,26 +21,20 @@ int fogapproach(uint16_t range) {
       printf("crazy = %d ",(1<<16)-range+ (range & (~(range-1))));
       return -1;
     }
-    int failure = 0;
-    int softfailure = 0;
 
     for(int k = 0; k < 1<<16; ++k) {
         uint32_t r = k;
         uint16_t c =(r* range) >> 16;
         int leftover = (r* range) & 0xFFFF;
-        if(leftover > (1<<16)-range) ++softfailure;
         if(leftover <= threshold) {
             counter[c]++;
         } else {
 
              if(leftover <= (1<<16)-range)
               return -1;
-            ++failure;
 
         }
     }
-    printf("failure rate  %f \n",failure * 1.0 / (1<<16));
-    printf("soft failure rate  %f \n",softfailure * 1.0 / (1<<16));
 
     int minc = UINT16_MAX+1;
     int maxc = 0;
@@ -51,64 +45,36 @@ int fogapproach(uint16_t range) {
     }
     printf("%d ** %d +++  %d -- %d +++ %d \n",-minc+maxc,minc,UINT16_MAX/range,(UINT16_MAX+range-1)/range,maxc);
     if(minc == maxc) printf("******fair\n");
-    else printf("======unfair \n");
+    else {printf("======unfair \n"); return -1;}
     return 0;
 }
-int nakedfogapproach(uint16_t range) {
-    printf("naked fog range %d \n",range);
+
+int fasterthanfogapproach(uint16_t range) {
+    printf("fog range %d \n",range);
 
     int counter[1<<16];
     for(int k = 0; k < 1<<16; ++k) {
         counter[k] = 0;
     }
+    int fastthreshold = (1<<16)-range+ (range & (~(range-1)));
     int threshold = (int) ((uint32_t)((1<<16)/range*range) -1);
-    if(threshold < (1<<16)-range) return -1;
-    int failure = 0;
-    int softfailure = 0;
-
-    for(int k = 0; k < 1<<16; ++k) {
-        uint32_t r = k;
-        uint16_t c =(r* range) >> 16;
-        counter[c]++;
+    if(threshold+1 < fastthreshold){
+      printf("threshold = %d ",threshold);
+      printf("crazy = %d ",fastthreshold);
+      return -1;
     }
-    printf("failure rate  %f \n",failure * 1.0 / (1<<16));
-    printf("soft failure rate  %f \n",softfailure * 1.0 / (1<<16));
-
-    int minc = UINT16_MAX+1;
-    int maxc = 0;
-    int fair = counter[0];
-    for(int k = 0; k < range; ++k) {
-        if(counter[k]<minc) minc = counter[k];
-        if(counter[k]>maxc) maxc = counter[k];
-    }
-    printf("%d ** %d  %d \n",-minc+maxc,minc,maxc);
-    if(minc == maxc) printf("******fair\n");
-    else printf("======unfair \n");
-    return 0;
-}
-
-int cheapfogapproach(uint16_t range) {
-    printf("cheap fog range %d \n",range);
-
-    int counter[1<<16];
-    for(int k = 0; k < 1<<16; ++k) {
-        counter[k] = 0;
-    }
-    int threshold = (int) ((uint32_t)((1<<16)/range*range) -1);
-    if(threshold < (1<<16)-range) return -1;
-    int failure = 0;
-    int softfailure = 0;
 
     for(int k = 0; k < 1<<16; ++k) {
         uint32_t r = k;
         uint16_t c =(r* range) >> 16;
         int leftover = (r* range) & 0xFFFF;
-        if(leftover <= (1<<16)-range){
-          counter[c]++;
+        if(leftover < fastthreshold) {
+            counter[c]++;
+        } else {
+          if(leftover <= threshold)
+            counter[c]++;
         }
     }
-    printf("failure rate  %f \n",failure * 1.0 / (1<<16));
-    printf("soft failure rate  %f \n",softfailure * 1.0 / (1<<16));
 
     int minc = UINT16_MAX+1;
     int maxc = 0;
@@ -117,11 +83,12 @@ int cheapfogapproach(uint16_t range) {
         if(counter[k]<minc) minc = counter[k];
         if(counter[k]>maxc) maxc = counter[k];
     }
-    printf("%d ** %d  %d \n",-minc+maxc,minc,maxc);
-    if(minc == maxc) printf("******************************fair\n");
-    else printf("======unfair \n");
+    printf("%d ** %d +++  %d -- %d +++ %d \n",-minc+maxc,minc,UINT16_MAX/range,(UINT16_MAX+range-1)/range,maxc);
+    if(minc == maxc) printf("******fair\n");
+    else {printf("======unfair \n"); return -1;}
     return 0;
 }
+
 
 
 int classicapproach(uint16_t range) {
@@ -153,7 +120,7 @@ int classicapproach(uint16_t range) {
     }
     printf("%d ** %d +++  %d -- %d +++ %d \n",-minc+maxc,minc,UINT16_MAX/range,(UINT16_MAX+range-1)/range,maxc);
     if(minc == maxc) printf("******fair\n");
-    else printf("======unfair \n");
+    else {printf("======unfair \n"); return -1;}
     return 0;
 }
 
@@ -164,9 +131,7 @@ int main() {
     for(int k = 1; k <60000; k++) {
         int r = fogapproach(k);
         if(r<0) return r;
-        r = nakedfogapproach(k);
-        if(r<0) return r;
-        r = cheapfogapproach(k);
+        r = fasterthanfogapproach(k);
         if(r<0) return r;
 
         r = classicapproach(k);
