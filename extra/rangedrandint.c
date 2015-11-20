a76ebc18bbd51ac838cb9202b17f49b712f0b157
Daniel Lemire <lemire@gmail.com>
2015-11-19 19:53:59 -0500

    Streamlining.

--- extra/rangedrandint.c
+++ extra/rangedrandint.c
@@ -49,8 +49,76 @@ int fogapproach(uint16_t range) {
     return 0;
 }
 
+int reversefogapproach(uint16_t range) {
+    printf("reverse fog range %d \n",range);
+
+    int counter[1<<16];
+    for(int k = 0; k < 1<<16; ++k) {
+        counter[k] = 0;
+    }
+    int threshold = (1<<16) % range;
+    for(int k = 0; k < 1<<16; ++k) {
+        uint32_t r = k;
+        uint16_t c =(r* range) >> 16;
+        int leftover = (r* range) & 0xFFFF;
+        if(leftover >= threshold) {
+            counter[c]++;
+        } else {
+        }
+    }
+
+    int minc = UINT16_MAX+1;
+    int maxc = 0;
+    int fair = counter[0];
+    for(int k = 0; k < range; ++k) {
+        if(counter[k]<minc) minc = counter[k];
+        if(counter[k]>maxc) maxc = counter[k];
+    }
+    printf("%d ** %d +++  %d -- %d +++ %d \n",-minc+maxc,minc,UINT16_MAX/range,(UINT16_MAX+range-1)/range,maxc);
+    if(minc == maxc) printf("******fair\n");
+    else {printf("======unfair \n"); return -1;}
+    return 0;
+}
+
+int fasterreversefogapproach(uint16_t range) {
+    printf("faster reverse fog range %d \n",range);
+
+    int counter[1<<16];
+    for(int k = 0; k < 1<<16; ++k) {
+        counter[k] = 0;
+    }
+    int threshold = (1<<16) % range;
+    int cheap = range- (range & (~(range-1)));
+    if(cheap < threshold ) {
+      printf("bug");
+      abort();
+    }
+    for(int k = 0; k < 1<<16; ++k) {
+        uint32_t r = k;
+        uint16_t c =(r* range) >> 16;
+        int leftover = (r* range) & 0xFFFF;
+        if(leftover >= threshold) {
+            counter[c]++;
+        } else {
+        }
+    }
+
+    int minc = UINT16_MAX+1;
+    int maxc = 0;
+    int fair = counter[0];
+    for(int k = 0; k < range; ++k) {
+        if(counter[k]<minc) minc = counter[k];
+        if(counter[k]>maxc) maxc = counter[k];
+    }
+    printf("%d ** %d +++  %d -- %d +++ %d \n",-minc+maxc,minc,UINT16_MAX/range,(UINT16_MAX+range-1)/range,maxc);
+    if(minc == maxc) printf("******fair\n");
+    else {printf("======unfair \n"); return -1;}
+    return 0;
+}
+
+
 int fasterthanfogapproach(uint16_t range) {
-    printf("fog range %d \n",range);
+    printf("fasterthanfog range %d \n",range);
 
     int counter[1<<16];
     for(int k = 0; k < 1<<16; ++k) {
@@ -131,6 +199,11 @@ int main() {
     for(int k = 1; k <60000; k++) {
         int r = fogapproach(k);
         if(r<0) return r;
+        r = reversefogapproach(k);
+        if(r<0) return r;
+        r = fasterreversefogapproach(k);
+        if(r<0) return r;
+
         r = fasterthanfogapproach(k);
         if(r<0) return r;
 
