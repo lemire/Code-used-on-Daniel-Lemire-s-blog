8c244b1d70a9f1eeb7fb5785b13032cfac72ab40
Daniel Lemire <lemire@gmail.com>
2015-12-24 22:48:12 -0500

    Entering Nate's version.

--- extra/simdspeed.cpp
+++ extra/simdspeed.cpp
@@ -1,6 +1,6 @@
 // Daniel Lemire, Nov 2nd 2013
 /***
-$ g++-4.8.0 -O3 -mavx -o simdspeed simdspeed.cpp 
+$ g++-4.8.0 -O3 -mavx -o simdspeed simdspeed.cpp
 91 29 28
 1 3.14 3.25
 #ignore=724.07733 724.07733 724.07733
@@ -48,11 +48,11 @@ using namespace std;
 
 __attribute__((__noinline__,__target__("no-avx"),__target__("no-sse2")))
 float scalar(const float * a, const float *b, size_t length) {
-	float sum = 0;
-	for(size_t i = 0; i < length;i++ ) {
-		sum += a[i] * b[i];
-	}
-	return sqrt(sum);
+    float sum = 0;
+    for(size_t i = 0; i < length; i++ ) {
+        sum += a[i] * b[i];
+    }
+    return sqrt(sum);
 }
 
 #ifdef __SSE3__
@@ -60,15 +60,15 @@ __attribute__((__noinline__))
 float scalar128(const float * a, const float *b, size_t length) {
     __m128 sum = _mm_set1_ps(0);
     assert(length/4*4==length);
-	for(size_t i = 0; i < length/4; ++i ) {
-		const __m128 veca = _mm_loadu_ps (a+4*i);
-		const __m128 vecb = _mm_loadu_ps (b+4*i);
-		const __m128 r1 = _mm_mul_ps(veca, vecb);
+    for(size_t i = 0; i < length/4; ++i ) {
+        const __m128 veca = _mm_loadu_ps (a+4*i);
+        const __m128 vecb = _mm_loadu_ps (b+4*i);
+        const __m128 r1 = _mm_mul_ps(veca, vecb);
         sum = _mm_add_ps(sum,r1);
-	}
+    }
     __m128 r2 = _mm_hadd_ps(sum, sum);
     __m128 r3 = _mm_hadd_ps(r2, r2);
-	return sqrt( _mm_cvtss_f32 (r3));
+    return sqrt( _mm_cvtss_f32 (r3));
 }
 #endif
 
@@ -77,48 +77,48 @@ __attribute__((__noinline__))
 float scalar256(const float * a, const float *b, size_t length) {
     __m256 sum =  _mm256_set1_ps (0);
     assert(length/8*8==length);
-	for(size_t i = 0; i < length/8; ++i ) {
-		const __m256 veca = _mm256_loadu_ps (a+8*i);
-		const __m256 vecb = _mm256_loadu_ps (b+8*i);
-		const __m256 r1 = _mm256_mul_ps(veca, vecb);
+    for(size_t i = 0; i < length/8; ++i ) {
+        const __m256 veca = _mm256_loadu_ps (a+8*i);
+        const __m256 vecb = _mm256_loadu_ps (b+8*i);
+        const __m256 r1 = _mm256_mul_ps(veca, vecb);
         sum =  _mm256_add_ps (sum,r1);
-	}
+    }
     __m256 r2 = _mm256_hadd_ps(sum,sum);
     __m256 r3 = _mm256_hadd_ps(r2, r2);
     __m256 r4 = _mm256_hadd_ps(r3, r3);
-	return sqrt( _mm_cvtss_f32 ( _mm256_extractf128_ps (r4,0)) );
+    return sqrt( _mm_cvtss_f32 ( _mm256_extractf128_ps (r4,0)) );
 }
 #endif
 
 int main() {
     uint64_t cycles_start, cycles_final;
     int time1, time2, time3;
-	size_t N =  1024*1024;
-    int T=5;
-	vector<float> a(N,0.1);
-	vector<float> b(N,5);
-	float bogus1 = 0;
+    size_t N =  1024*4;
+    int T=10000;
+    vector<float> a(N,0.1);
+    vector<float> b(N,5);
+    float bogus1 = 0;
     float bogus2 = 0;
     float bogus3 = 0;
-	RDTSC_START(cycles_start);
-	for(int k = 0; k<T;++k)
-	bogus1 += scalar(&a[0], &b[0],N);
-	RDTSC_FINAL(cycles_final);
+    RDTSC_START(cycles_start);
+    for(int k = 0; k<T; ++k)
+        bogus1 += scalar(&a[0], &b[0],N);
+    RDTSC_FINAL(cycles_final);
 
-	time1 = cycles_final - cycles_start;
+    time1 = cycles_final - cycles_start;
 #ifdef __SSE3__
-	RDTSC_START(cycles_start);
-    for(int k = 0; k<T;++k)
-	bogus2 += scalar128(&a[0], &b[0],N);
-	RDTSC_FINAL(cycles_final);
+    RDTSC_START(cycles_start);
+    for(int k = 0; k<T; ++k)
+        bogus2 += scalar128(&a[0], &b[0],N);
+    RDTSC_FINAL(cycles_final);
 
     time2 = cycles_final - cycles_start;
 #endif
 #ifdef __AVX__
-	RDTSC_START(cycles_start);
-    for(int k = 0; k<T;++k)
-	bogus3 += scalar256(&a[0], &b[0],N);
-	RDTSC_FINAL(cycles_final);
+    RDTSC_START(cycles_start);
+    for(int k = 0; k<T; ++k)
+        bogus3 += scalar256(&a[0], &b[0],N);
+    RDTSC_FINAL(cycles_final);
 
     time3 = cycles_final - cycles_start;
 #endif
