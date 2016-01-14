01c973603c3409c1651fa222ff015d1e6f29d334
Daniel Lemire <lemire@gmail.com>
2016-01-14 15:29:22 -0500

    version with intrinsics.

--- extra/popcnt.c
+++ extra/popcnt.c
@@ -467,6 +467,27 @@ uint64_t bitset_count_popcnt(uint8_t *bitset) {
     return total_count;
 }
 
+uint64_t intrinsics_count(uint8_t *bitset) {
+    // these are precomputed hamming weights (weight(0), weight(1)...)
+    const __m256i shuf = _mm256_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+       0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+    const __m256i  mask = _mm256_set1_epi8(0x0f); // low 4 bits of each byte
+    __m256i total = _mm256_setzero_si256();
+    __m256i zero = _mm256_setzero_si256();
+    for(unsigned  k = 0; k < BITSET_BYTES/sizeof(__m256i); k++) {
+        __m256i ymm1 = _mm256_lddqu_si256((const __m256i *)bitset + k);
+        __m256i ymm2 = _mm256_srli_epi32(ymm1,4); // shift right, shiftingin zeroes
+        ymm1 = _mm256_and_si256(ymm1,mask); // contains even 4 bits
+        ymm2 = _mm256_and_si256(ymm2,mask); // contains odd 4 bits
+        ymm1 = _mm256_shuffle_epi8(shuf,ymm1);// use table look-up to sum the 4 bits
+        ymm2 = _mm256_shuffle_epi8(shuf,ymm2);
+        ymm1 = _mm256_add_epi8(ymm1,ymm2);// each byte contains weight of input byte (values are in [0,8))
+        ymm1 = _mm256_sad_epu8(zero,ymm1);// produces 4 64-bit counters (having values in [0,32))
+        total= _mm256_add_epi64(total,ymm1); // add the 4 64-bit counters to previous counter
+    }
+    return _mm256_extract_epi64(total,0)+_mm256_extract_epi64(total,1)+_mm256_extract_epi64(total,2)+_mm256_extract_epi64(total,3);
+}
+
 
 uint64_t bitset_count_avx2_32(uint8_t *bitset) {
     ymm_t bytes, ymm1, ymm2, total, zero;
@@ -543,6 +564,7 @@ int main(/* int argc, char **argv */) {
 
     uint64_t count = bitset_count_popcnt(bitset);
 
+    RDTSC_BEST(intrinsics_count(bitset), count, RDTSC_REPEAT, BITSET_BYTES / 8);
     RDTSC_BEST(bitset_count_popcnt(bitset), count, RDTSC_REPEAT, BITSET_BYTES / 8);
     RDTSC_BEST(bitset_count_avx2_32(bitset), count, RDTSC_REPEAT, BITSET_BYTES / 8);
     RDTSC_BEST(bitset_count_avx2_64(bitset), count, RDTSC_REPEAT, BITSET_BYTES / 8);
