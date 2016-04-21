c75d64d01481a85efacac61f3e0999b2eec120eb
Daniel Lemire <lemire@gmail.com>
2016-04-21 11:52:23 -0400

    Saving...

--- extra/simdunion.c
+++ extra/simdunion.c
@@ -3,8 +3,35 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <string.h>
+#include <inttypes.h>
 #include <assert.h>
+
+
+#define RDTSC_START(cycles)                                                   \
+    do {                                                                      \
+        register unsigned cyc_high, cyc_low;                                  \
+        __asm volatile(                                                       \
+            "cpuid\n\t"                                                       \
+            "rdtsc\n\t"                                                       \
+            "mov %%edx, %0\n\t"                                               \
+            "mov %%eax, %1\n\t"                                               \
+            : "=r"(cyc_high), "=r"(cyc_low)::"%rax", "%rbx", "%rcx", "%rdx"); \
+        (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                      \
+    } while (0)
+
+#define RDTSC_FINAL(cycles)                                                   \
+    do {                                                                      \
+        register unsigned cyc_high, cyc_low;                                  \
+        __asm volatile(                                                       \
+            "rdtscp\n\t"                                                      \
+            "mov %%edx, %0\n\t"                                               \
+            "mov %%eax, %1\n\t"                                               \
+            "cpuid\n\t"                                                       \
+            : "=r"(cyc_high), "=r"(cyc_low)::"%rax", "%rbx", "%rcx", "%rdx"); \
+        (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                      \
+    } while (0)
+
 
 #include <x86intrin.h>
 static bool is_sorted(__m128i x) {
@@ -45,9 +72,9 @@ static void sse_merge(__m128i *vA, __m128i *vB, // input 1 & 2
 // vMax = 4 5 6 7
     *vMin = _mm_alignr_epi8(*vMin, *vMin, 4);
 // result: 0 1 2 3
-    assert(is_sorted(*vMin));
-    assert(is_sorted(*vMax));
-    assert((uint32_t)_mm_extract_epi32(*vMin,3)<=(uint32_t)_mm_extract_epi32(*vMax,0));
+    //assert(is_sorted(*vMin));
+    //assert(is_sorted(*vMax));
+    //assert((uint32_t)_mm_extract_epi32(*vMin,3)<=(uint32_t)_mm_extract_epi32(*vMax,0));
 
 }
 
@@ -83,13 +110,13 @@ static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
     while((pos1 < len1) && (pos2<len2)) {
         // code is not optimal, too much branching?
         if(curA <= curB) {
-          V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
-          pos1++;
-          curA = (pos1 < len1) ? array1[4*pos1] : 0;
+            V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
+            pos1++;
+            curA = (pos1 < len1) ? array1[4*pos1] : 0;
         } else {
-          V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
-          pos2++;
-          curB = (pos2 < len2) ? array2[4*pos2] : 0;
+            V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
+            pos2++;
+            curB = (pos2 < len2) ? array2[4*pos2] : 0;
         }
         sse_merge(&V,&vMax,&vMin,&vMax);
         _mm_storeu_si128((__m128i* )output + outpos,vMin);
@@ -97,22 +124,69 @@ static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
     }
     //
     while(pos1<len1) {
-      V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
-      pos1++;
-      sse_merge(&V,&vMax,&vMin,&vMax);
-      _mm_storeu_si128((__m128i* )output + outpos,vMin);
-      outpos++;
+        V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
+        pos1++;
+        sse_merge(&V,&vMax,&vMin,&vMax);
+        _mm_storeu_si128((__m128i* )output + outpos,vMin);
+        outpos++;
     }
     while(pos2<len2) {
-      V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
-      pos2++;
-      sse_merge(&V,&vMax,&vMin,&vMax);
-      _mm_storeu_si128((__m128i* )output + outpos,vMin);
-      outpos++;
+        V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
+        pos2++;
+        sse_merge(&V,&vMax,&vMin,&vMax);
+        _mm_storeu_si128((__m128i* )output + outpos,vMin);
+        outpos++;
     }
     _mm_storeu_si128((__m128i* )output + outpos,vMax);
     outpos++;
     return outpos * 4;
+}
+
+uint32_t union_uint32(const uint32_t *set_1, uint32_t size_1, const uint32_t *set_2,
+                      uint32_t size_2, uint32_t *buffer) {
+    uint32_t pos = 0, idx_1 = 0, idx_2 = 0;
+
+    if (0 == size_2) {
+        memcpy(buffer, set_1, size_1 * sizeof(uint32_t));
+        return size_1;
+    }
+    if (0 == size_1) {
+        memcpy(buffer, set_2, size_2 * sizeof(uint32_t));
+        return size_2;
+    }
+
+    uint32_t val_1 = set_1[idx_1], val_2 = set_2[idx_2];
+
+    while (true) {
+        if (val_1 < val_2) {
+            buffer[pos++] = val_1;
+            ++idx_1;
+            if (idx_1 >= size_1) break;
+            val_1 = set_1[idx_1];
+        } else if (val_2 < val_1) {
+            buffer[pos++] = val_2;
+            ++idx_2;
+            if (idx_2 >= size_2) break;
+            val_2 = set_2[idx_2];
+        } else {
+            buffer[pos++] = val_1;
+            ++idx_1;
+            ++idx_2;
+            if (idx_1 >= size_1 || idx_2 >= size_2) break;
+            val_1 = set_1[idx_1];
+            val_2 = set_2[idx_2];
+        }
+    }
+    if (idx_1 < size_1) {
+        const uint32_t n_elems = size_1 - idx_1;
+        memcpy(buffer + pos, set_1 + idx_1, n_elems * sizeof(uint32_t));
+        pos += n_elems;
+    } else if (idx_2 < size_2) {
+        const uint32_t n_elems = size_2 - idx_2;
+        memcpy(buffer + pos, set_2 + idx_2, n_elems * sizeof(uint32_t));
+        pos += n_elems;
+    }
+    return pos;
 }
 
 void demo() {
@@ -200,14 +274,52 @@ void skewedbigdemo(uint32_t size) {
     free(array2);
 }
 
+
+void benchmark(uint32_t size) {
+    assert(size % 4 == 0);
+    uint32_t * array1= malloc(size * sizeof(uint32_t));
+    uint32_t len1 = size;
+    uint32_t * array2= malloc(size * sizeof(uint32_t));
+    uint32_t len2 = size;
+    uint32_t last;
+    last = 0;
+    for(int k = 0; k < len1; ++k) {
+        last = last + (rand()&0x0F);
+        array1[k] =last;
+    }
+    last = 0;
+    for(int k = 0; k < len1; ++k) {
+        last = last + (rand()&0x0F);
+        array2[k] = last;
+    }
+    uint32_t * output = malloc(2 * size * sizeof(uint32_t));
+    uint64_t cycles_start = 0, cycles_final = 0;
+    RDTSC_START(cycles_start);
+    uint32_t lensse = sse_unite(array1, len1,array2, len2,output);
+    RDTSC_FINAL(cycles_final);
+    printf("SSE took %" PRIu64 " cycles\n",
+           cycles_final - cycles_start);
+
+    RDTSC_START(cycles_start);
+    uint32_t lenslow =  union_uint32(array1, len1,array2, len2,output);
+    RDTSC_FINAL(cycles_final);
+    printf("scalar took %" PRIu64 " cycles\n",
+           cycles_final - cycles_start);
+
+    free(output);
+    free(array1);
+    free(array2);
+}
+
 int main() {
-    for(int k = 4; k <1024; k+=4) {
+    for(int k = 4; k <32; k+=4) {
         printf("k=%d\n",k);
         for(int j = 0; j< 1000; ++j) {
-          bigdemo(k);
+            bigdemo(k);
         }
         for(int j = 0; j< 1000; ++j) {
-          skewedbigdemo(k);
+            skewedbigdemo(k);
         }
     }
+    benchmark(512);
 }
