0c27a6f1956f2e9504413c8c26fe3fea6ba9e4ae
Daniel Lemire <lemire@gmail.com>
2016-04-21 16:31:41 -0400

    Simple trick.

--- extra/simdunion.c
+++ extra/simdunion.c
@@ -40,11 +40,35 @@ static bool is_sorted(__m128i x) {
            && ( (uint32_t)_mm_extract_epi32(x,2)<=(uint32_t)_mm_extract_epi32(x,3));
 }
 
+static void array_print(const uint32_t * x, uint32_t len) {
+    for(uint32_t i = 0; i < len; i++) {
+        printf("%u,",x[i]);
+    }
+    printf("\n");
+}
+
+static bool array_is_sorted(const uint32_t * x, uint32_t len) {
+    for(uint32_t i = 1; i < len; i++) {
+        if(x[i] < x[i-1]) return false;
+    }
+    return true;
+}
+static bool array_is_strictly_sorted(const uint32_t * x, uint32_t len) {
+    for(uint32_t i = 1; i < len; i++) {
+        if(x[i] <= x[i-1]) return false;
+    }
+    return true;
+}
+
+
+static void print32(__m128i x) {
+    printf("%u %u %u %u \n", _mm_extract_epi32(x,0),_mm_extract_epi32(x,1),_mm_extract_epi32(x,2),_mm_extract_epi32(x,3));
+}
+
 // Inoue and Taura, SIMD- and Cache-Friendly Algorithm for Sorting an Array of Structures
 static void sse_merge(__m128i *vA, __m128i *vB, // input 1 & 2
                       __m128i *vMin, __m128i *vMax) { // output
-    __m128i vTmp; // temporary register
-
+    __m128i vTmp;
 //vA = 0 1 6 7 (hypothetical)
 //vB = 2 3 4 5 (hypothetical)
 
@@ -72,20 +96,59 @@ static void sse_merge(__m128i *vA, __m128i *vB, // input 1 & 2
 // vMax = 4 5 6 7
     *vMin = _mm_alignr_epi8(*vMin, *vMin, 4);
 // result: 0 1 2 3
-    //assert(is_sorted(*vMin));
-    //assert(is_sorted(*vMax));
-    //assert((uint32_t)_mm_extract_epi32(*vMin,3)<=(uint32_t)_mm_extract_epi32(*vMax,0));
+//print32(*vMin);
+    assert(is_sorted(*vMin));
+    assert(is_sorted(*vMax));
+    assert((uint32_t)_mm_extract_epi32(*vMin,3)<=(uint32_t)_mm_extract_epi32(*vMax,0));
 
 }
 
-static void print32(__m128i x) {
-    printf("%u %u %u %u \n", _mm_extract_epi32(x,0),_mm_extract_epi32(x,1),_mm_extract_epi32(x,2),_mm_extract_epi32(x,3));
+static uint8_t uniqshuf[]= {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf,
+                            0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,
+                            0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,
+                            0x4,0x5,0x6,0x7,0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0xc,0xd,0xe,0xf,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xFF,0xFF,0xFF,0xFF,
+                            0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0x8,0x9,0xa,0xb,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x8,0x9,0xa,0xb,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x4,0x5,0x6,0x7,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0x0,0x1,0x2,0x3,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                            0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+                           };
+
+
+static int store_unique(__m128i old,__m128i new, uint32_t *  output) {
+
+    __m128i vTmp = _mm_alignr_epi8(new, old, 16-4);
+    int M = _mm_movemask_epi8(_mm_cmpeq_epi32(vTmp,new));//_pdep_u32(,0x1111);
+    M=_pext_u32(M,0x1111);
+    int numberofnewvalues = 4 - _mm_popcnt_u32(M);
+    __m128i key =  _mm_lddqu_si128((const __m128i* )uniqshuf + M);
+    __m128i val =  _mm_shuffle_epi8(new,key);
+    _mm_storeu_si128((__m128i* )output,val);
+    return numberofnewvalues;
+}
+
+static uint32_t unique(uint32_t *  out, uint32_t len) {
+    uint32_t pos = 1;
+    for(uint32_t i = 1; i < len; ++i) {
+        if(out[i] != out[i-1]) {
+            out[pos++] = out[i];
+        }
+    }
+    return pos;
 }
 
 
-static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
-                          uint32_t *  __restrict__ array2, uint32_t length2, uint32_t *  __restrict__  output) {
+static uint32_t sse_unite_simple(uint32_t * __restrict__ array1, uint32_t length1 ,
+                                 uint32_t *  __restrict__ array2, uint32_t length2, uint32_t *  __restrict__  output) {
     __m128i vA, vB, V, vMin,vMax;
+    uint32_t * initoutput = output;
     assert(length1 % 4 == 0); // because I am lazy
     assert(length2 % 4 == 0); // because I am lazy
     assert(length1 != 0); // because I am lazy
@@ -94,8 +157,6 @@ static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
     uint32_t len2 = length2 / 4;
     uint32_t pos1 = 0;
     uint32_t pos2 = 0;
-    uint32_t outpos = 0;
-
     // we start the machine
     vA = _mm_lddqu_si128((const __m128i* )array1 + pos1);
     pos1++;
@@ -105,8 +166,8 @@ static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
     curA = (pos1 < len1) ? array1[4*pos1] : 0;
     curB = (pos2 < len2) ? array2[4*pos2] : 0;
     sse_merge(&vA,&vB,&vMin,&vMax);
-    _mm_storeu_si128((__m128i* )output + outpos,vMin);
-    outpos++;
+    _mm_storeu_si128((__m128i* )output,vMin);
+    output+= 4;
     while((pos1 < len1) && (pos2<len2)) {
         // code is not optimal, too much branching?
         if(curA <= curB) {
@@ -119,27 +180,90 @@ static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
             curB = (pos2 < len2) ? array2[4*pos2] : 0;
         }
         sse_merge(&V,&vMax,&vMin,&vMax);
-        _mm_storeu_si128((__m128i* )output + outpos,vMin);
-        outpos++;
+        _mm_storeu_si128((__m128i* )output,vMin);
+        output+= 4;
     }
     //
     while(pos1<len1) {
         V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
         pos1++;
         sse_merge(&V,&vMax,&vMin,&vMax);
-        _mm_storeu_si128((__m128i* )output + outpos,vMin);
-        outpos++;
+        _mm_storeu_si128((__m128i* )output,vMin);
+        output+= 4;
     }
     while(pos2<len2) {
         V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
         pos2++;
         sse_merge(&V,&vMax,&vMin,&vMax);
-        _mm_storeu_si128((__m128i* )output + outpos,vMin);
-        outpos++;
+        _mm_storeu_si128((__m128i* )output,vMin);
+        output+=4;
     }
-    _mm_storeu_si128((__m128i* )output + outpos,vMax);
-    outpos++;
-    return outpos * 4;
+    _mm_storeu_si128((__m128i* )output,vMax);
+    output+= 4;
+    uint32_t len = output-initoutput  /*outpos * 4*/;
+    return unique(initoutput, len);
+}
+
+static uint32_t sse_unite(uint32_t * __restrict__ array1, uint32_t length1 ,
+                          uint32_t *  __restrict__ array2, uint32_t length2, uint32_t *  __restrict__  output) {
+    __m128i vA, vB, V, vMin,vMax;
+    __m128i laststore;
+    uint32_t * initoutput = output;
+    assert(length1 % 4 == 0); // because I am lazy
+    assert(length2 % 4 == 0); // because I am lazy
+    assert(length1 != 0); // because I am lazy
+    assert(length2 != 0); // because I am lazy
+    uint32_t len1 = length1 / 4;
+    uint32_t len2 = length2 / 4;
+    uint32_t pos1 = 0;
+    uint32_t pos2 = 0;
+    // we start the machine
+    vA = _mm_lddqu_si128((const __m128i* )array1 + pos1);
+    pos1++;
+    vB = _mm_lddqu_si128((const __m128i* )array2 + pos2);
+    pos2++;
+    uint32_t curA, curB;
+    curA = (pos1 < len1) ? array1[4*pos1] : 0;
+    curB = (pos2 < len2) ? array2[4*pos2] : 0;
+    sse_merge(&vA,&vB,&vMin,&vMax);
+    laststore = _mm_set1_epi32(array1[0] < array2[0] ? array1[0] -1 : array2[0] -1);
+    output+= store_unique(laststore,vMin, output);
+    laststore=vMin;
+    ///outpos++;
+    while((pos1 < len1) && (pos2<len2)) {
+        // code is not optimal, too much branching?
+        if(curA <= curB) {
+            V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
+            pos1++;
+            curA = (pos1 < len1) ? array1[4*pos1] : 0;
+        } else {
+            V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
+            pos2++;
+            curB = (pos2 < len2) ? array2[4*pos2] : 0;
+        }
+        sse_merge(&V,&vMax,&vMin,&vMax);
+        output+= store_unique(laststore,vMin, output);
+        laststore = vMin;
+    }
+    while(pos1<len1) {
+        V = _mm_lddqu_si128((const __m128i* )array1 + pos1);
+        pos1++;
+        sse_merge(&V,&vMax,&vMin,&vMax);
+        output+= store_unique(laststore,vMin, output);
+        laststore = vMin;
+    }
+    while(pos2<len2) {
+        V = _mm_lddqu_si128((const __m128i* )array2 + pos2);
+        pos2++;
+        sse_merge(&V,&vMax,&vMin,&vMax);
+        output+= store_unique(laststore,vMin, output);
+        laststore = vMin;
+    }
+    output+= store_unique(laststore,vMax,output);
+
+    uint32_t len = output-initoutput  /*outpos * 4*/;
+
+    return len;
 }
 
 uint32_t union_uint32(const uint32_t *set_1, uint32_t size_1, const uint32_t *set_2,
@@ -208,67 +332,76 @@ void demo() {
 }
 
 
-
-void bigdemo(uint32_t size) {
-    assert(size % 4 == 0);
-    uint32_t * array1= malloc(size * sizeof(uint32_t));
-    uint32_t len1 = size;
-    uint32_t * array2= malloc(size * sizeof(uint32_t));
-    uint32_t len2 = size;
+void skewedbigdemo(uint32_t len1, uint32_t len2) {
+    assert(len1 % 4 == 0);
+    uint32_t * array1= malloc(len1 * sizeof(uint32_t));
+    assert(len2 % 4 == 0);
+    uint32_t * array2= malloc(len2 * sizeof(uint32_t));
     uint32_t last;
     last = 0;
     for(int k = 0; k < len1; ++k) {
-        last = last + (rand()&0x0F);
+        last = last + (rand()&0x0F) + 1;
         array1[k] =last;
     }
+    assert(array_is_strictly_sorted(array1,len1));
+
     last = 0;
-    for(int k = 0; k < len1; ++k) {
-        last = last + (rand()&0x0F);
+    for(int k = 0; k < len2; ++k) {
+        last = last + (rand()&0x0F) + 1;
         array2[k] = last;
     }
-    uint32_t * output = malloc(2 * size * sizeof(uint32_t));
-    uint32_t len = sse_unite(array1, len1,array2, len2,output);
-    for(int k = 0 ; k < len; ++k) {
-        if(k>0) {
-            if(output[k]<output[k-1]) {
-                printf("bug at k =%d out of %d ",k,len);
-                abort();
-            }
-        }
-    }
-    free(output);
-    free(array1);
-    free(array2);
-}
+    assert(array_is_strictly_sorted(array2,len2));
 
-
-void skewedbigdemo(uint32_t size) {
-    assert(size % 4 == 0);
-    uint32_t * array1= malloc(size * sizeof(uint32_t));
-    uint32_t len1 = size;
-    uint32_t * array2= malloc(2 * size * sizeof(uint32_t));
-    uint32_t len2 = 2 * size;
-    uint32_t last;
-    last = 0;
-    for(int k = 0; k < len1; ++k) {
-        last = last + (rand()&0x0F);
-        array1[k] =last;
-    }
-    last = 0;
-    for(int k = 0; k < len1; ++k) {
-        last = last + (rand()&0x0F);
-        array2[k] = last;
-    }
     uint32_t * output = malloc((len1 + len2) * sizeof(uint32_t));
     uint32_t len = sse_unite(array1, len1,array2, len2,output);
     for(int k = 0 ; k < len; ++k) {
         if(k>0) {
-            if(output[k]<output[k-1]) {
-                printf("bug at k =%d out of %d ",k,len);
+            if(output[k]<=output[k-1]) {
+                printf("bug at k =%d out of %d:  %u %u",k,len,output[k-1],output[k]);
                 abort();
             }
         }
     }
+    uint32_t * outputs = malloc((len1 + len2) * sizeof(uint32_t));
+    uint32_t lens = sse_unite_simple(array1, len1,array2, len2,outputs);
+    for(int k = 0 ; k < lens; ++k) {
+        if(k>0) {
+            if(outputs[k]<=outputs[k-1]) {
+                printf("bug at k =%d out of %d:  %u %u",k,lens,outputs[k-1],outputs[k]);
+                abort();
+            }
+        }
+    }
+
+
+    uint32_t * output2 = malloc((len1 + len2) * sizeof(uint32_t));
+
+    uint32_t lenslow =  union_uint32(array1, len1,array2, len2,output2);
+    if(len != lenslow) {
+        printf("bad length = %d %d \n", len,lenslow);
+        array_print(output,len);
+        array_print(output2,lenslow);
+        abort();
+    }
+    if(lens != lenslow) {
+        printf("bad s length = %d %d \n", lens,lenslow);
+        array_print(outputs,lens);
+        array_print(output2,lenslow);
+        abort();
+    }
+    for(int k = 0 ; k < len; ++k) {
+        if(output[k]!=output2[k]) {
+            printf("bug at k =%d out of %d:  %u %u",k,len,output[k],output2[k]);
+            abort();
+        }
+        if(outputs[k]!=output2[k]) {
+            printf("bug at k =%d out of %d:  %u %u",k,len,outputs[k],output2[k]);
+            abort();
+        }
+
+    }
+    free(output2);
+    free(outputs);
     free(output);
     free(array1);
     free(array2);
@@ -276,6 +409,7 @@ void skewedbigdemo(uint32_t size) {
 
 
 void benchmark(uint32_t size) {
+    printf("== benchmark size = %d \n", size);
     assert(size % 4 == 0);
     uint32_t * array1= malloc(size * sizeof(uint32_t));
     uint32_t len1 = size;
@@ -284,14 +418,18 @@ void benchmark(uint32_t size) {
     uint32_t last;
     last = 0;
     for(int k = 0; k < len1; ++k) {
-        last = last + (rand()&0x0F);
+        last = last + (rand()&0x0F) + 1;
         array1[k] =last;
     }
+    assert(array_is_strictly_sorted(array1,len1));
+
     last = 0;
-    for(int k = 0; k < len1; ++k) {
-        last = last + (rand()&0x0F);
+    for(int k = 0; k < len2; ++k) {
+        last = last + (rand()&0x0F) + 1;
         array2[k] = last;
     }
+    assert(array_is_strictly_sorted(array2,len2));
+
     uint32_t * output = malloc(2 * size * sizeof(uint32_t));
     uint64_t cycles_start = 0, cycles_final = 0;
     RDTSC_START(cycles_start);
@@ -299,12 +437,21 @@ void benchmark(uint32_t size) {
     RDTSC_FINAL(cycles_final);
     printf("SSE took %" PRIu64 " cycles\n",
            cycles_final - cycles_start);
+    RDTSC_START(cycles_start);
+    uint32_t slensse = sse_unite_simple(array1, len1,array2, len2,output);
+    RDTSC_FINAL(cycles_final);
+    printf("simple SSE took %" PRIu64 " cycles\n",
+           cycles_final - cycles_start);
 
     RDTSC_START(cycles_start);
     uint32_t lenslow =  union_uint32(array1, len1,array2, len2,output);
     RDTSC_FINAL(cycles_final);
     printf("scalar took %" PRIu64 " cycles\n",
            cycles_final - cycles_start);
+    printf("lensse = %d slensse = %d lenslow = %d \n", lensse, slensse,lenslow);
+    assert(lensse == lenslow);
+    assert(slensse == lenslow);
+
 
     free(output);
     free(array1);
@@ -315,11 +462,17 @@ int main() {
     for(int k = 4; k <32; k+=4) {
         printf("k=%d\n",k);
         for(int j = 0; j< 1000; ++j) {
-            bigdemo(k);
+            skewedbigdemo(k, k);
         }
         for(int j = 0; j< 1000; ++j) {
-            skewedbigdemo(k);
+            skewedbigdemo(k, 2*k);
+        }
+        for(int j = 0; j< 1000; ++j) {
+            skewedbigdemo(2*k, k);
         }
     }
     benchmark(512);
+    benchmark(1024);
+    benchmark(2048);
+
 }
