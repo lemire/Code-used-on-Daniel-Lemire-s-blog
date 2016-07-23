b93e1004dc44a2a12336acc5a4488eb63a25912f
Daniel Lemire <lemire@gmail.com>
2016-07-22 21:07:57 -0400

    Best results...

--- extra/bitsetrange/bitsetrange.c
+++ extra/bitsetrange/bitsetrange.c
@@ -6,8 +6,9 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
- __attribute__ ((noinline))
+__attribute__ ((noinline))
 static void bitset_set_range_noinline(uint64_t *bitmap, uint32_t start, uint32_t end) {
     if (start == end) return;
     uint32_t firstword = start / 64;
@@ -22,7 +23,7 @@ static void bitset_set_range_noinline(uint64_t *bitmap, uint32_t start, uint32_t
     bitmap[endword] |= (~UINT64_C(0)) >> ((-end) % 64);
 }
 
- __attribute__((always_inline)) 
+__attribute__((always_inline))
 static inline void bitset_set_range(uint64_t *bitmap, uint32_t start, uint32_t end) {
     if (start == end) return;
     uint32_t firstword = start / 64;
@@ -37,36 +38,21 @@ static inline void bitset_set_range(uint64_t *bitmap, uint32_t start, uint32_t e
     bitmap[endword] |= (~UINT64_C(0)) >> ((-end) % 64);
 }
 
-
- __attribute__((always_inline)) 
-static inline void bitset_set_range_inclusive(uint64_t *bitmap, uint16_t start, uint16_t end) {
-    if (start == end) return;
+__attribute__((always_inline))
+static inline void bitset_set_range_opti(uint64_t *bitmap, uint32_t start, uint32_t lenminusone) {
     uint32_t firstword = start / 64;
-    uint32_t endword = end  / 64;
+    uint32_t endword = (start + lenminusone) / 64;
     if (firstword == endword) {
-        bitmap[firstword] |= ((~UINT64_C(0)) << (start % 64)) &
-                             ((~UINT64_C(0)) >> ((-end) % 64));
+
+        bitmap[firstword] |=
+            ((~UINT64_C(0)) >> ((63-lenminusone)  ))<< (start %64);
         return;
     }
     bitmap[firstword] |= (~UINT64_C(0)) << (start % 64);
     for (uint32_t i = firstword + 1; i < endword; i++) bitmap[i] = ~UINT64_C(0);
-    bitmap[endword] |= (~UINT64_C(0)) >> ((-end) % 64);
+    bitmap[endword] |= (~UINT64_C(0)) >> ((-start - lenminusone - 1) % 64);
 }
 
- __attribute__((always_inline)) 
-static inline void bitset_set_range_bug(uint64_t *bitmap, uint32_t start, uint32_t end) {
-    if (start == end) return;
-    uint32_t firstword = start / 64;
-    uint32_t endword = (end - 1) / 64;
-    if (firstword == endword) {
-        bitmap[firstword] |= ((~UINT64_C(0)) << (start)) &
-                             ((~UINT64_C(0)) >> ((-end)));
-        return;
-    }
-    bitmap[firstword] |= (~UINT64_C(0)) << (start);
-    for (uint32_t i = firstword + 1; i < endword; i++) bitmap[i] = ~UINT64_C(0);
-    bitmap[endword] |= (~UINT64_C(0)) >> ((-end) );
-}
 
 /* struct rle16_s - run length pair
  *
@@ -85,6 +71,7 @@ typedef struct rle16_s rle16_t;
 
 
 
+__attribute__ ((noinline))
 void run_bitset_container_union(uint64_t *src_1,
                                 const rle16_t *src_2,
                                 size_t n_runs) {
@@ -95,38 +82,107 @@ void run_bitset_container_union(uint64_t *src_1,
     }
 }
 
-void run_bitset_container_union_inclusive(uint64_t *src_1,
-                                const rle16_t *src_2,
-                                size_t n_runs) {
+__attribute__ ((noinline))
+void run_bitset_container_union_opti(uint64_t *src_1,
+                                      const rle16_t *src_2,
+                                      size_t n_runs) {
     for (int32_t rlepos = 0; rlepos < n_runs; ++rlepos) {
         rle16_t rle = src_2[rlepos];
-        bitset_set_range_inclusive(src_1, rle.value,
-                         rle.value + rle.length);
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+    }
+}
+
+__attribute__ ((noinline))
+void run_bitset_container_union_optiu(uint64_t *src_1,
+                                       const rle16_t *src_2,
+                                       size_t n_runs) {
+    rle16_t rle;
+    int32_t rlepos = 0;
+    for (; rlepos + 1 < n_runs; rlepos += 2) {
+        rle = src_2[rlepos];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+        rle = src_2[rlepos + 1];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+    }
+    for (; rlepos < n_runs; ++rlepos) {
+        rle16_t rle = src_2[rlepos];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+    }
+}
+
+__attribute__ ((noinline))
+void run_bitset_container_union_optiuu(uint64_t *src_1,
+                                        const rle16_t *src_2,
+                                        size_t n_runs) {
+    rle16_t rle;
+    int32_t rlepos = 0;
+    for (; rlepos + 3 < n_runs; rlepos += 4) {
+        rle = src_2[rlepos];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+        rle = src_2[rlepos + 1];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+        rle = src_2[rlepos + 2];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+        rle = src_2[rlepos + 3];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
+    }
+    for (; rlepos < n_runs; ++rlepos) {
+        rle16_t rle = src_2[rlepos];
+        bitset_set_range_opti(src_1, rle.value,
+                               rle.length);
     }
 }
 
 
+__attribute__ ((noinline))
+void run_bitset_container_union_uu(uint64_t *src_1,
+                                        const rle16_t *src_2,
+                                        size_t n_runs) {
+    rle16_t rle;
+    int32_t rlepos = 0;
+    for (; rlepos + 3 < n_runs; rlepos += 4) {
+        rle = src_2[rlepos];
+        bitset_set_range(src_1, rle.value,
+                               rle.value + rle.length + UINT32_C(1));
+        rle = src_2[rlepos + 1];
+        bitset_set_range(src_1, rle.value,
+                               rle.value + rle.length + UINT32_C(1));
+        rle = src_2[rlepos + 2];
+        bitset_set_range(src_1, rle.value,
+                               rle.value + rle.length + UINT32_C(1));
+        rle = src_2[rlepos + 3];
+        bitset_set_range(src_1, rle.value,
+                               rle.value + rle.length + UINT32_C(1));
+    }
+    for (; rlepos < n_runs; ++rlepos) {
+        rle16_t rle = src_2[rlepos];
+        bitset_set_range(src_1, rle.value,
+                               rle.value + rle.length + UINT32_C(1));
+    }
+}
+
+
+
+__attribute__ ((noinline))
 void run_bitset_container_union_noinline(uint64_t *src_1,
-                                const rle16_t *src_2,
-                                size_t n_runs) {
+        const rle16_t *src_2,
+        size_t n_runs) {
     for (int32_t rlepos = 0; rlepos < n_runs; ++rlepos) {
         rle16_t rle = src_2[rlepos];
         bitset_set_range_noinline(src_1, rle.value,
-                         rle.value + rle.length + UINT32_C(1));
+                                  rle.value + rle.length + UINT32_C(1));
     }
 }
 
 
-
-void run_bitset_container_union_bug(uint64_t *src_1,
-                                const rle16_t *src_2,
-                                size_t n_runs) {
-    for (int32_t rlepos = 0; rlepos < n_runs; ++rlepos) {
-        rle16_t rle = src_2[rlepos];
-        bitset_set_range_bug(src_1, rle.value,
-                         rle.value + rle.length + UINT32_C(1));
-    }
-}
 
 
 #define RDTSC_START(cycles)                                                   \
@@ -182,27 +238,52 @@ void run_bitset_container_union_bug(uint64_t *src_1,
 
 
 void demo() {
-  uint64_t *bitmap = (uint64_t *) malloc((1<<16)/8);
-  memset(bitmap,0,(1<<16)/8);
-  size_t n_runs = 1024;
-  rle16_t * runs = (rle16_t *) malloc ( n_runs * sizeof(rle16_t));
-  int base = 0;
-  for(size_t k = 0; k < n_runs; ++k) {
-    int len = (rand() % 71) + 1;
-    runs[k].value = base;
-    runs[k].length = len - 1;
-    base += len + (rand() % 128);
-  }
-  int repeat = 5000;
-  BEST_TIME(run_bitset_container_union(bitmap,runs,n_runs),repeat,n_runs);
-  BEST_TIME(run_bitset_container_union_inclusive(bitmap,runs,n_runs),repeat,n_runs);
-  BEST_TIME(run_bitset_container_union_noinline(bitmap,runs,n_runs),repeat,n_runs);
-  BEST_TIME(run_bitset_container_union_bug(bitmap,runs,n_runs),repeat,n_runs);
+    size_t bsize = (1<<16)/8;
+    uint64_t *bitmap = (uint64_t *) malloc(bsize);
+    memset(bitmap,0,bsize);
+    size_t n_runs = 1024;
+    rle16_t * runs = (rle16_t *) malloc ( n_runs * sizeof(rle16_t));
+    int base = 0;
+    for(size_t k = 0; k < n_runs; ++k) {
+        int len = (rand() % 71) + 1;
+        runs[k].value = base;
+        runs[k].length = len - 1;
+        base += len + (rand() % 128);
+    }
+    uint64_t *bitmaptmp1 = (uint64_t *) malloc(bsize);
+    uint64_t *bitmaptmp2 = (uint64_t *) malloc(bsize);
+    memset(bitmaptmp1,0,bsize);
+    memset(bitmaptmp2,0,bsize);
+    run_bitset_container_union(bitmaptmp1,runs,n_runs);
+    run_bitset_container_union_noinline(bitmaptmp2,runs,n_runs);
+    assert(memcmp(bitmaptmp1, bitmaptmp2, bsize) == 0);
+    memset(bitmaptmp2,0,bsize);
+    run_bitset_container_union_opti(bitmaptmp2,runs,n_runs);
+    assert(memcmp(bitmaptmp1, bitmaptmp2, bsize) == 0);
+    memset(bitmaptmp2,0,bsize);
+    run_bitset_container_union_optiu(bitmaptmp2,runs,n_runs);
+    assert(memcmp(bitmaptmp1, bitmaptmp2, bsize) == 0);
+    memset(bitmaptmp2,0,bsize);
+    run_bitset_container_union_optiuu(bitmaptmp2,runs,n_runs);
+    assert(memcmp(bitmaptmp1, bitmaptmp2, bsize) == 0);
+    free(bitmaptmp1);
+    free(bitmaptmp2);
+    int repeat = 5000;
+    BEST_TIME(run_bitset_container_union(bitmap,runs,n_runs),repeat,n_runs);
+    BEST_TIME(run_bitset_container_union_uu(bitmap,runs,n_runs),repeat,n_runs);
+    BEST_TIME(run_bitset_container_union_noinline(bitmap,runs,n_runs),repeat,n_runs);
+    BEST_TIME(run_bitset_container_union_opti(bitmap,runs,n_runs),repeat,n_runs);
+    BEST_TIME(run_bitset_container_union_optiu(bitmap,runs,n_runs),repeat,n_runs);
+    BEST_TIME(run_bitset_container_union_optiuu(bitmap,runs,n_runs),repeat,n_runs);
 
-  free(bitmap);
-  free(runs);
+
+
+
+
+    free(bitmap);
+    free(runs);
 }
 
 int main() {
-  demo();
+    demo();
 }
