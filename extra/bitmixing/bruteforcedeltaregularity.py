1710916849dbbcbae4af3619b267488b876054bc
Daniel Lemire <lemire@gmail.com>
2016-08-08 16:24:27 -0400

    Saving...

--- extra/bitmixing/bruteforcedeltaregularity.py
+++ extra/bitmixing/bruteforcedeltaregularity.py
@@ -1,33 +1,38 @@
 # no 4->4 bit mixing function has perfect avalanche and invertibility
 # the functions go from {0,1,2,3} to {0,1,2,3}
 
+w=3
+
+basearr = [i for i in range(1<<w)]
+
+import itertools
+
 def isinvertible(arr):
     return len(set(arr))== len(arr)
 
 def deltareg(arr):
-    count = [[0 for i in range(2)] for j in range(2)]
     dreg = True
-    minsize = 4
+    minsize = (1<<w)
     bady = 0
-    for y in range(4):
+    maps = []
+    for y in range((1<<w)):
         if(y == 0) : continue
         d = set()
-        for x in range(4) :
-            diff = (arr[x]+arr[(x-y)%4]) % 4#(arr[x] - arr[(x+y)%4]) % 4
+        for x in range((1<<w)) :
+            diff = (arr[x]-arr[(x-y)%(1<<w)]) % (1<<w)
             d.add(diff)
+        maps.append([y,d])
         if(minsize > len(set(d))):
             minsize = len(set(d))
             bady = y
         if (len(set(d))<> len(arr)) :
             dreg = False
-    print(arr,minsize,bady)
-    return dreg
+    return minsize
 
-for i1 in range(4):
-    for i2 in range(4):
-        for i3 in range(4):
-            for i4 in range(4):
-                arr = [i1,i2,i3,i4]
-                if(isinvertible(arr)):
-                  if(deltareg(arr)):
-                    for i in range(100): print "success"
+bestdeltareg = 0
+for perm in itertools.permutations(basearr):
+    tdr = deltareg(perm)
+    if(bestdeltareg < tdr):
+        bestdeltareg = tdr
+        print("best deltareg = ", tdr, " out of ", 1<<w)
+        if(tdr == 1 << w):  print "success"
