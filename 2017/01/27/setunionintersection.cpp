004da4ce9acfaa6dad091fbba00c616c1b7a9533
Daniel Lemire <lemire@gmail.com>
2017-01-27 22:46:26 -0500

    Updating with treeset

--- 2017/01/27/setunionintersection.cpp
+++ 2017/01/27/setunionintersection.cpp
@@ -11,9 +11,11 @@
 #include <string.h>
 #include <unordered_set>
 #include <vector>
-#include <x86intrin.h>
+#include <set>
+
 
 typedef std::unordered_set<uint32_t> hashset;
+typedef std::set<uint32_t> treeset;
 typedef std::vector<uint32_t> vector;
 
 static void setintersection(hashset &h1, hashset &h2, hashset &answer) {
@@ -45,28 +47,49 @@ static void arrayintersection(vector &v1, vector &v2, vector &v) {
   std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(),
                         std::back_inserter(v));
 }
+
+static void treesetunion(treeset &v1, treeset &v2, treeset &v) {
+  v.clear();
+  std::set_union(v1.begin(), v1.end(), v2.begin(), v2.end(),
+                        std::inserter(v,v.begin()));
+}
+
+static void treesetintersection(treeset &v1, treeset &v2, treeset &v) {
+  v.clear();
+  std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(),
+                        std::inserter(v,v.begin()));
+}
+
 int demo(const int N) {
   printf("%d\n", N);
   hashset s1;
   hashset s2;
   vector v1;
   vector v2;
+  treeset t1;
+  treeset t2;
   for (int z = 0; z < N; ++z) {
     s1.insert(3 * z + 1);
     v1.push_back(3 * z + 1);
+    t1.insert(3 * z + 1);
     s2.insert(N - z);
     v2.push_back(N - z);
+    t2.insert(N - z);
   }
   std::sort(v1.begin(), v1.end());
   std::sort(v2.begin(), v2.end());
   const int repeat = 5;
   hashset out;
   vector v;
+  treeset t;
   BEST_TIME_NOCHECK(setintersection(s1, s2, out), , repeat, 2 * N, true);
   BEST_TIME_NOCHECK(setunion(s1, s2, out), , repeat, 2 * N, true);
   BEST_TIME_NOCHECK(arrayintersection(v1, v2, v), , repeat, 2 * N, true);
   BEST_TIME_NOCHECK(arrayunion(v1, v2, v), , repeat, 2 * N, true);
-  return out.size() + v.size();
+  BEST_TIME_NOCHECK(treesetintersection(t1, t2, t), , repeat, 2 * N, true);
+  BEST_TIME_NOCHECK(treesetunion(t1, t2, t), , repeat, 2 * N, true);
+
+  return out.size() + v.size() + t.size();
 }
 
 int main() {
