c6438947a533067126e78d4d3c8081bc8ae5e124
Daniel Lemire <lemire@gmail.com>
2017-01-30 16:12:33 -0500

    Saving...

--- 2017/01/30/linearprobing/Sources/main.swift
+++ 2017/01/30/linearprobing/Sources/main.swift
@@ -4,6 +4,7 @@ import Swimsuit;
 for power in 20...30 {
   let size = 1 << power
   print("trial \(size)")
+  // could use an  IndexSet, but we want to illustrate a generic problem (not specific to Int)
   var d = Set<Int>()
   let nanobuild = Swimsuit.nanotime() {
      for i in 1...size {
@@ -11,7 +12,7 @@ for power in 20...30 {
      }
   }
   print("time to build set: ", Double(nanobuild) / Double(size) , " ns/element")
-  var dcopy = Set<Int>()
+  var dcopy = Set<Int>() // could make much faster by specify minimumCapacity 
   dcopy.insert(1000)
   let nanocopy = Swimsuit.nanotime() {
      for i in d {
