a9babec38b1d897839febf9817834c2361ed3a64
Daniel Lemire <lemire@gmail.com>
2017-01-31 15:36:39 -0500

    Saving...

--- extra/bitset/c/arraybitsetintersection.c
+++ extra/bitset/c/arraybitsetintersection.c
@@ -47,6 +47,14 @@ static inline uint64_t neg_asm_bitset_contains(const uint64_t *bitset,
     ASM_INPLACESHIFT_RIGHT(word, p);
     return word & 1;
 }
+static inline bool bool_neg_asm_bitset_contains(const uint64_t *bitset,
+                                        uint16_t pos) {
+    uint64_t word = bitset[pos >> 6];
+    const uint64_t p = pos;
+    ASM_INPLACESHIFT_RIGHT(word, p);
+    return word & 1;
+}
+
 
 
 
@@ -90,6 +98,20 @@ size_t asm_branchless_array_bitset_intersection(const uint16_t *src_1, size_t or
 }
 
 
+size_t bool_asm_branchless_array_bitset_intersection(const uint16_t *src_1, size_t origcard,
+                                         const uint64_t *src_2,
+                                         uint16_t *dst) {
+    size_t newcard = 0;
+    for (int i = 0; i < origcard; ++i) {
+        uint16_t key = src_1[i];
+        dst[newcard] = key;
+        newcard += bool_neg_asm_bitset_contains(src_2, key);
+   }
+    return newcard;
+}
+
+
+
 
 
 size_t asm_array_bitset_intersection(const uint16_t *src_1, size_t origcard,
@@ -126,8 +148,17 @@ int main() {
   BEST_TIME_NOCHECK(array_bitset_intersection(array,N,bitset,array), randomize(array,N), repeat, N, true);
   BEST_TIME_NOCHECK(branchless_array_bitset_intersection(array,N,bitset,array), randomize(array,N), repeat, N, true);
   BEST_TIME_NOCHECK(asm_branchless_array_bitset_intersection(array,N,bitset,array), randomize(array,N), repeat, N, true);
+  BEST_TIME_NOCHECK(bool_asm_branchless_array_bitset_intersection(array,N,bitset,array), randomize(array,N), repeat, N, true);
   BEST_TIME_NOCHECK(asm_array_bitset_intersection(array,N,bitset,array), randomize(array,N), repeat, N, true);
-
+  printf("going more predictible\n");
+  for(size_t k = 0; k < 1024; k++) {
+    bitset[k] = UINT64_C(0xaa55ffffffff55aa); // mostly just ones
+  }
+  BEST_TIME_NOCHECK(array_bitset_intersection(array,N,bitset,array), , repeat, N, true);
+  BEST_TIME_NOCHECK(branchless_array_bitset_intersection(array,N,bitset,array), , repeat, N, true);
+  BEST_TIME_NOCHECK(asm_branchless_array_bitset_intersection(array,N,bitset,array), , repeat, N, true);
+  BEST_TIME_NOCHECK(bool_asm_branchless_array_bitset_intersection(array,N,bitset,array), , repeat, N, true);
+  BEST_TIME_NOCHECK(asm_array_bitset_intersection(array,N,bitset,array), , repeat, N, true);
   free(bitset);
   free(array);
 }
