00ec462e95b5900532612cff5b990d2123125c0a
Daniel Lemire <lemire@gmail.com>
2017-02-14 11:41:04 -0500

    Saving...

--- 2017/02/14/newlines.c
+++ 2017/02/14/newlines.c
@@ -70,6 +70,47 @@ size_t avxcount(char * buffer, size_t size) {
     return answer;
 }
 
+size_t avxcountuu(char * buffer, size_t size) {
+    size_t answer = 0;
+    __m256i cnt = _mm256_setzero_si256();
+    __m256i newline = _mm256_set1_epi8('\n');
+    __m256i mask1 = _mm256_set1_epi8(1);
+    size_t i = 0;
+    uint8_t tmpbuffer[sizeof(__m256i)];
+    while( i + 32 <= size ) {
+        size_t remaining = size - i;
+        size_t howmanytimes =  remaining / 32;
+        if(howmanytimes > 256) howmanytimes = 256;
+        const __m256i * buf = (const __m256i *) (buffer + i);
+        size_t j = 0;
+        for (; j + 1 <  howmanytimes; j+= 2) {
+            __m256i newdata1 = _mm256_lddqu_si256(buf + j);
+            __m256i newdata2 = _mm256_lddqu_si256(buf + j + 1);
+            __m256i cmp1 = _mm256_cmpeq_epi8(newline,newdata1);
+            __m256i cmp2 = _mm256_cmpeq_epi8(newline,newdata2);
+            cmp1 = _mm256_and_si256(mask1, cmp1);
+            cmp2 = _mm256_and_si256(mask1, cmp2);
+            __m256i cnt1 = _mm256_add_epi8(cmp1,cmp2);
+            cnt = _mm256_add_epi8(cnt,cnt1);
+        }
+        for (; j <  howmanytimes; j++) {
+            __m256i newdata = _mm256_lddqu_si256(buf + j);
+            __m256i cmp = _mm256_cmpeq_epi8(newline,newdata);
+            cmp = _mm256_and_si256(mask1, cmp);
+            cnt = _mm256_add_epi8(cnt,cmp);
+        }
+        i += howmanytimes * 32;
+        _mm256_storeu_si256((__m256i *) tmpbuffer,cnt);
+        for(int k = 0; k < sizeof(__m256i); ++k) answer += tmpbuffer[k];
+        cnt = _mm256_setzero_si256();
+    }
+    for(; i < size; i++)
+        if(buffer[i] == '\n') answer ++;
+    return answer;
+}
+
+
+
 size_t avxcountu(char * buffer, size_t size) {
     size_t answer = 0;
     __m256i cnt = _mm256_setzero_si256();
@@ -127,5 +168,6 @@ int main() {
     BEST_TIME(basiccount(buffer,N),tc, , repeat, N, true);
     BEST_TIME(avxcount(buffer,N),tc, , repeat, N, true);
     BEST_TIME(avxcountu(buffer,N),tc, , repeat, N, true);
+    BEST_TIME(avxcountuu(buffer,N),tc, , repeat, N, true);
     free(buffer);
 }
