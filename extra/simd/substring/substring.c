430650870bcf0288555e503fea8ddc35795c6ecc
Daniel Lemire <lemire@gmail.com>
2017-05-09 14:20:55 -0400

    Implemented a new scheme.

--- extra/simd/substring/substring.c
+++ extra/simd/substring/substring.c
@@ -1,3 +1,5 @@
+//http://www.dmi.unict.it/~faro/smart/
+
 // gcc -O3 -o substring substring.c -march=native
 #include <assert.h>
 #include <stdlib.h>
@@ -100,14 +102,9 @@ int sse4search(char * hay, int size, char *needle, int needlesize) {
     int r = _mm_cvtsi128_si32(x);
     if(r != 0) {
       int offset = __builtin_ctz(r);
-                  if (memcmp(hay + i + offset + 1, needle + 1, needlesize - 2) == 0) {
-                      return i + offset;
-                  }
-
-//      v = _mm_loadu_si128((const __m128i *)(hay + i + offset));
-  //    x = _mm_cmpistrm(n,v,_SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ORDERED | _SIDD_BIT_MASK);
-    //  r = _mm_cvtsi128_si32(x);
-      //if((r & 1 ) != 0) return i + offset;
+      if (memcmp(hay + i + offset + 1, needle + 1, needlesize - 2) == 0) {
+          return i + offset;
+      }
     }
   }
   return size;
@@ -129,6 +126,68 @@ int sse4searchp2(char * hay, int size, char *needle, int needlesize) {
     }
   }
   return size;
+}
+
+// from: https://arxiv.org/pdf/1612.01506.pdf
+size_t naiveavx2(const char* s, size_t text_size, const char* needle, size_t needle_size) {
+      assert(text_size % 32 == 0);
+      // todo: fix it so we can handle variable-length inputs and
+      // can catch matches at the end of the data.
+      for (size_t i = 0; i < text_size - needle_size; i += 32) {
+        uint32_t found = 0xFFFFFFFF; // 32 1-bits
+        size_t j = 0;
+        for (; (j < needle_size) && (found != 0) ; ++j) {
+          __m256i textvector = _mm256_loadu_si256((const __m256i *)(s + i + j));
+          __m256i needlevector = _mm256_set1_epi8(needle[j]);
+          uint32_t bitmask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(textvector, needlevector));
+          found = found & bitmask;
+        }
+        if(found != 0) {
+          // got a match... maybe
+          return i + __builtin_ctz(found);
+        }
+      }
+      return text_size;
+}
+
+// from: https://arxiv.org/pdf/1612.01506.pdf
+size_t naiveavx2_unrolled(const char* s, size_t text_size, const char* needle, size_t needle_size) {
+      assert(text_size % 32 == 0);
+      // todo: fix it so we can handle variable-length inputs and
+      // can catch matches at the end of the data.
+      for (size_t i = 0; i < text_size - needle_size; i += 32) {
+        uint32_t found = 0xFFFFFFFF; // 32 1-bits
+        size_t j = 0;
+        for (; (j + 3 < needle_size) && (found != 0)  ; j += 4) {
+          __m256i textvector1 = _mm256_loadu_si256((const __m256i *)(s + i + j));
+          __m256i needlevector1 = _mm256_set1_epi8(needle[j]);
+          __m256i textvector2 = _mm256_loadu_si256((const __m256i *)(s + i + j + 1));
+          __m256i needlevector2 = _mm256_set1_epi8(needle[j + 1]);
+          __m256i cmp1 = _mm256_cmpeq_epi8(textvector1, needlevector1);
+          __m256i cmp2 = _mm256_cmpeq_epi8(textvector2, needlevector2);
+          __m256i textvector3 = _mm256_loadu_si256((const __m256i *)(s + i + j + 2));
+          __m256i needlevector3 = _mm256_set1_epi8(needle[j + 2]);
+          __m256i textvector4 = _mm256_loadu_si256((const __m256i *)(s + i + j + 3));
+          __m256i needlevector4 = _mm256_set1_epi8(needle[j + 3]);
+          __m256i cmp3 = _mm256_cmpeq_epi8(textvector3, needlevector3);
+          __m256i cmp4 = _mm256_cmpeq_epi8(textvector4, needlevector4);
+          __m256i cmp12 = _mm256_and_si256(cmp1,cmp2);
+          __m256i cmp34 = _mm256_and_si256(cmp3,cmp4);
+          uint32_t bitmask = _mm256_movemask_epi8(_mm256_and_si256(cmp12,cmp34));
+          found = found & bitmask;
+        }
+        for (; (j < needle_size) && (found != 0) ; ++j) {
+          __m256i textvector = _mm256_loadu_si256((const __m256i *)(s + i + j));
+          __m256i needlevector = _mm256_set1_epi8(needle[j]);
+          uint32_t bitmask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(textvector, needlevector));
+          found = found & bitmask;
+        }
+        if(found != 0) {
+          // got a match... maybe
+          return i + __builtin_ctz(found);
+        }
+      }
+      return text_size;
 }
 
 // implements scheme described in http://0x80.pl/articles/simd-friendly-karp-rabin.html
@@ -161,7 +220,7 @@ size_t  avx2_strstr_anysize(const char* s, size_t n, const char* needle, size_t 
 }
 
 void demo() {
-  const int N = 2048;
+  const int N = 64;
   char *hay = create_random_string(N);
   char needle[16 + 1];
   int expected;
@@ -177,6 +236,11 @@ void demo() {
                         repeat, N);
   BEST_TIME(avx2_strstr_anysize(hay,N,needle,16) , ,expected,
                         repeat, N);
+  BEST_TIME(naiveavx2(hay,N,needle,16) , ,expected,
+                        repeat, N);
+  BEST_TIME(naiveavx2_unrolled(hay,N,needle,16) , ,expected,
+                        repeat, N);
+
   free(hay);
 }
 int main() {
