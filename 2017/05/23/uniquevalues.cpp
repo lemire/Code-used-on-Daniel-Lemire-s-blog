56ef96c3536b76ec9b47fc8f0bef41763a0223af
Daniel Lemire <lemire@gmail.com>
2017-05-23 11:26:20 -0400

    Saving...

--- 2017/05/23/uniquevalues.cpp
+++ 2017/05/23/uniquevalues.cpp
@@ -1,4 +1,4 @@
-
+// g++ -O3 -o uniquevalues uniquevalues.cpp  -std=c++11 -Wall -Wextra
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
@@ -22,11 +22,10 @@ void demo(size_t N) {
     std::cout << "N = " << N <<std::endl;
     uint64_t * values = new uint64_t[N];
     for(size_t i = 0; i < N; ++i) {
-      values[i] = rand();
+      values[i] = ((uint16_t)rand()) | ((uint32_t)rand() << 16) | ((uint64_t)rand()<<32) |((uint64_t)rand()<<48);
     }
-    for(size_t i = rand() % N ; i < N; ++i) {
-      values[i] = values[0]; 
-    }
+    values[N/2] = values[0];
+    values[N-1] = values[1];
     size_t expected = distinct_count_hash(values,N);
     std::cout <<" expected = "<<expected << std::endl;
     const int repeat = 1;
@@ -35,5 +34,5 @@ void demo(size_t N) {
     delete[] values;
 }
 int main() {
-  for(size_t N = 100; N < 100000000; N*=10) demo(N);
+  for(size_t N = 10; N < 100000000; N*=10) demo(N);
 }
