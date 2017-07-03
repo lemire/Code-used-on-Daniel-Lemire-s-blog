251491542ecba1a4dda0a6f3132e2441cde1abb9
Daniel Lemire <lemire@gmail.com>
2017-07-03 14:24:09 -0400

    Preparing blog post

---
+++ 2017/07/03/despacebenchmark.c
@@ -0,0 +1,181 @@
+// gcc -std=c99 -O3 -o despacebenchmark despacebenchmark.c
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <string.h>
+#include "despacer.h"
+
+
+const char * nameofcycles = "ns";
+
+
+#include <time.h>
+
+#define RDTSC_START(cycles) \
+    do {                    \
+       struct timeval tv; \
+       gettimeofday(&tv, NULL); \
+       cycles =  (uint64_t)tv.tv_sec * 1000000000 + tv.tv_usec*1000; \
+    } while (0)
+
+#define RDTSC_FINAL(cycles) \
+    do {                    \
+        RDTSC_START(cycles);   \
+    } while (0)
+
+
+
+#define BEST_TIME_NOCHECK(test, pre, repeat, size)                                  \
+  do {                                                                         \
+    printf("%-40s: ", #test);                                                     \
+    fflush(NULL);                                                              \
+    uint64_t cycles_start, cycles_final, cycles_diff;                          \
+    uint64_t min_diff = (uint64_t)-1;                                          \
+    for (int i = 0; i < repeat; i++) {                                         \
+      pre;                                                                     \
+      __asm volatile("" ::: /* pretend to clobber */ "memory");                \
+      RDTSC_START(cycles_start);                                               \
+      test;                                                       \
+      RDTSC_FINAL(cycles_final);                                               \
+      cycles_diff = (cycles_final - cycles_start);                             \
+      if (cycles_diff < min_diff)                                              \
+        min_diff = cycles_diff;                                                \
+    }                                                                          \
+    uint64_t S = (uint64_t)size;                                               \
+    float cycle_per_op = (min_diff) / (float)S;                                \
+    printf(" %.3f %s per operation", cycle_per_op, nameofcycles);                        \
+    printf("\n");                                                              \
+    fflush(NULL);                                                              \
+  } while (0)
+
+#define BEST_TIME_NOCHECK_NOPRE(test, repeat, size)                                  \
+  do {                                                                         \
+    printf("%-40s: ", #test);                                                     \
+    fflush(NULL);                                                              \
+    uint64_t cycles_start, cycles_final, cycles_diff;                          \
+    uint64_t min_diff = (uint64_t)-1;                                          \
+    RDTSC_START(cycles_start);                                               \
+    for (int i = 0; i < repeat; i++) {                                         \
+      __asm volatile("" ::: /* pretend to clobber */ "memory");                \
+       test;                                                       \
+    }                                                                          \
+    RDTSC_FINAL(cycles_final);                                               \
+    cycles_diff = (cycles_final - cycles_start);                             \
+    min_diff = cycles_diff;                                                \
+    uint64_t S = (uint64_t)size;                                               \
+    float cycle_per_op = (min_diff) / ((float)S * repeat);                                \
+    printf(" %.3f %s per operation", cycle_per_op, nameofcycles);                        \
+    printf("\n");                                                              \
+    fflush(NULL);                                                              \
+  } while (0)
+
+
+
+
+#define BEST_TIME_NOPRE(test, answer,  repeat, size)                                  \
+  do {                                                                         \
+    printf("%-40s: ", #test);                                                     \
+    fflush(NULL);                                                              \
+    uint64_t cycles_start, cycles_final, cycles_diff;                          \
+    uint64_t min_diff = (uint64_t)-1;                                          \
+    int wrong_answer = 0;                                                      \
+    RDTSC_START(cycles_start);                                               \
+    for (int i = 0; i < repeat; i++) {                                         \
+      if ((test != answer) && (wrong_answer == 0)) {                                                    \
+        printf("ERROR: returned: %d, expected: %d\n", (int) test, (int)answer); \
+        wrong_answer = 1;                                                      \
+      }                                                                          \
+    }                                                                          \
+    RDTSC_FINAL(cycles_final);                                               \
+    cycles_diff = (cycles_final - cycles_start);                             \
+    min_diff = cycles_diff;                                                \
+    uint64_t S = (uint64_t)size;                                               \
+    float cycle_per_op = (min_diff) / ((float)S * repeat);                                \
+    printf(" %.3f %s per operation", cycle_per_op, nameofcycles);                        \
+    if (wrong_answer)                                                          \
+      printf(" [ERROR]");                                                      \
+    printf("\n");                                                              \
+    fflush(NULL);                                                              \
+  } while (0)
+
+
+
+#define BEST_TIME(test, answer, pre, repeat, size)                                  \
+  do {                                                                         \
+    printf("%-40s: ", #test);                                                     \
+    fflush(NULL);                                                              \
+    uint64_t cycles_start, cycles_final, cycles_diff;                          \
+    uint64_t min_diff = (uint64_t)-1;                                          \
+    int wrong_answer = 0;                                                      \
+    for (int i = 0; i < repeat; i++) {                                         \
+       pre;                                                                     \
+      __asm volatile("" ::: /* pretend to clobber */ "memory");                \
+      RDTSC_START(cycles_start);                                               \
+      if (test != answer)                                                      \
+        wrong_answer = 1;                                                      \
+      RDTSC_FINAL(cycles_final);                                               \
+      cycles_diff = (cycles_final - cycles_start);                             \
+      if (cycles_diff < min_diff)                                              \
+        min_diff = cycles_diff;                                                \
+    }                                                                          \
+    uint64_t S = (uint64_t)size;                                               \
+    float cycle_per_op = (min_diff) / (float)S;                                \
+    printf(" %.2f %s per operation", cycle_per_op, nameofcycles);                        \
+    if (wrong_answer)                                                          \
+      printf(" [ERROR]");                                                      \
+    printf("\n");                                                              \
+    fflush(NULL);                                                              \
+  } while (0)
+
+
+// let us estimate that we have a 1% proba of hitting a white space
+size_t fillwithtext(char *buffer, size_t size) {
+  size_t howmany = 0;
+  for (size_t i = 0; i < size; ++i) {
+    double r = ((double)rand() / (RAND_MAX));
+    if (r < 0.01) {
+      buffer[i] = ' ';
+      howmany++;
+    } else if (r < 0.02) {
+      buffer[i] = '\n';
+      howmany++;
+    } else if (r < 0.03) {
+      buffer[i] = '\r';
+      howmany++;
+    } else {
+      do {
+        buffer[i] = (char)rand();
+      } while (buffer[i] <= 32);
+    }
+  }
+  return howmany;
+}
+
+
+int main(int argc, char ** argv) {
+  const int N = 1024 * 32;
+  int alignoffset = 0;
+  if(argc>1)  {
+    alignoffset = atoi(argv[1]);
+    printf("alignment offset = %d \n", alignoffset);
+  }
+  char *origbuffer = malloc(N + alignoffset);
+  char *origtmpbuffer = malloc(N + alignoffset);
+  char *buffer = origbuffer + alignoffset;
+  char *tmpbuffer = origtmpbuffer + alignoffset;
+  printf("pointer alignment = %d bytes \n", 1<< __builtin_ctzll((uintptr_t)(const void *)(buffer)));
+
+  int repeat = 100;
+  size_t howmanywhite = fillwithtext(buffer, N);
+
+  BEST_TIME_NOCHECK_NOPRE(memcpy(tmpbuffer,buffer,N),
+                   repeat, N);
+  printf("\n");
+  BEST_TIME(despace(buffer, N), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
+  BEST_TIME(neon_despace(buffer, N), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
+  free(origbuffer);
+  free(origtmpbuffer);
+}
---
+++ 2017/07/03/despacer.h
@@ -0,0 +1,117 @@
+#ifndef DESPACER_H
+#define DESPACER_H
+#include <stddef.h>
+#include <stdint.h>
+
+static inline size_t despace(char *bytes, size_t howmany) {
+  size_t i = 0, pos = 0;
+  while (i < howmany) {
+    const char c = bytes[i++];
+    bytes[pos] = c;
+    pos += (c > 32 ? 1 : 0);
+  }
+  return pos;
+}
+
+// let us go neon
+#include <arm_neon.h>
+
+
+static inline uint8x16_t is_white(uint8x16_t data) {
+  const uint8x16_t wchar = vdupq_n_u8(' ');
+  uint8x16_t isw = vcleq_u8(data, wchar);
+  return isw;
+}
+
+
+
+////////////
+// see https://stackoverflow.com/a/41406502/73007
+// compiles to something like
+//        uqxtn   v0.2s, v0.2d
+//        fmov    x0, d0
+//////////
+static inline uint64_t is_not_zero(uint8x16_t v) {
+  uint64x2_t v64 = vreinterpretq_u64_u8(v);
+  uint32x2_t v32 = vqmovn_u64(v64);
+  uint64x1_t result = vreinterpret_u64_u32(v32);
+#ifdef __clang__
+  return result[0];
+#else
+  return result;
+#endif
+}
+
+
+
+static inline size_t neon_despace(char *bytes, size_t howmany) {
+   size_t i = 0, pos = 0;
+  const size_t chunk_size = 16 * 4 * 1;
+  uint8x16_t justone = vdupq_n_u8(1);
+  for (; i + chunk_size <= howmany; /*i += chunk_size*/) {
+    uint8x16x4_t vecbytes = vld4q_u8((uint8_t *)bytes + i);
+    uint8x16_t w0 = is_white(vecbytes.val[0]);
+    uint64_t haswhite0 = is_not_zero(w0);
+    w0 = vaddq_u8(justone, w0);
+    uint8x16_t w1 = is_white(vecbytes.val[1]);
+    uint64_t haswhite1 = is_not_zero(w1);
+    w1 = vaddq_u8(justone, w1);
+    uint8x16_t w2 = is_white(vecbytes.val[2]);
+    uint64_t haswhite2 = is_not_zero(w2);
+    w2 = vaddq_u8(justone, w2);
+    uint8x16_t w3 = is_white(vecbytes.val[3]);
+    uint64_t haswhite3 = is_not_zero(w3);
+    w3 = vaddq_u8(justone, w3);
+    if(!haswhite0) {
+      vst1q_u8((uint8_t *)bytes + pos,vecbytes.val[0]);
+      pos += 16;
+      i += 16;
+     } else {
+      for (int k = 0; k < 16; k++) {
+        bytes[pos] = bytes[i++];
+        pos += w0[k];
+      }
+    }
+    if(!haswhite1) {
+      vst1q_u8((uint8_t *)bytes + pos,vecbytes.val[1]);
+      pos += 16;
+      i += 16;
+     } else {
+       for (int k = 0; k < 16; k++) {
+        bytes[pos] = bytes[i++];
+        pos += w1[k];
+      }
+    }
+    if(!haswhite2) {
+      vst1q_u8((uint8_t *)bytes + pos,vecbytes.val[2]);
+      pos += 16;
+      i += 16;
+     } else {
+       for (int k = 0; k < 16; k++) {
+        bytes[pos] = bytes[i++];
+        pos += w2[k];
+      }
+    }
+    if(!haswhite3) {
+      vst1q_u8((uint8_t *)bytes + pos,vecbytes.val[3]);
+      pos += 16;
+      i += 16;
+     } else {
+       for (int k = 0; k < 16; k++) {
+        bytes[pos] = bytes[i++];
+        pos += w3[k];
+      }
+    }
+  }
+  while (i < howmany) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
+  }
+  return pos;
+}
+
+
+#endif // end of file
