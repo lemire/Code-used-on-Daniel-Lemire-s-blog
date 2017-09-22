4161195ef8299f4897d6c3cf0ba7ea722baa3cc5
Daniel Lemire <lemire@gmail.com>
2017-09-21 21:18:12 -0400

    Remark

--- 2017/09/21/run.sh
+++ 2017/09/21/run.sh
@@ -4,4 +4,4 @@ lldb --batch  bare -o "disas -n popcntArray"
 lldb --batch  bare -o "disas -n multiplyHigh"
 lldb --batch  bare -o "disas -n popCnt"
 echo "function names might be mangled on some platforms."
-
+echo "You might need to hunt for symbols and call swift demangle or use a better script"
