fbdc156538712042befeb35c29893757f1c57a21
Daniel Lemire <lemire@gmail.com>
2018-08-14 16:54:25 -0400

    More careful disabling of AVX-512.

--- 2018/08/13/all.sh
+++ 2018/08/13/all.sh
@@ -1,9 +1,9 @@
 ./script20.sh 2>&1 | tee results_test1_20.txt
-./script20.sh 2>&1 | tee results_test2_20.txt
-./script20.sh 2>&1 | tee results_test3_20.txt
 ./script40.sh 2>&1 | tee results_test1_40.txt
-./script40.sh 2>&1 | tee results_test2_40.txt
-./script40.sh 2>&1 | tee results_test3_40.txt
 ./script80.sh 2>&1 | tee results_test1_80.txt
+./script20.sh 2>&1 | tee results_test2_20.txt
+./script40.sh 2>&1 | tee results_test2_40.txt
 ./script80.sh 2>&1 | tee results_test2_80.txt
+./script20.sh 2>&1 | tee results_test3_20.txt
+./script40.sh 2>&1 | tee results_test3_40.txt
 ./script80.sh 2>&1 | tee results_test3_80.txt
--- 2018/08/13/script80.sh
+++ 2018/08/13/script80.sh
@@ -2,8 +2,7 @@ set -x
 
 cat /proc/cpuinfo  | grep 'name'| uniq
 gcc --version
-gcc -O3 -o bubblenoavx512 bubble.c -march=native -mno-avx512dq -lpthread
+gcc -O3 -o bubblenoavx512 bubble.c -march=native -mno-avx512dq  -mno-avx512f -lpthread
 gcc -O3 -o bubble bubble.c -march=native -lpthread
 for num in `seq 1 1 5`; do time ./bubblenoavx512 80; done
 for num in `seq 1 1 5`; do time ./bubble 80; done
-
