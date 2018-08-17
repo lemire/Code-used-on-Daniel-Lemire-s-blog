400f6a091a0e910bb8b15a0b31b8a417792da368
Daniel Lemire <lemire@gmail.com>
2018-08-17 14:29:31 -0400

    Formatting.

--- extra/packing/pack12/pack12.c
+++ extra/packing/pack12/pack12.c
@@ -10,9 +10,7 @@
 #include <string.h>
 
 // for comparison purposes
-uint32_t get8(size_t index, uint8_t *data) {
-  return data[index];
-}
+uint32_t get8(size_t index, uint8_t *data) { return data[index]; }
 
 // gets a 12-bit value at index from packed 12-bit values in data
 uint32_t get(size_t index, uint8_t *data) {
@@ -23,8 +21,8 @@ uint32_t get(size_t index, uint8_t *data) {
   return (word >> (bitPos & 7)) & 0xFFF;
 }
 
-
-// sets a 12-bit value at index to packed 12-bit values datal, val should be a 12-bit value
+// sets a 12-bit value at index to packed 12-bit values datal, val should be a
+// 12-bit value
 void set(size_t index, uint32_t val, uint8_t *data) {
   size_t wordpos = (index >> 1) * 3;
   unsigned int wp = (index & 1) * 12;
@@ -37,58 +35,55 @@ void set(size_t index, uint32_t val, uint8_t *data) {
 }
 
 uint32_t getthomas(size_t index, uint8_t *data) {
-         size_t bitPos = index * 12;
-         size_t firstBytePos = (size_t) (bitPos >> 3);
-         uint32_t word = __builtin_bswap32(*((uint32_t*) (data + firstBytePos))) >> 8;
-          return (uint32_t) ((word >> (24 - 12 - (bitPos & 7))) & 0xFFF);
+  size_t bitPos = index * 12;
+  size_t firstBytePos = (size_t)(bitPos >> 3);
+  uint32_t word = __builtin_bswap32(*((uint32_t *)(data + firstBytePos))) >> 8;
+  return (uint32_t)((word >> (24 - 12 - (bitPos & 7))) & 0xFFF);
 }
 
 void setthomas(size_t index, uint32_t value, uint8_t *data) {
-         size_t bitPos = index * 12;
-         size_t firstBytePos = (size_t) (bitPos >> 3);
-         uint32_t word = __builtin_bswap32(*((uint32_t*) (data + firstBytePos))) >> 8;
-         word &= ~(0xFFF << (24 - 12 - (bitPos & 7)));
-         word |= ((value & 0xFFF) << (24 - 12 - (bitPos & 7)));
-         data[firstBytePos] = (uint8_t) (word >> 16);
-         data[firstBytePos + 1] = (uint8_t) (word >> 8);
-         data[firstBytePos + 2] = (uint8_t) word;
+  size_t bitPos = index * 12;
+  size_t firstBytePos = (size_t)(bitPos >> 3);
+  uint32_t word = __builtin_bswap32(*((uint32_t *)(data + firstBytePos))) >> 8;
+  word &= ~(0xFFF << (24 - 12 - (bitPos & 7)));
+  word |= ((value & 0xFFF) << (24 - 12 - (bitPos & 7)));
+  data[firstBytePos] = (uint8_t)(word >> 16);
+  data[firstBytePos + 1] = (uint8_t)(word >> 8);
+  data[firstBytePos + 2] = (uint8_t)word;
 }
 
-__attribute__((optimize("no-tree-vectorize")))
-uint32_t sum8(uint8_t *data, size_t number) {
+__attribute__((optimize("no-tree-vectorize"))) uint32_t sum8(uint8_t *data,
+                                                             size_t number) {
   uint32_t s = 0xFFF;
   for (size_t i = 0; i < number; i++)
-    s += get8(i,data);
+    s += get8(i, data);
   return s;
 }
 
-__attribute__((optimize("no-tree-vectorize")))
-uint32_t sum(uint8_t *data, size_t number) {
+__attribute__((optimize("no-tree-vectorize"))) uint32_t sum(uint8_t *data,
+                                                            size_t number) {
   uint32_t s = 0xFFF;
   for (size_t i = 0; i < number; i++)
     s += get(i, data);
   return s;
 }
 
-
-__attribute__((optimize("no-tree-vectorize")))
-uint32_t sumthomas(uint8_t *data, size_t number) {
+__attribute__((optimize("no-tree-vectorize"))) uint32_t
+sumthomas(uint8_t *data, size_t number) {
   uint32_t s = 0xFFF;
   for (size_t i = 0; i < number; i++)
     s += getthomas(i, data);
   return s;
 }
 
-
-
-__attribute__((optimize("no-tree-vectorize")))
-bool populate(uint8_t *data, size_t number) {
+__attribute__((optimize("no-tree-vectorize"))) bool populate(uint8_t *data,
+                                                             size_t number) {
   for (size_t i = 0; i < number; i++)
     set(i, 0xFFF - (i & 0xFFF), data);
   return true;
 }
-__attribute__((optimize("no-tree-vectorize")))
-bool populatethomas(uint8_t *data, size_t number) {
+__attribute__((optimize("no-tree-vectorize"))) bool
+populatethomas(uint8_t *data, size_t number) {
   for (size_t i = 0; i < number; i++)
     setthomas(i, 0xFFF - (i & 0xFFF), data);
   return true;
@@ -125,12 +120,13 @@ void demo(size_t number) {
     set(i, val, data);
   }
   int repeat = 50;
-  uint32_t expected8 = sum8(data,number);
+  uint32_t expected8 = sum8(data, number);
   BEST_TIME(sum8(data, number), expected8, , repeat, number, number, true);
   uint32_t expected = sum(data, number);
   BEST_TIME(sum(data, number), expected, , repeat, number, number, true);
   uint32_t expectedthomas = sumthomas(data, number);
-  BEST_TIME(sumthomas(data, number), expectedthomas, , repeat, number, number, true);
+  BEST_TIME(sumthomas(data, number), expectedthomas, , repeat, number, number,
+            true);
 
   BEST_TIME(populate(data, number), true, , repeat, number, number, true);
   BEST_TIME(populatethomas(data, number), true, , repeat, number, number, true);
