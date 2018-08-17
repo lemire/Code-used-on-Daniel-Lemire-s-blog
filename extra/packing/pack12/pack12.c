311bde8cf115fd47b799270d26777a5ddcfcc21f
Daniel Lemire <lemire@gmail.com>
2018-08-17 14:26:55 -0400

    Tweaking.

--- extra/packing/pack12/pack12.c
+++ extra/packing/pack12/pack12.c
@@ -36,11 +36,29 @@ void set(size_t index, uint32_t val, uint8_t *data) {
   memcpy(data + wordpos, &word, sizeof(uint32_t));
 }
 
+uint32_t getthomas(size_t index, uint8_t *data) {
+         size_t bitPos = index * 12;
+         size_t firstBytePos = (size_t) (bitPos >> 3);
+         uint32_t word = __builtin_bswap32(*((uint32_t*) (data + firstBytePos))) >> 8;
+          return (uint32_t) ((word >> (24 - 12 - (bitPos & 7))) & 0xFFF);
+}
+
+void setthomas(size_t index, uint32_t value, uint8_t *data) {
+         size_t bitPos = index * 12;
+         size_t firstBytePos = (size_t) (bitPos >> 3);
+         uint32_t word = __builtin_bswap32(*((uint32_t*) (data + firstBytePos))) >> 8;
+         word &= ~(0xFFF << (24 - 12 - (bitPos & 7)));
+         word |= ((value & 0xFFF) << (24 - 12 - (bitPos & 7)));
+         data[firstBytePos] = (uint8_t) (word >> 16);
+         data[firstBytePos + 1] = (uint8_t) (word >> 8);
+         data[firstBytePos + 2] = (uint8_t) word;
+}
+
 __attribute__((optimize("no-tree-vectorize")))
 uint32_t sum8(uint8_t *data, size_t number) {
   uint32_t s = 0xFFF;
   for (size_t i = 0; i < number; i++)
-    s += get8(s & (number - 1),data) * 0xF7F7F7F;
+    s += get8(i,data);
   return s;
 }
 
@@ -48,10 +66,18 @@ __attribute__((optimize("no-tree-vectorize")))
 uint32_t sum(uint8_t *data, size_t number) {
   uint32_t s = 0xFFF;
   for (size_t i = 0; i < number; i++)
-    s += get(s & (number - 1), data) * 0xF7F7F7F;
+    s += get(i, data);
   return s;
 }
 
+
+__attribute__((optimize("no-tree-vectorize")))
+uint32_t sumthomas(uint8_t *data, size_t number) {
+  uint32_t s = 0xFFF;
+  for (size_t i = 0; i < number; i++)
+    s += getthomas(i, data);
+  return s;
+}
 
 
 
@@ -61,7 +87,12 @@ bool populate(uint8_t *data, size_t number) {
     set(i, 0xFFF - (i & 0xFFF), data);
   return true;
 }
-
+__attribute__((optimize("no-tree-vectorize")))
+bool populatethomas(uint8_t *data, size_t number) {
+  for (size_t i = 0; i < number; i++)
+    setthomas(i, 0xFFF - (i & 0xFFF), data);
+  return true;
+}
 
 void validate(size_t number) {
   uint8_t *data =
@@ -93,20 +124,23 @@ void demo(size_t number) {
     uint32_t val = 0xFFF - (i & 0xFFF);
     set(i, val, data);
   }
-  uint32_t expected = sum(data, number);
   int repeat = 50;
   uint32_t expected8 = sum8(data,number);
   BEST_TIME(sum8(data, number), expected8, , repeat, number, number, true);
-
+  uint32_t expected = sum(data, number);
   BEST_TIME(sum(data, number), expected, , repeat, number, number, true);
+  uint32_t expectedthomas = sumthomas(data, number);
+  BEST_TIME(sumthomas(data, number), expectedthomas, , repeat, number, number, true);
+
   BEST_TIME(populate(data, number), true, , repeat, number, number, true);
+  BEST_TIME(populatethomas(data, number), true, , repeat, number, number, true);
+
   free(data);
 }
 
 int main() {
   validate(1024);
 
-//  demo(1024);
   demo(1024 * 1024 * 16);
 
   return EXIT_SUCCESS;
