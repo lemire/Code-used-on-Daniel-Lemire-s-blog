f58cd5f2451c209e5bf23aaad1c4032497f8f1cd
Daniel Lemire <lemire@gmail.com>
2018-08-20 11:26:16 -0400

    Latest blog post.

--- 2018/08/20/fastrange.c
+++ 2018/08/20/fastrange.c
@@ -79,6 +79,27 @@ uint32_t fastsum(uint32_t * z, uint32_t N, uint32_t * accesses, uint32_t nmbr) {
   return sum;
 }
 
+#ifdef __AVX2__
+#include <x86intrin.h>
+
+// assume N is divisible by 4
+uint32_t vectorsum(uint32_t * z, uint32_t N, uint32_t * accesses, uint32_t nmbr) {
+  __m256i Nvec = _mm256_set1_epi32(N);
+  __m128i sum = _mm_setzero_si128();
+  for(uint32_t j = 0; j < nmbr ; j+=4) {
+     __m256i fourints = _mm256_loadu_si256((const __m256i *)(accesses + j));
+     __m256i four64bitsproducts =  _mm256_mul_epu32(fourints, Nvec);
+     __m256i fourtop32ints = _mm256_srli_epi64(four64bitsproducts,32);
+     __m128i four32ints = _mm256_i64gather_epi32 (z,fourtop32ints , 4);
+     sum = _mm_add_epi32(sum, four32ints);
+  }
+  uint32_t buffer[4];
+  _mm_storeu_si128((__m128i *)buffer,sum);
+  return buffer[0] + buffer[1] + buffer[2] + buffer[3];
+}
+
+#endif
+
 // N is a power of two
 uint32_t maskedsum(uint32_t * z, uint32_t N, uint32_t * accesses, uint32_t nmbr) {
   uint32_t sum = 0;
@@ -101,6 +122,10 @@ void demo(uint32_t N) {
   uint32_t expected2 = fastsum(z,N,accesses,nmbr);
   BEST_TIME(modsum(z,N,accesses,nmbr), expected1, 1000, nmbr);
   BEST_TIME(fastsum(z,N,accesses,nmbr), expected2, 1000, nmbr);
+#ifdef __AVX2__
+  uint32_t expected3 = vectorsum(z,N,accesses,nmbr);
+  if(N % 4 == 0) BEST_TIME(vectorsum(z,N,accesses,nmbr), expected3, 1000, nmbr);
+#endif
   free(z);
   free(accesses);
 }
@@ -114,10 +139,13 @@ void demopoweroftwo(uint32_t N) {
   for(uint32_t i = 0 ; i < nmbr; ++i) accesses[i] = rand(); // some rand. number
   uint32_t expected1 = modsum(z,N,accesses,nmbr);
   uint32_t expected2 = fastsum(z,N,accesses,nmbr);
-
   BEST_TIME(modsum(z,N,accesses,nmbr), expected1, 1000, nmbr);
   BEST_TIME(fastsum(z,N,accesses,nmbr), expected2, 1000, nmbr);
   BEST_TIME(maskedsum(z,N,accesses,nmbr), expected1, 1000, nmbr);
+#ifdef __AVX2__
+  uint32_t expected3 = vectorsum(z,N,accesses,nmbr);
+  BEST_TIME(vectorsum(z,N,accesses,nmbr), expected3, 1000, nmbr);
+#endif
 
   free(z);
   free(accesses);
