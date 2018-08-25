1f1f2446d3492f5aeb663dfc589f61631f0ff6a7
Daniel Lemire <lemire@gmail.com>
2018-08-24 21:25:12 -0400

    Adding zero to bench

--- 2018/08/24/benchmark.sh
+++ 2018/08/24/benchmark.sh
@@ -1,4 +1,5 @@
-
+  for i in {1..20}; do time ./mandelbrotzero ${i} 3200; done
+  sleep 2
   for i in {1..20}; do time ./mandelbrot ${i} 3200; done
   sleep 2
   for i in {1..20}; do time ./mandelbrot512 ${i} 3200; done
