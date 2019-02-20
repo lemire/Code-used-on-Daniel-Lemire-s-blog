6133adee10c5e440129025e5e9da26d7a1f280f8
Daniel Lemire <lemire@gmail.com>
2019-02-19 19:36:24 -0500

    Saving...

--- 2019/02/19/hash.c
+++ 2019/02/19/hash.c
@@ -12,6 +12,13 @@
 #define CONSTANTADD 27961
 #define REPEAT 1000000
 
+volatile uint32_t dv = D;
+
+static inline uint32_t slowmod32(uint32_t a) {
+  return a % dv;
+}
+
+
 static inline uint32_t compilermod32(uint32_t a) {
   return a % D;
 }
@@ -26,6 +33,13 @@ static inline uint32_t indirectmod64(uint32_t a) {
   return a - quotient * D;
 }
 
+uint32_t slow() {
+  uint32_t seed = INITSEED;
+  for(size_t i = 0; i < REPEAT; i++) {
+    seed = slowmod32(seed * MULTIPLIER + CONSTANTADD) ;
+  }
+  return seed;
+}
 
 uint32_t compiler() {
   uint32_t seed = INITSEED;
@@ -55,6 +69,7 @@ void demo() {
   bool verbose = true;
   int repeat = 50;
   uint32_t expected = indirect();
+  BEST_TIME(slow(), expected, , repeat, REPEAT, verbose);
   BEST_TIME(compiler(), expected, , repeat, REPEAT, verbose);
   BEST_TIME(direct(), expected, , repeat, REPEAT, verbose);
   BEST_TIME(indirect(), expected, , repeat, REPEAT, verbose);
