11b4362e8575e752805c213685604a506b88f677
Daniel Lemire <lemire@gmail.com>
2019-03-22 11:18:40 -0400

    Splitting off code.

--- extra/fastflags/countbits.cpp
+++ extra/fastflags/countbits.cpp
@@ -25,7 +25,6 @@ void scalar_morenaive(const uint16_t *data, size_t n, uint32_t *flags) {
 }
 
 
-// fixme
 void fastavx2(uint16_t *array, size_t len, uint32_t *flags) {
   for (size_t i = 0; i < 16; i++)
     flags[i] = 0;
@@ -62,6 +61,113 @@ void fastavx2(uint16_t *array, size_t len, uint32_t *flags) {
     count16 = _mm256_setzero_si256();
     size_t j = 0;
     size_t maxj = i + 65535 + 16 <= len ? 65535 : len - i;
+    if (maxj > 4) {
+      for (; j + 3 < maxj; j += 4) {
+        __m256i input1 = _mm256_loadu_si256((__m256i *)(array + i + j));
+        __m256i m1 = _mm256_and_si256(input1, bits);
+        __m256i eq1 = _mm256_cmpeq_epi16(bits, m1);
+        count16 = _mm256_sub_epi16(count16, eq1);
+        __m256i input2 = _mm256_loadu_si256((__m256i *)(array + i + j + 1));
+        __m256i m2 = _mm256_and_si256(input2, bits);
+        __m256i eq2 = _mm256_cmpeq_epi16(bits, m2);
+        count16 = _mm256_sub_epi16(count16, eq2);
+        __m256i input3 = _mm256_loadu_si256((__m256i *)(array + i + j + 2));
+        __m256i m3 = _mm256_and_si256(input3, bits);
+        __m256i eq3 = _mm256_cmpeq_epi16(bits, m3);
+        count16 = _mm256_sub_epi16(count16, eq3);
+        __m256i input4 = _mm256_loadu_si256((__m256i *)(array + i + j + 3));
+        __m256i m4 = _mm256_and_si256(input4, bits);
+        __m256i eq4 = _mm256_cmpeq_epi16(bits, m4);
+        count16 = _mm256_sub_epi16(count16, eq4);
+      }
+    }
+    for (; j < maxj; j++) {
+      __m256i input = _mm256_loadu_si256((__m256i *)(array + i + j));
+      __m256i m = _mm256_and_si256(input, bits);
+      __m256i eq = _mm256_cmpeq_epi16(bits, m);
+      count16 = _mm256_sub_epi16(count16, eq);
+    }
+    i += j;
+    _mm256_storeu_si256((__m256i *)buffer, count16);
+    for (size_t k = 0; k < 16; k++) {
+      flags[k] += buffer[k];
+    }
+  }
+}
+
+void morefastavx2(uint16_t *array, size_t len, uint32_t *flags) {
+  for (size_t i = 0; i < 16; i++)
+    flags[i] = 0;
+  uint16_t buffer[16];
+  __m256i bits = _mm256_set_epi16(-32768, 16384, 8192, 4096, 2048, 1024, 512,
+                                  256, 128, 64, 32, 16, 8, 4, 2, 1);
+  // we do the first part
+  if (len < 64) {
+    // don't bother with handcrafted SIMD
+    scalar_naive(array, len, flags);
+    return;
+  }
+  // handle the start (naively)
+  __m256i count16 = _mm256_setzero_si256();
+
+  {
+    uint16_t startbuffer[32];
+    memset(startbuffer, 0, 32 * 2);
+    memcpy(startbuffer + 16, array, 16 * 2);
+    for (size_t i = 1; i < 16; i++) {
+      __m256i input = _mm256_loadu_si256((__m256i *)(startbuffer + i));
+      __m256i m = _mm256_and_si256(input, bits);
+      __m256i eq = _mm256_cmpeq_epi16(bits, m);
+      count16 = _mm256_sub_epi16(count16, eq);
+    }
+  }
+  _mm256_storeu_si256((__m256i *)buffer, count16);
+  for (size_t k = 0; k < 16; k++) {
+    flags[k] += buffer[k];
+  }
+
+  // main loop starts here
+  for (size_t i = 0; i + 16 <= len;) {
+    count16 = _mm256_setzero_si256();
+    size_t j = 0;
+    size_t maxj = i + 65535 + 16 <= len ? 65535 : len - i;
+    if (maxj > 8) {
+      for (; j + 7 < maxj; j += 8) {
+        __m256i input1 = _mm256_loadu_si256((__m256i *)(array + i + j));
+        __m256i m1 = _mm256_and_si256(input1, bits);
+        __m256i eq1 = _mm256_cmpeq_epi16(bits, m1);
+        count16 = _mm256_sub_epi16(count16, eq1);
+        __m256i input2 = _mm256_loadu_si256((__m256i *)(array + i + j + 1));
+        __m256i m2 = _mm256_and_si256(input2, bits);
+        __m256i eq2 = _mm256_cmpeq_epi16(bits, m2);
+        count16 = _mm256_sub_epi16(count16, eq2);
+        __m256i input3 = _mm256_loadu_si256((__m256i *)(array + i + j + 2));
+        __m256i m3 = _mm256_and_si256(input3, bits);
+        __m256i eq3 = _mm256_cmpeq_epi16(bits, m3);
+        count16 = _mm256_sub_epi16(count16, eq3);
+        __m256i input4 = _mm256_loadu_si256((__m256i *)(array + i + j + 3));
+        __m256i m4 = _mm256_and_si256(input4, bits);
+        __m256i eq4 = _mm256_cmpeq_epi16(bits, m4);
+        count16 = _mm256_sub_epi16(count16, eq4);
+        __m256i input5 = _mm256_loadu_si256((__m256i *)(array + i + j + 4));
+        __m256i m5 = _mm256_and_si256(input5, bits);
+        __m256i eq5 = _mm256_cmpeq_epi16(bits, m5);
+        count16 = _mm256_sub_epi16(count16, eq5);
+        __m256i input6 = _mm256_loadu_si256((__m256i *)(array + i + j + 5));
+        __m256i m6 = _mm256_and_si256(input6, bits);
+        __m256i eq6 = _mm256_cmpeq_epi16(bits, m6);
+        count16 = _mm256_sub_epi16(count16, eq6);
+        __m256i input7 = _mm256_loadu_si256((__m256i *)(array + i + j + 6));
+        __m256i m7 = _mm256_and_si256(input7, bits);
+        __m256i eq7 = _mm256_cmpeq_epi16(bits, m7);
+        count16 = _mm256_sub_epi16(count16, eq7);
+        __m256i input8 = _mm256_loadu_si256((__m256i *)(array + i + j + 7));
+        __m256i m8 = _mm256_and_si256(input8, bits);
+        __m256i eq8 = _mm256_cmpeq_epi16(bits, m8);
+        count16 = _mm256_sub_epi16(count16, eq8);
+      }
+    }
+
     if (maxj > 4) {
       for (; j + 3 < maxj; j += 4) {
         __m256i input1 = _mm256_loadu_si256((__m256i *)(array + i + j));
@@ -135,7 +241,6 @@ int main() {
     assert(counter[i] == truecounter[i]);
   }
   BEST_TIME_NOCHECK(scalar_morenaive(array, len, counter), , repeat, len, true);
-
   for (size_t i = 0; i < 16; i++) {
     assert(counter[i] == truecounter[i]);
   }
@@ -143,7 +248,10 @@ int main() {
   for (size_t i = 0; i < 16; i++) {
     assert(counter[i] == truecounter[i]);
   }
-
+  BEST_TIME_NOCHECK(morefastavx2(array, len, counter), , repeat, len, true);
+  for (size_t i = 0; i < 16; i++) {
+    assert(counter[i] == truecounter[i]);
+  }
   BEST_TIME_NOCHECK(flag_stats_avx2(array, len, counter), , repeat, len, true);
   for (size_t i = 0; i < 16; i++) {
     assert(counter[i] == truecounter[i]);
