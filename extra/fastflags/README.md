72f5d80a9c91ff68ed4eeddcb3ddc5eed182a7ce
Daniel Lemire <lemire@gmail.com>
2019-03-21 21:45:32 -0400

    Trying a few things.

--- extra/fastflags/README.md
+++ extra/fastflags/README.md
@@ -1,1 +1,19 @@
+Given an array of 16-bit values, we want to count the number of first bit set, the number
+of second bit set, and so forth (16 counters in total).
+
+Ref: https://github.com/mklarqvist/FastFlagStats
+
+Naive scalar code:
+```
+
+void scalar_naive(const uint16_t *data, size_t n, uint32_t *flags) {
+  memset(flags, 0, 16 * sizeof(uint32_t));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < 16; ++j) {
+      flags[j] += ((data[i] & (1 << j)) >> j);
+    }
+  }
+}
+```
+
 g++ -O3 -march=native -o countbits countbits.cpp && ./countbits
--- extra/fastflags/countbits.cpp
+++ extra/fastflags/countbits.cpp
@@ -12,6 +12,87 @@ void scalar_naive(const uint16_t *data, size_t n, uint32_t *flags) {
   }
 }
 
+void scalar_morenaive(const uint16_t *data, size_t n, uint32_t *flags) {
+  memset(flags, 0, 16 * sizeof(uint32_t));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < 16; ++j) {
+      flags[j] += (data[i]  >> j) & 1;
+    }
+  }
+}
+
+void flag_stats_avx2(const uint16_t*  data, uint32_t n, uint32_t*  flags) {
+
+    __m256i masks[16];
+    __m256i counters[16];
+    for(int i = 0; i < 16; ++i) {
+        masks[i]    = _mm256_set1_epi16(1 << i);
+        counters[i] = _mm256_set1_epi16(0);
+    }
+    uint32_t out_counters[16] = {0};
+
+    const __m256i hi_mask = _mm256_set1_epi32(0xFFFF0000);
+    const __m256i lo_mask = _mm256_set1_epi32(0x0000FFFF);
+    const __m256i* data_vectors = reinterpret_cast<const __m256i*>(data);
+    const uint32_t n_cycles = n / 16;
+    const uint32_t n_update_cycles = n_cycles / 65536;
+
+#define UPDATE(idx) counters[idx]  = _mm256_add_epi16(counters[idx],  _mm256_srli_epi16(_mm256_and_si256(data_vectors[pos], masks[idx]),  idx))
+#define ITERATION  {                                   \
+        UPDATE(0);  UPDATE(1);  UPDATE(2);  UPDATE(3); \
+        UPDATE(4);  UPDATE(5);  UPDATE(6);  UPDATE(7); \
+        UPDATE(8);  UPDATE(9);  UPDATE(10); UPDATE(11);\
+        UPDATE(12); UPDATE(13); UPDATE(14); UPDATE(15);\
+        ++pos; ++k;                                    \
+}
+    uint32_t pos = 0;
+    for(int i = 0; i < n_update_cycles; ++i) { // each block of 2^16 values
+        for(int k = 0; k < 65536; ) // max sum of each 16-bit value in a register
+            ITERATION // unrolled
+
+        // Compute vector sum
+        for(int k = 0; k < 16; ++k) { // each flag register
+            // Accumulator
+            // ((16-bit high & 16 high) >> 16) + (16-bit low & 16-low)
+            __m256i x = _mm256_add_epi32(
+                           _mm256_srli_epi32(_mm256_and_si256(counters[k], hi_mask), 16),
+                           _mm256_and_si256(counters[k], lo_mask));
+            __m256i t1 = _mm256_hadd_epi32(x,x);
+            __m256i t2 = _mm256_hadd_epi32(t1,t1);
+            __m128i t4 = _mm_add_epi32(_mm256_castsi256_si128(t2),_mm256_extractf128_si256(t2,1));
+            out_counters[k] += _mm_cvtsi128_si32(t4);
+
+            /*
+            // Naive counter
+            uint16_t* d = reinterpret_cast<uint16_t*>(&counters[k]);
+            for(int j = 0; j < 16; ++j) { // each uint16_t in the register
+                out_counters[k] += d[j];
+            }
+            */
+
+            counters[k] = _mm256_set1_epi16(0);
+        }
+    }
+
+    // residual
+    for(int i = pos*16; i < n; ++i) {
+        for(int j = 0; j < 16; ++j)
+            out_counters[j] += ((data[i] & (1 << j)) >> j);
+    }
+
+
+    for(int i = 0; i < 16; ++i) flags[i] = out_counters[i];
+
+    //std::cerr << "simd=";
+    //for(int i = 0; i < 16; ++i) std::cerr << " " << out_counters[i];
+    //std::cerr << std::endl;
+
+#undef ITERATION
+#undef UPDATE
+
+
+}
+
 // fixme
 void fastavx2(uint16_t *array, size_t len, uint32_t *flags) {
   for (size_t i = 0; i < 16; i++)
@@ -80,15 +161,34 @@ int main() {
 
   int repeat = 10;
   BEST_TIME_NOCHECK(scalar_naive(array, len, counter), , repeat, len, true);
+  printf("\n");
+
+  for (size_t i = 0; i < 16; i++) {
+    printf("%u ", counter[i]);
+  }
+  printf("\n");
+  BEST_TIME_NOCHECK(scalar_morenaive(array, len, counter), , repeat, len, true);
+  printf("\n");
+
   for (size_t i = 0; i < 16; i++) {
     printf("%u ", counter[i]);
   }
   printf("\n");
 
   BEST_TIME_NOCHECK(fastavx2(array, len, counter), , repeat, len, true);
+  printf("\n");
+
   for (size_t i = 0; i < 16; i++) {
     printf("%u ", counter[i]);
   }
   printf("\n");
 
+  BEST_TIME_NOCHECK(flag_stats_avx2(array, len, counter), , repeat, len, true);
+  printf("\n");
+
+
+  for (size_t i = 0; i < 16; i++) {
+    printf("%u ", counter[i]);
+  }
+  printf("\n");
 }
