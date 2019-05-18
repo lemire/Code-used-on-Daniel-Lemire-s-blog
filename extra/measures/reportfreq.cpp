939fabb7e42386b7036b269b2322b3ca80c56aca
Daniel Lemire <lemire@gmail.com>
2019-05-18 15:51:27 -0400

    Trying...

--- extra/measures/reportfreq.cpp
+++ extra/measures/reportfreq.cpp
@@ -4,25 +4,48 @@
 
 void measure_frequency() {
   const size_t test_duration_in_cycles =
-      1000 * 1000; // we are going to use 1 million cycles
-  size_t cycles = test_duration_in_cycles;
-  auto begin = std::chrono::high_resolution_clock::now();
+     10 * 1000 * 1000; // we are going to use 10 million cycles
+  size_t cycles = 2 * test_duration_in_cycles;
+  // travis feels strongly about the measure-twice-and-subtract trick.
+  auto begin1 = std::chrono::high_resolution_clock::now();
 #ifdef __x86_64__
-  __asm volatile("cyclemeasure:\n dec %[counter] \n jnz cyclemeasure \n"
+  __asm volatile("cyclemeasure1:\n dec %[counter] \n jnz cyclemeasure1 \n"
                  : /* read/write reg */ [counter] "+r"(cycles));
 #elif defined(__aarch64__)
   __asm volatile(
-      "cyclemeasure:\nsubs %[counter],%[counter],#1\nbne cyclemeasure\n "
+      "cyclemeasure1:\nsubs %[counter],%[counter],#1\nbne cyclemeasure1\n "
       : /* read/write reg */ [counter] "+r"(cycles));
 #endif
-  auto end = std::chrono::high_resolution_clock::now();
-  double nanoseconds =
-      std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
-  std::cout << nanoseconds << "ns" << std::endl;
-  std::cout << test_duration_in_cycles << "cycles" << std::endl;
+  auto end1 = std::chrono::high_resolution_clock::now();
+  cycles = test_duration_in_cycles;
+  double nanoseconds1 =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - begin1)
+          .count();
+  auto begin2 = std::chrono::high_resolution_clock::now();
+#ifdef __x86_64__
+  __asm volatile("cyclemeasure2:\n dec %[counter] \n jnz cyclemeasure2 \n"
+                 : /* read/write reg */ [counter] "+r"(cycles));
+#elif defined(__aarch64__)
+  __asm volatile(
+      "cyclemeasure2:\nsubs %[counter],%[counter],#1\nbne cyclemeasure2\n "
+      : /* read/write reg */ [counter] "+r"(cycles));
+#endif
+  auto end2 = std::chrono::high_resolution_clock::now();
+  double nanoseconds2 =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - begin2)
+          .count();
+  double nanoseconds = (nanoseconds1 - nanoseconds2);
+  std::cout << nanoseconds << " ns" << std::endl;
+  std::cout << test_duration_in_cycles << " cycles" << std::endl;
 
   double frequency = double(test_duration_in_cycles) / nanoseconds;
   std::cout << frequency << "GHz" << std::endl;
 }
 
-int main() { measure_frequency(); }
+int main() {
+  for (int i = 0; i < 10; i++) {
+    std::cout << "test " << i << std::endl;
+    measure_frequency();
+    std::cout << std::endl;
+  }
+}
