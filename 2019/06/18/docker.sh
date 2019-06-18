6964788cda790ae1f30109728f4ee8dfb88495af
Daniel Lemire <lemire@gmail.com>
2019-06-18 15:36:42 -0400

    Preparing.

---
+++ 2019/06/18/docker.sh
@@ -0,0 +1,2 @@
+docker build . -t testshuffle
+docker run -t testshuffle
---
+++ 2019/06/18/getline.cpp
@@ -0,0 +1,46 @@
+
+#include "benchmark.h"
+#include <istream>
+#include <string>
+#include <random>
+#include <sstream>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void rewind(std::istream & is) {
+  is.clear();
+  is.seekg(0,std::ios::beg);
+}
+
+__attribute__ ((noinline))
+size_t sum_line_lengths(std::istream & is) {
+  std::string line;
+  size_t sumofalllinelengths{0};
+  while(getline(is, line)) {
+    sumofalllinelengths += line.size();
+  }
+  return sumofalllinelengths;
+}
+
+void get_line_benchmark(size_t size) {
+    printf(" %s\n", __PRETTY_FUNCTION__);
+    bool verbose = true;
+    printf("generating and parsing input having %zu bytes.\n", size);
+    int repeat = 10;
+    std::vector<char> blob(size + 1, 'a');
+    blob [size] = '\0';
+    // add break lines at random
+    // average line length should be 80
+    for(size_t br = 0; br < size / 80; br++) {
+      blob[rand() % size] = '\n';
+    }
+    std::istringstream ss(std::string(blob.data(),size + 1));
+    BEST_TIME_NS(sum_line_lengths(ss), rewind(ss), repeat, size, verbose);
+}
+
+int main(int argc, char** argv) {
+  get_line_benchmark(100000);
+  return EXIT_SUCCESS;
+}
