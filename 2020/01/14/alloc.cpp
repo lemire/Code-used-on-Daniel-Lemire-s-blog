5c7abb9499cdc01c4a1e1029ac0939649fc4b96c
Daniel Lemire <lemire@gmail.com>
2020-01-14 12:31:24 -0500

    Saving...

--- 2020/01/14/alloc.cpp
+++ 2020/01/14/alloc.cpp
@@ -24,6 +24,19 @@ __attribute__((noinline))void benchmemset(size_t s,  char * buf) {
             << std::endl;
 }
 
+__attribute__((noinline))char * benchmemcpy(size_t s,  char * buf) {
+  char * newbuf = new char[s]();
+
+  std::chrono::time_point<std::chrono::steady_clock> start_clock =
+      std::chrono::steady_clock::now();
+  memcpy(newbuf, buf,s);
+  std::chrono::time_point<std::chrono::steady_clock> end_clock =
+      std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed = end_clock - start_clock;
+  std::cout << "memcpy: " << (s / (1024. * 1024 * 1024.)) / elapsed.count() << " GB/s"
+            << std::endl;
+  return newbuf;
+}
 
 int main() {
   for (size_t i = 268435456; i < 1073741824; i *= 2) {
@@ -34,7 +47,9 @@ int main() {
     for (int z = 0; z < 3; z++) {
       char *b = bench(i);
       benchmemset(i, b);
+      char *b2 = benchmemcpy(i,b);
       delete[] b;
+      delete[] b2;
     }
   }
 }
