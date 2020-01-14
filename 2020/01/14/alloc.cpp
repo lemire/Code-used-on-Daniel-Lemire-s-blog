aa9a53f4ea778a264e2f64f302a7841dc2279753
Daniel Lemire <lemire@gmail.com>
2020-01-14 14:38:45 -0500

    Putting fences.

--- 2020/01/14/alloc.cpp
+++ 2020/01/14/alloc.cpp
@@ -6,6 +6,7 @@ __attribute__((noinline)) char *bench(size_t s) {
   std::chrono::time_point<std::chrono::steady_clock> start_clock =
       std::chrono::steady_clock::now();
   char *buf1 = new char[s]();
+  asm volatile ("" : : : "memory");
   std::chrono::time_point<std::chrono::steady_clock> end_clock =
       std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed = end_clock - start_clock;
@@ -15,6 +16,20 @@ __attribute__((noinline)) char *bench(size_t s) {
   return buf1;
 }
 
+
+__attribute__((noinline)) char *benchnothrow(size_t s) {
+  std::chrono::time_point<std::chrono::steady_clock> start_clock =
+      std::chrono::steady_clock::now();
+  char *buf1 = new(std::nothrow) char[s]();
+  asm volatile ("" : : : "memory");
+  std::chrono::time_point<std::chrono::steady_clock> end_clock =
+      std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed = end_clock - start_clock;
+  std::cout << "new(std::nothrow) char[s](): "
+            << (s / (1024. * 1024 * 1024.)) / elapsed.count() << " GB/s"
+            << std::endl;
+  return buf1;
+}
 __attribute__((noinline)) char *benchallocandtouch(size_t s) {
   std::chrono::time_point<std::chrono::steady_clock> start_clock =
       std::chrono::steady_clock::now();
@@ -22,6 +37,7 @@ __attribute__((noinline)) char *benchallocandtouch(size_t s) {
   for (size_t i = 0; i < s; i += 4096) {
     buf1[i] = 0;
   }
+  asm volatile ("" : : : "memory");
   std::chrono::time_point<std::chrono::steady_clock> end_clock =
       std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed = end_clock - start_clock;
@@ -35,6 +51,7 @@ __attribute__((noinline)) void benchmemset(size_t s, char *buf) {
   std::chrono::time_point<std::chrono::steady_clock> start_clock =
       std::chrono::steady_clock::now();
   memset(buf, 0, s);
+  asm volatile ("" : : : "memory");
   std::chrono::time_point<std::chrono::steady_clock> end_clock =
       std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed = end_clock - start_clock;
@@ -44,10 +61,11 @@ __attribute__((noinline)) void benchmemset(size_t s, char *buf) {
 
 __attribute__((noinline)) char *benchmemcpy(size_t s, char *buf) {
   char *newbuf = new char[s]();
-
+  asm volatile ("" : : : "memory");
   std::chrono::time_point<std::chrono::steady_clock> start_clock =
       std::chrono::steady_clock::now();
   memcpy(newbuf, buf, s);
+  asm volatile ("" : : : "memory");
   std::chrono::time_point<std::chrono::steady_clock> end_clock =
       std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed = end_clock - start_clock;
@@ -63,12 +81,14 @@ int main() {
 
     for (int z = 0; z < 3; z++) {
       char *b0 = benchallocandtouch(i);
+      char *bz = benchnothrow(i);
       char *b = bench(i);
       benchmemset(i, b);
       char *b2 = benchmemcpy(i, b);
       delete[] b;
       delete[] b2;
       delete[] b0;
+      delete[] bz;
       std::cout << std::endl;
     }
     std::cout << std::endl;
