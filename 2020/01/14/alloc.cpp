c6bd729dd981210509cee97076076094fa012b16
Oliver Schönrock <oliver@openbrackets.net>
2020-01-15 11:05:07 +0000

    centralising timing same for memory fence hack pulling out constants formatting into clean columns

--- 2020/01/14/alloc.cpp
+++ 2020/01/14/alloc.cpp
@@ -1,124 +1,106 @@
 #include <chrono>
-#include <cstring>
 #include <cstdlib>
+#include <cstring>
+#include <iomanip>
 #include <iostream>
-__attribute__((noinline)) char *benchcalloc(size_t s) {
-  std::chrono::time_point<std::chrono::steady_clock> start_clock =
-      std::chrono::steady_clock::now();
-  char *buf1 = (char*)calloc(s, sizeof(char));
+
+#define MEMORY_FENCE asm volatile("" : : : "memory");
+
+constexpr std::size_t MB        = 1024 * 1024;
+constexpr std::size_t page_size = 4096;
+
+using clk        = std::chrono::high_resolution_clock;
+using time_point = std::chrono::time_point<clk>;
+using dur_double = std::chrono::duration<double>;
+using std::chrono::duration_cast;
+
+class Timer {
+public:
+  Timer(size_t size, const std::string& cmd) : _size{size}, _cmd{cmd}, _start_clock{clk::now()} {};
+
+  ~Timer() {
+    auto duration  = clk::now() - _start_clock;
+    auto elapsed_s = duration_cast<dur_double>(duration).count();
+    // waiting for std::format!
+    std::printf("%7lu pages %5lu MB   %-30s %9.3f ms  %7.1f GB/s \n", _size / page_size, _size / MB,
+                _cmd.data(), elapsed_s * 1000, _size / (1024. * 1024 * 1024.) / elapsed_s);
+    ;
+  };
+
+private:
+  time_point  _start_clock;
+  size_t      _size;
+  std::string _cmd;
+};
+
+__attribute__((noinline)) char* benchcalloc(size_t s) {
+  auto  t    = Timer{s, "calloc"};
+  char* buf1 = (char*)calloc(s, sizeof(char));
   // we need to touch the memory because calloc will cheat
-  for (size_t i = 0; i < s; i += 4096) {
-   buf1[i] = 0;
-  }
+  for (size_t i = 0; i < s; i += page_size) buf1[i] = 0;
   buf1[s - 1] = 0;
-  asm volatile ("" : : : "memory"); // memory fence because we are paranoid
-  std::chrono::time_point<std::chrono::steady_clock> end_clock =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed = end_clock - start_clock;
-  std::cout << "calloc             : "
-            << (s / (1024. * 1024 * 1024.)) / elapsed.count() << " GB/s"
-            << std::endl;
-  return buf1;
-}
-__attribute__((noinline)) char *bench(size_t s) {
-  std::chrono::time_point<std::chrono::steady_clock> start_clock =
-      std::chrono::steady_clock::now();
-  char *buf1 = new char[s]();
-  asm volatile ("" : : : "memory");
-  std::chrono::time_point<std::chrono::steady_clock> end_clock =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed = end_clock - start_clock;
-  std::cout << "new char[s]()      : "
-            << (s / (1024. * 1024 * 1024.)) / elapsed.count() << " GB/s"
-            << std::endl;
+  MEMORY_FENCE
   return buf1;
 }
 
-
-__attribute__((noinline)) char *benchnothrow(size_t s) {
-  std::chrono::time_point<std::chrono::steady_clock> start_clock =
-      std::chrono::steady_clock::now();
-  char *buf1 = new(std::nothrow) char[s]();
-  asm volatile ("" : : : "memory");
-  std::chrono::time_point<std::chrono::steady_clock> end_clock =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed = end_clock - start_clock;
-  std::cout << "new(std::nothrow) char[s](): "
-            << (s / (1024. * 1024 * 1024.)) / elapsed.count() << " GB/s"
-            << std::endl;
+__attribute__((noinline)) char* bench(size_t s) {
+  auto  t    = Timer{s, "new char[s]()"};
+  char* buf1 = new char[s]();
+  MEMORY_FENCE
   return buf1;
 }
-__attribute__((noinline)) char *benchallocandtouch(size_t s) {
-  std::chrono::time_point<std::chrono::steady_clock> start_clock =
-      std::chrono::steady_clock::now();
-  char *buf1 = new char[s];
-  for (size_t i = 0; i < s; i += 4096) {
-   buf1[i] = 0;
-  }
+
+__attribute__((noinline)) char* benchnothrow(size_t s) {
+  auto  t    = Timer{s, "new(std::nothrow) char[s]()"};
+  char* buf1 = new (std::nothrow) char[s]();
+  MEMORY_FENCE
+  return buf1;
+}
+
+__attribute__((noinline)) char* benchallocandtouch(size_t s) {
+  auto  t    = Timer{s, "new char[s] + touch"};
+  char* buf1 = new char[s];
+  for (size_t i = 0; i < s; i += page_size) buf1[i] = 0;
   buf1[s - 1] = 0;
-  asm volatile ("" : : : "memory");
-  std::chrono::time_point<std::chrono::steady_clock> end_clock =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed = end_clock - start_clock;
-  std::cout << "new char[s] + touch: "
-            << (s / (1024. * 1024 * 1024.)) / elapsed.count() << " GB/s"
-            << std::endl;
+  MEMORY_FENCE
   return buf1;
 }
 
-__attribute__((noinline)) void benchmemset(size_t s, char *buf) {
-  std::chrono::time_point<std::chrono::steady_clock> start_clock =
-      std::chrono::steady_clock::now();
+__attribute__((noinline)) void benchmemset(size_t s, char* buf) {
+  auto t = Timer{s, "memset"};
   memset(buf, 0, s);
-  asm volatile ("" : : : "memory");
-  std::chrono::time_point<std::chrono::steady_clock> end_clock =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed = end_clock - start_clock;
-  std::cout << "memset: " << (s / (1024. * 1024 * 1024.)) / elapsed.count()
-            << " GB/s" << std::endl;
+  MEMORY_FENCE
 }
 
-__attribute__((noinline)) char *benchmemcpy(size_t s, char *buf) {
-  char *newbuf = new char[s]();
-  asm volatile ("" : : : "memory");
-  std::chrono::time_point<std::chrono::steady_clock> start_clock =
-      std::chrono::steady_clock::now();
+__attribute__((noinline)) char* benchmemcpy(size_t s, char* buf) {
+  char* newbuf = new char[s]();
+  MEMORY_FENCE
+  auto t = Timer{s, "memcpy"};
   memcpy(newbuf, buf, s);
-  asm volatile ("" : : : "memory");
-  std::chrono::time_point<std::chrono::steady_clock> end_clock =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed = end_clock - start_clock;
-  std::cout << "memcpy: " << (s / (1024. * 1024 * 1024.)) / elapsed.count()
-            << " GB/s" << std::endl;
+  MEMORY_FENCE
   return newbuf;
 }
 
 int main() {
-  for (size_t i = 268435456; i < 1073741824; i *= 2) {
-    std::cout << "page count: " << i / 4096
-              << ", volume:" << (i / (1024. * 1024)) << " MB" << std::endl;
-
-    for (int z = 0; z < 3; z++) {
-      char *bc = benchcalloc(i);
+  for (size_t i = 256 * MB; i <= 1024 * MB; i *= 2) {
+    for (int z = 0; z < 1; z++) {
+      char* bc = benchcalloc(i);
       free(bc);
-      char *b0 = benchallocandtouch(i);
-      char *bz = benchnothrow(i);
-      char *b = bench(i);
+      char* b0 = benchallocandtouch(i);
+      char* bz = benchnothrow(i);
+      char* b  = bench(i);
       benchmemset(i, b);
-      char *b2 = benchmemcpy(i, b);
+      char* b2 = benchmemcpy(i, b);
       delete[] b;
       delete[] b2;
       delete[] b0;
       delete[] bz;
-      std::cout << std::endl;
+      std::cout << '\n';
     }
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
   }
-  char *buftmp = new(std::nothrow) char[10000000000000L]; // just to show that it can be done
-  if(buftmp != nullptr) {
-    std::cout << "I just allocated a gigantic array." << std::endl;
+  char* buftmp = new (std::nothrow) char[10'000'000'000'000L]; // just to show that it can be done
+  if (buftmp != nullptr) {
+    std::cout << "I just allocated a gigantic array." << '\n';
   }
   delete[] buftmp;
 }
