748a8a683b2f9d3fc04b25fd5d4b27168458cebe
Daniel Lemire <lemire@gmail.com>
2020-01-30 16:44:38 -0500

    Format

--- 2020/01/30/costofthread.cpp
+++ 2020/01/30/costofthread.cpp
@@ -7,19 +7,18 @@
 #include <thread>
 #include <vector>
 
-
-using clk        = std::chrono::high_resolution_clock;
+using clk = std::chrono::high_resolution_clock;
 using time_point = std::chrono::time_point<clk>;
 using dur_double = std::chrono::duration<double>;
 using std::chrono::duration_cast;
 
 class Timer {
 public:
-  Timer(const std::string& cmd) :  _cmd{cmd}, _start{clk::now()} {};
+  Timer(const std::string &cmd) : _cmd{ cmd }, _start{ clk::now() } {};
 
   double time_ns() {
-    auto duration   = clk::now() - _start;
-    auto elapsed_s  = duration_cast<dur_double>(duration).count();
+    auto duration = clk::now() - _start;
+    auto elapsed_s = duration_cast<dur_double>(duration).count();
     return elapsed_s * 1000 * 1000 * 1000;
   }
 
@@ -27,40 +26,40 @@ public:
 
 private:
   std::string _cmd;
-  time_point  _start;
+  time_point _start;
 };
 
-size_t counter{0};
+size_t counter{ 0 };
 
 double startthread() {
-    auto t = Timer{ __FUNCTION__};
-    auto mythread = std::thread([] {counter++;
-        });
-    mythread.join();
-    return t.time_ns();
+  auto t = Timer{ __FUNCTION__ };
+  auto mythread = std::thread([] { counter++; });
+  mythread.join();
+  return t.time_ns();
 }
-
 
 #include <cmath>
 
 double mean(const std::vector<double> &v) {
-    if(v.size() == 0) return 0;// right.
-    double sum = 0;
-    for (auto &x: v) {
-        sum += x;
-    }
-    return sum / v.size();
+  if (v.size() == 0)
+    return 0; // right.
+  double sum = 0;
+  for (auto &x : v) {
+    sum += x;
+  }
+  return sum / v.size();
 }
 
 double std_error(const std::vector<double> &v, double meanval) {
-  if(v.size() == 0) return 0;// right
-    double s {0};
+  if (v.size() == 0)
+    return 0; // right
+  double s{ 0 };
 
-    for (auto &x: v) {
-        s += (x - meanval) * (x - meanval);
-    }
+  for (auto &x : v) {
+    s += (x - meanval) * (x - meanval);
+  }
 
-    return std::sqrt(s / v.size());
+  return std::sqrt(s / v.size());
 }
 int main() {
   std::vector<double> timings;
@@ -71,7 +70,8 @@ int main() {
   double stde = std_error(timings, meanval);
   std::cout.precision(6);
   std::cout << meanval << " ns +/- " << stde << std::endl;
-  std::cout << "min: " << *min_element(timings.begin(), timings.end()) << std::endl;
-  std::cout << "max: " << *max_element(timings.begin(), timings.end()) << std::endl;
-
+  std::cout << "min: " << *min_element(timings.begin(), timings.end())
+            << std::endl;
+  std::cout << "max: " << *max_element(timings.begin(), timings.end())
+            << std::endl;
 }
