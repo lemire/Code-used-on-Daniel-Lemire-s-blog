983489fa039a9b7c1f87e4521daed189656360dc
Daniel Lemire <lemire@gmail.com>
2020-03-11 17:28:24 -0400

    Modif.

--- 2020/03/11/implicitrealloc.cpp
+++ 2020/03/11/implicitrealloc.cpp
@@ -1,14 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
+void demo() {
+  char * array = new char[20]; 
+  delete[] array; 
+  char * newarray = new char[21];
+  if(newarray == array) {
+    std::cout << "they are the same!" << std::endl;
+  }
+}
 
 int main() {
-  char * array = new char[20];
-  memset(array,12,20);
-  delete[] array;
-  array = new char[20];
-  for(size_t i = 0; i < 21; i++) {
-    printf("%d ", (int)array[i]);
-  }
-  printf("\n");
+  demo();
 }
