6f0566c8cc5ba2c7ea3a52c2460e35842a4f547e
Daniel Lemire <lemire@gmail.com>
2020-03-17 21:32:24 -0400

    Saving...

--- 2020/03/17/README.md
+++ 2020/03/17/README.md
@@ -1,42 +1,60 @@
-```
 trial 0
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.0967 ns
+cycles: 2.33773
+2 * _mm_storeu_si128      : 2.02564 ns
+cycles: 2.08742
+ratio 1.03508
 trial 1
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.10647 ns
+cycles: 2.33944
+2 * _mm_storeu_si128      : 2.03077 ns
+cycles: 2.08205
+ratio 1.03728
 trial 2
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.11136 ns
+cycles: 2.3431
+2 * _mm_storeu_si128      : 2.03297 ns
+cycles: 2.08449
+ratio 1.03856
 trial 3
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.11868 ns
+cycles: 2.3431
+2 * _mm_storeu_si128      : 2.04518 ns
+cycles: 2.08571
+ratio 1.03594
 trial 4
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.12112 ns
+cycles: 2.34188
+2 * _mm_storeu_si128      : 2.04762 ns
+cycles: 2.08034
+ratio 1.0359
 trial 5
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.13089 ns
+cycles: 2.34286
+2 * _mm_storeu_si128      : 2.05739 ns
+cycles: 2.08864
+ratio 1.03573
 trial 6
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.13822 ns
+cycles: 2.33871
+2 * _mm_storeu_si128      : 2.06252 ns
+cycles: 2.08669
+ratio 1.0367
 trial 7
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.14554 ns
+cycles: 2.34042
+2 * _mm_storeu_si128      : 2.06984 ns
+cycles: 2.0862
+ratio 1.03657
 trial 8
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
+_mm256_storeu_si256      : 2.15043 ns
+cycles: 2.34139
+2 * _mm_storeu_si128      : 2.07961 ns
+cycles: 2.09158
+ratio 1.03405
 trial 9
-_mm256_storeu_si256      : 2.04724 ns
-2 * _mm_storeu_si128      : 1.88189 ns
-ratio 1.08787
-```
+_mm256_storeu_si256      : 2.15775 ns
+cycles: 2.34408
+2 * _mm_storeu_si128      : 2.08205 ns
+cycles: 2.08718
+ratio 1.03636
--- 2020/03/17/alignbench.cpp
+++ 2020/03/17/alignbench.cpp
@@ -1,3 +1,10 @@
+
+#ifndef __linux__
+#warning "We require Linux."
+#else
+#include "linux-perf-events.h"
+#endif
+
 #include <chrono>
 #include <cstring>
 #include <iostream>
@@ -6,34 +13,44 @@
 #include "align_alloc.h"
 
 void demo() {
-  // allocate an 8kB, cache-page aligned block
-  size_t N = 8192;
+  // allocate cache-page aligned block
+  size_t N = 256 * 1024;
   char *buffer = (char *)aligned_malloc(64, N);
   memset(buffer, 1, N);
-  // we are going to
-
+  size_t hits = (N - 64) / 64;
+  std::vector<int> evts;
+  evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+  evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+  LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+  std::vector<unsigned long long> results;
+  results.resize(evts.size());
   // if you hit unlucky, unlucky + 64,
   // you are exactly 16 bytes away from the
   // cache-line boundary
-  char *unlucky = buffer + 48;
+  char * const unlucky = buffer + 48;
   __m256i vec = _mm256_setzero_si256();
   std::chrono::time_point<std::chrono::steady_clock> start_clock, end_clock;
   std::chrono::duration<double> elapsed = std::chrono::duration<double>::max();
   size_t maxrepeat = 500;
+  size_t bestcycles = 10000000000000;
   for (int time = 0; time < maxrepeat; time++) {
     asm volatile("" : : : "memory");
     start_clock = std::chrono::steady_clock::now();
-    for (size_t i = 0; i < 127; i++) {
+    unified.start();
+    for (size_t i = 0; i < hits; i++) {
       _mm256_storeu_si256((__m256i *)(unlucky + i * 64), vec);
     }
+    unified.end(results);
     end_clock = std::chrono::steady_clock::now();
     elapsed =
         end_clock - start_clock < elapsed ? end_clock - start_clock : elapsed;
+    bestcycles = results[0] < bestcycles ? results[0] : bestcycles;
   }
   std::cout << "_mm256_storeu_si256      : "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed)
                        .count() /
-                   127.0 << " ns" << std::endl;
+                   double(hits) << " ns" << std::endl;
+  std::cout << "cycles: " << double(bestcycles) / double(hits) << std::endl;
   std::chrono::duration<double> elapsed128 =
       std::chrono::duration<double>::max();
   __m128i vec128 = _mm_setzero_si128();
@@ -41,18 +58,23 @@ void demo() {
 
     asm volatile("" : : : "memory");
     start_clock = std::chrono::steady_clock::now();
-    for (size_t i = 0; i < 127; i++) {
+    unified.start();
+    for (size_t i = 0; i < hits; i++) {
       _mm_storeu_si128((__m128i *)(unlucky + i * 64), vec128);
       _mm_storeu_si128((__m128i *)(unlucky + i * 64 + 16), vec128);
     }
+    unified.end(results);
+ 
     end_clock = std::chrono::steady_clock::now();
     elapsed128 = end_clock - start_clock < elapsed128 ? end_clock - start_clock
                                                       : elapsed128;
-  }
+    bestcycles = results[0] < bestcycles ? results[0] : bestcycles;
+   }
   std::cout << "2 * _mm_storeu_si128      : "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed128)
                        .count() /
-                   127.0 << " ns" << std::endl;
+                   double(hits) << " ns" << std::endl;
+  std::cout << "cycles: " << double(bestcycles) / double(hits) << std::endl;
   std::cout << "ratio "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed)
                        .count() *
