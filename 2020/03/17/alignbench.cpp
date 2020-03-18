24eee8c1367b4ade6b472932b647039c4f6f6b65
Daniel Lemire <lemire@gmail.com>
2020-03-17 20:56:52 -0400

    Updated.

--- 2020/03/17/alignbench.cpp
+++ 2020/03/17/alignbench.cpp
@@ -23,42 +23,47 @@ void demo() {
   for (int time = 0; time < maxrepeat; time++) {
     asm volatile("" : : : "memory");
     start_clock = std::chrono::steady_clock::now();
-    for (size_t i = 0; i < 128; i++) {
+    for (size_t i = 0; i < 127; i++) {
       _mm256_storeu_si256((__m256i *)(unlucky + i * 64), vec);
     }
     end_clock = std::chrono::steady_clock::now();
     elapsed =
         end_clock - start_clock < elapsed ? end_clock - start_clock : elapsed;
   }
-  std::cout
-      << "_mm256_storeu_si256      : "
-      << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count() /
-             128.0
-      << " ns" << std::endl;
+  std::cout << "_mm256_storeu_si256      : "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed)
+                       .count() /
+                   127.0 << " ns" << std::endl;
+  std::chrono::duration<double> elapsed128 =
+      std::chrono::duration<double>::max();
   __m128i vec128 = _mm_setzero_si128();
   for (int time = 0; time < maxrepeat; time++) {
 
     asm volatile("" : : : "memory");
     start_clock = std::chrono::steady_clock::now();
-    for (size_t i = 0; i < 128; i++) {
+    for (size_t i = 0; i < 127; i++) {
       _mm_storeu_si128((__m128i *)(unlucky + i * 64), vec128);
       _mm_storeu_si128((__m128i *)(unlucky + i * 64 + 16), vec128);
     }
     end_clock = std::chrono::steady_clock::now();
-    elapsed =
-        end_clock - start_clock < elapsed ? end_clock - start_clock : elapsed;
+    elapsed128 = end_clock - start_clock < elapsed128 ? end_clock - start_clock
+                                                      : elapsed128;
   }
-  std::cout
-      << "2 * _mm_storeu_si128      : "
-      << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count() /
-             128.0
-      << " ns" << std::endl;
+  std::cout << "2 * _mm_storeu_si128      : "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed128)
+                       .count() /
+                   127.0 << " ns" << std::endl;
+  std::cout << "ratio "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed)
+                       .count() *
+                   1.0 / std::chrono::duration_cast<std::chrono::nanoseconds>(
+                             elapsed128).count() << std::endl;
   aligned_free(buffer);
 }
 
 int main() {
   for (int k = 0; k < 10; k++) {
-    std::cout << "trial "<< k << std::endl;
+    std::cout << "trial " << k << std::endl;
     demo();
   }
   return EXIT_SUCCESS;
