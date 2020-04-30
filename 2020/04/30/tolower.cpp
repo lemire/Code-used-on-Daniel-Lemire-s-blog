3450f6f5037add516fd785d31e2fa25cd7a07460
Daniel Lemire <lemire@gmail.com>
2020-04-30 17:24:39 -0400

    Branchless version.

--- 2020/04/30/tolower.cpp
+++ 2020/04/30/tolower.cpp
@@ -87,28 +87,29 @@ bool is_ascii_equal_ignoring_case(const char *input1, const char *input2,
 // compare two 8-byte strings. If any non-ASCII, return false.
 // Otherwise, compare as
 bool compare_ascii(uint64_t w1, uint64_t w2) {
-    const uint64_t diff = w1 ^ w2; // only valid diff at 0x20
-    if ((diff & 0xdfdfdfdfdfdfdfdf) != 0) { //  ~0x20 = 0xdf
-        return false;
-    }
-    // check if any non-ascii
-    const uint64_t any_non_ascii = (w1 | w2) & packed_byte(0x80);
-    if (any_non_ascii) {
-        return false;
-    }
+  const uint64_t diff = w1 ^ w2;          // only valid diff at 0x20
+  if ((diff & 0xdfdfdfdfdfdfdfdf) != 0) { //  ~0x20 = 0xdf
+    return false;
+  }
+  // check if any non-ascii
+  const uint64_t any_non_ascii = (w1 | w2) & packed_byte(0x80);
+  if (any_non_ascii) {
+    return false;
+  }
 
-    // validate if w1 contains only A-Z/a-z chars (then w2 must also be in this range)
-    w1 |= packed_byte(0x20);
+  // validate if w1 contains only A-Z/a-z chars (then w2 must also be in this
+  // range)
+  w1 |= packed_byte(0x20);
 
-    const uint64_t A = w1 + packed_byte(128 - 'A');
-    const uint64_t Z = w1 + packed_byte(128 - 'Z' - 1);
-    const uint64_t mask_lower = (A ^ Z) & packed_byte(0x80);
+  const uint64_t A = w1 + packed_byte(128 - 'A');
+  const uint64_t Z = w1 + packed_byte(128 - 'Z' - 1);
+  const uint64_t mask_lower = (A ^ Z) & packed_byte(0x80);
 
-    return mask_lower == packed_byte(0x80);
+  return mask_lower == packed_byte(0x80);
 }
 
 bool is_ascii_equal_ignoring_case_mula(const char *input1, const char *input2,
-                                  size_t len) {
+                                       size_t len) {
   size_t i = 0;
   for (; i + sizeof(uint64_t) <= len; i += sizeof(uint64_t)) {
     uint64_t w1, w2;
@@ -128,30 +129,30 @@ bool is_ascii_equal_ignoring_case_mula(const char *input1, const char *input2,
   }
   return true;
 }
-// are they ASCII *and* identical up to case?
+// are they ASCII *and* identical up to case, or not empty?
 bool equal_ascii_ignore_case(uint64_t w1, uint64_t w2) {
-    const uint64_t diff = w1 ^ w2; // only valid diff at 0x20
-    if ((diff & 0xdfdfdfdfdfdfdfdf) != 0) //  ~0x20 = 0xdf
-    {
-        return false;
-    }
-    const uint64_t is_diff = (diff << 2);
-    // check if any non-ascii
-    const uint64_t any_non_ascii = (w1 | w2) & packed_byte(0x80);
-    if (any_non_ascii) {
-        // for safety, reject if any has non-ASCII
-        return false;
-    }
-    w1 |= packed_byte(0x20); // if it were in A-Z, it becomes a-z, otherwise, it remains there
-    const uint64_t A = w1 + packed_byte(128 - 'a');
-    const uint64_t Z = w1 + packed_byte(128 - 'z' - 1);
-    const uint64_t mask_lower = (A ^ Z) & packed_byte(0x80);
-    // mask_lower indicate [a-zA-Z], check that it matches our diff
-    return (mask_lower & is_diff) == is_diff;
+  const uint64_t diff = w1 ^ w2;        // only valid diff at 0x20
+  //if ((diff & 0xdfdfdfdfdfdfdfdf) != 0) //  ~0x20 = 0xdf
+  //{
+  //  return false;
+  //}
+  // is_diff is where we have a difference, any bit set outside 0x80 will get a false return
+  const uint64_t is_diff = (diff << 2) | (diff >> (64 - 2 ));
+  // check if any non-ascii
+  const uint64_t non_ascii = (w1 | w2) & packed_byte(0x80);
+  const uint64_t ascii = ~non_ascii;
+  w1 |= packed_byte(
+      0x20); // if it were in A-Z, it becomes a-z, otherwise, a-z is unchanged
+  w1 &= packed_byte(0x7f); // set high bits to zero so the following work
+  const uint64_t A = w1 + packed_byte(128 - 'a');
+  const uint64_t Z = w1 + packed_byte(128 - 'z' - 1);
+  // mask_lower indicate [a-zA-Z], check that it matches our diff
+  const uint64_t mask_lower = ((A ^ Z) & packed_byte(0x80)) & ascii;
+  return (mask_lower & is_diff) == is_diff;
 }
 
 bool is_ascii_equal_ignoring_case_mula2(const char *input1, const char *input2,
-                                  size_t len) {
+                                        size_t len) {
   size_t i = 0;
   for (; i + sizeof(uint64_t) <= len; i += sizeof(uint64_t)) {
     uint64_t w1, w2;
@@ -171,7 +172,6 @@ bool is_ascii_equal_ignoring_case_mula2(const char *input1, const char *input2,
   }
   return true;
 }
-
 
 void demo() {
   srand(time(NULL));
@@ -223,8 +223,8 @@ void demo() {
     std::cout << t.time_ns() / N << std::endl;
     std::cout << "mula ";
     t = Timer{"mula"};
-    is_the_same = (is_ascii_equal_ignoring_case_mula((const char *)random.data(),
-                               (const char *)randomlower.data(), N));
+    is_the_same = (is_ascii_equal_ignoring_case_mula(
+        (const char *)random.data(), (const char *)randomlower.data(), N));
     if (!is_the_same) {
       std::cerr << "bug" << std::endl;
       abort();
@@ -232,8 +232,8 @@ void demo() {
     std::cout << t.time_ns() / N << std::endl;
     std::cout << "mula (2) ";
     t = Timer{"mula (2) "};
-    is_the_same = (is_ascii_equal_ignoring_case_mula2((const char *)random.data(),
-                               (const char *)randomlower.data(), N));
+    is_the_same = (is_ascii_equal_ignoring_case_mula2(
+        (const char *)random.data(), (const char *)randomlower.data(), N));
     if (!is_the_same) {
       std::cerr << "bug" << std::endl;
       abort();
