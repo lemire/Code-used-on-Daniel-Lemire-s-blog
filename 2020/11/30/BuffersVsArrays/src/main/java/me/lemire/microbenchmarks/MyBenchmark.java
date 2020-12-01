15d1c42c187d92e2523d03612c88f4cd52b29f29
Daniel Lemire <lemire@gmail.com>
2020-12-01 11:51:44 -0500

    Saving.

--- 2020/11/30/BuffersVsArrays/src/main/java/me/lemire/microbenchmarks/MyBenchmark.java
+++ 2020/11/30/BuffersVsArrays/src/main/java/me/lemire/microbenchmarks/MyBenchmark.java
@@ -22,20 +22,22 @@ public class MyBenchmark {
         int[] array = new int[N];
         IntBuffer buffer = IntBuffer.allocate(N);
         IntBuffer buffer_crazy = ByteBuffer.allocate(N * Integer.BYTES).order(ByteOrder.nativeOrder()).asIntBuffer();
-
+        IntBuffer buffer_direct = ByteBuffer.allocateDirect(N * Integer.BYTES).order(ByteOrder.nativeOrder()).asIntBuffer();;
 
         public BenchmarkState() {
             for(int k = 0; k  < array.length; k++) { 
                 array[k] = k;
                 buffer.put(k, k);
                 buffer_crazy.put(k, k);
+                buffer_direct.put(k, k);
             }
-            System.out.println("is backed by array: "+buffer.hasArray());
-            System.out.println("is backed by array: "+buffer_crazy.hasArray());     
+            System.out.println("buffer.hasArray(): "+buffer.hasArray());
+            System.out.println("buffer_crazy.hasArray(): "+buffer_crazy.hasArray());     
+            System.out.println("buffer_direct.hasArray(): "+buffer_direct.hasArray());     
             System.out.println("buffer.order: "+buffer.order());
             System.out.println("buffer_crazy.order: "+buffer_crazy.order());   
+            System.out.println("buffer_direct.order: "+buffer_direct.order());   
         }
-
     }
 
     @Benchmark
@@ -56,6 +58,13 @@ public class MyBenchmark {
     public void buffer_crazy(BenchmarkState s) throws IOException {
         for(int k = 0; k  < s.buffer_crazy.limit(); k++) { 
             s.buffer_crazy.put(k, s.buffer_crazy.get(k) + 1);
+        }
+    }
+
+    @Benchmark
+    public void buffer_direct(BenchmarkState s) throws IOException {
+        for(int k = 0; k  < s.buffer_direct.limit(); k++) { 
+            s.buffer_direct.put(k, s.buffer_direct.get(k) + 1);
         }
     }
 }
