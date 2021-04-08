c8e48f81ba883993ab9c7bee9b1372b43baba3d2
Daniel Lemire <lemire@gmail.com>
2021-04-08 10:13:41 -0400

    Cleaner.

--- extra/radix/java/src/main/java/me/lemire/microbenchmarks/algorithms/PartialRadixSort.java
+++ extra/radix/java/src/main/java/me/lemire/microbenchmarks/algorithms/PartialRadixSort.java
@@ -72,6 +72,36 @@ public class PartialRadixSort {
 		}
 	}
 
+
+	public static void newnewPartialRadixSort(int[] data) {
+		final int radix = 8;
+		int shift = 16;
+		int[] copy = new int[data.length];
+		int[] histogram = new int[(1 << radix) + 1];
+		// We want to avoid copying the data, see
+		// https://github.com/RoaringBitmap/RoaringBitmap/issues/470
+		int[] primary = data;
+		int[] secondary = copy;
+		while (shift < 32) {
+			for (int i = 0; i < data.length; ++i) {
+				++histogram[((primary[i] >>> shift) & 0xFF) + 1];
+			}
+			for (int i = 0; i < 1 << radix; ++i) {
+				histogram[i + 1] += histogram[i];
+			}
+			for (int i = 0; i < primary.length; ++i) {
+				secondary[histogram[(primary[i] >>> shift) & 0xFF]++] = primary[i];
+			}
+			// swap
+			int[] tmp = primary;
+			primary = secondary;
+			secondary = tmp;
+			//
+			shift += radix;
+			if(shift < 32) { Arrays.fill(histogram, 0); }
+		}	
+  }
+
 	public static void unrollPartialRadixSort(int[] primary) {
 		final int radix = 8;
 
@@ -185,7 +215,11 @@ public class PartialRadixSort {
 		newPartialRadixSort(state.testArr);
 		blackhole.consume(state.testArr);
 	}
-
+	@Benchmark
+	public void BM_newnew(Blackhole blackhole, BenchmarkState state) {
+		newnewPartialRadixSort(state.testArr);
+		blackhole.consume(state.testArr);
+	}
 	@Benchmark
 	public void BM_unroll(Blackhole blackhole, BenchmarkState state) {
 		unrollPartialRadixSort(state.testArr);
