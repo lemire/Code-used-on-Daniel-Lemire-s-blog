805a49d9741e8bda505dbcf035fc0774c6fa1513
Daniel Lemire <lemire@gmail.com>
2021-05-17 16:29:32 -0400

    Tweaking.

--- 2021/05/17/itoa.cpp
+++ 2021/05/17/itoa.cpp
@@ -1,4 +1,5 @@
 
+#include "m1cycles.h"
 #include <algorithm>
 #include <chrono>
 #include <cstdint>
@@ -8,8 +9,7 @@
 // The function returns a pointer to the start of the integer in ASCII
 // written over the passed buffer.
 // see https://github.com/cassioneri/calendar
-__attribute__ ((noinline))
-char *itoa_neri(char *c, uint32_t n) {
+__attribute__((noinline)) char *itoa_neri(char *c, uint32_t n) {
   uint64_t constexpr p32 = uint64_t(1) << 32;
   while (n >= 10) {
     uint64_t const u = uint64_t(429496730) * n;
@@ -27,8 +27,8 @@ char *itoa_neri(char *c, uint32_t n) {
 // The pointer c should be at then end of a region containing at least 10 bytes.
 // The function returns a pointer to the start of the integer in ASCII
 // written over the passed buffer.
-__attribute__ ((noinline))
-char *itoa_std(char *c, uint32_t n) {
+__attribute__((noinline)) char *itoa_std(char *c, uint32_t n) {
+    size_t count{0};
   while (n >= 10) {
     uint32_t const p = n / 10;
     uint32_t const r = n % 10;
@@ -41,8 +41,7 @@ char *itoa_std(char *c, uint32_t n) {
   return c;
 }
 
-__attribute__ ((noinline))
-char *fake_itoa_std(char *c, uint32_t n) {
+__attribute__((noinline)) char *fake_itoa_std(char *c, uint32_t n) {
   while (n >= 10) {
     n /= 10;
     c--;
@@ -71,19 +70,24 @@ public:
 char buffer[11];
 
 void run_std(uint32_t N) {
-  for (uint32_t i = 0; i < N; i++) {
-    itoa_std(buffer + 10, i);
+  // We go from 20 * 1000 * 1000 to 40 * 1000 * 1000
+  // so that we have a flat number of digits (to ease analysis)
+  uint8_t seed = 0;
+  for (uint32_t i = N; i < 2 * N; i++) {
+    seed = *itoa_std(buffer + 10, i+seed);
   }
 }
 
 void run_fake(uint32_t N) {
-  for (uint32_t i = 0; i < N; i++) {
-    fake_itoa_std(buffer + 10, i);
+  uint8_t seed = 0;
+  for (uint32_t i = N; i < 2 * N; i++) {
+    seed = *fake_itoa_std(buffer + 10, i+seed);
   }
 }
 void run_neri(uint32_t N) {
-  for (uint32_t i = 0; i < N; i++) {
-    itoa_neri(buffer + 10, i);
+  uint8_t seed = 0;
+  for (uint32_t i = N; i < 2 * N; i++) {
+    seed = *itoa_neri(buffer + 10, i+seed);
   }
 }
 
@@ -98,32 +102,90 @@ void demo() {
             << std::endl;
   x = 1e300;
   y = 0;
-  for (size_t i = 0; i < trials; i++) {
-    time.start();
-    run_std(N);
-    auto result = time.stop() * 1.0 / N;
-    y += result;
-    if (result < x) {
-      x = result;
+  {
+#ifdef __APPLE__
+    performance_counters agg_min{1e300};
+    performance_counters agg_avg{0.0};
+#endif
+    for (size_t i = 0; i < trials; i++) {
+#ifdef __APPLE__
+      performance_counters start = get_counters();
+#endif
+      time.start();
+      run_std(N);
+      auto result = time.stop() * 1.0 / N;
+#ifdef __APPLE__
+      performance_counters end = get_counters();
+      performance_counters diff = end - start;
+      agg_min = agg_min.min(diff);
+      agg_avg += diff;
+#endif
+      y += result;
+      if (result < x) {
+        x = result;
+      }
     }
+#ifdef __APPLE__
+    agg_avg /= N * trials;
+    agg_min /= N;
+#endif
+    y = y / trials;
+#ifdef __APPLE__
+    std::cout << "with std     : " << x << " (" << (y - x) * 100 / x << " %)"
+              << std::endl;
+    printf(" %8.2f instructions/int (+/- %3.1f %%) ", agg_min.instructions,
+           (agg_avg.instructions - agg_min.instructions) * 100.0 /
+               agg_min.instructions);
+    printf("\n");
+    printf(" %8.2f cycles/int (+/- %3.1f %%) ", agg_min.cycles,
+           (agg_avg.cycles - agg_min.cycles) * 100.0 / agg_min.cycles);
+    printf("\n");
+#endif
   }
-  y = y / trials;
-  std::cout << "with std     : " << x << " ("
-            << (y - x) * 100 / x << " %)" << std::endl;
+
   x = 1e300;
   y = 0;
-  for (size_t i = 0; i < trials; i++) {
-    time.start();
-    run_fake(N);
-    auto result = time.stop() * 1.0 / N;
-    y += result;
-    if (result < x) {
-      x = result;
+  {
+#ifdef __APPLE__
+    performance_counters agg_min{1e300};
+    performance_counters agg_avg{0.0};
+#endif
+    for (size_t i = 0; i < trials; i++) {
+#ifdef __APPLE__
+      performance_counters start = get_counters();
+#endif
+      time.start();
+      run_fake(N);
+      auto result = time.stop() * 1.0 / N;
+#ifdef __APPLE__
+      performance_counters end = get_counters();
+      performance_counters diff = end - start;
+      agg_min = agg_min.min(diff);
+      agg_avg += diff;
+#endif
+      y += result;
+      if (result < x) {
+        x = result;
+      }
     }
+#ifdef __APPLE__
+    agg_avg /= N * trials;
+    agg_min /= N;
+#endif
+    y = y / trials;
+#ifdef __APPLE__
+    std::cout << "fake         : " << x << " (" << (y - x) * 100 / x << " %)"
+              << std::endl;
+    printf(" %8.2f instructions/int (+/- %3.1f %%) ", agg_min.instructions,
+           (agg_avg.instructions - agg_min.instructions) * 100.0 /
+               agg_min.instructions);
+    printf("\n");
+    printf(" %8.2f cycles/int (+/- %3.1f %%) ", agg_min.cycles,
+           (agg_avg.cycles - agg_min.cycles) * 100.0 / agg_min.cycles);
+    printf("\n");
+#endif
   }
-  y = y / trials;
-  std::cout << "fake         : " << x << " (" << (y - x) * 100 / x
-            << " %)" << std::endl;
+
   x = 1e300;
   y = 0;
   for (size_t i = 0; i < trials; i++) {
@@ -136,8 +198,12 @@ void demo() {
     }
   }
   y = y / trials;
-  std::cout << "neri         : " << x << " (" << (y - x) * 100 / x
-            << " %)" << std::endl;
+  std::cout << "neri         : " << x << " (" << (y - x) * 100 / x << " %)"
+            << std::endl;
 }
 
-int main() { demo();   demo(); }
+int main() {
+  setup_performance_counters();
+  demo();
+  demo();
+}
---
+++ 2021/05/17/m1cycles.h
@@ -0,0 +1,64 @@
+#ifndef M1CYCLES_H
+#define M1CYCLES_H
+#ifdef  __APPLE__
+#include <cstdint>
+
+struct performance_counters {
+  double cycles;
+  double branches;
+  double missed_branches;
+  double instructions;
+  performance_counters(uint64_t c, uint64_t b, uint64_t m, uint64_t i)
+      : cycles(c), branches(b), missed_branches(m), instructions(i) {}
+  performance_counters(double c, double b, double m, double i)
+      : cycles(c), branches(b), missed_branches(m), instructions(i) {}
+  performance_counters(double init)
+      : cycles(init), branches(init), missed_branches(init),
+        instructions(init) {}
+
+  inline performance_counters &operator-=(const performance_counters &other) {
+    cycles -= other.cycles;
+    branches -= other.branches;
+    missed_branches -= other.missed_branches;
+    instructions -= other.instructions;
+    return *this;
+  }
+  inline performance_counters &min(const performance_counters &other) {
+    cycles = other.cycles < cycles ? other.cycles : cycles;
+    branches = other.branches < branches ? other.branches : branches;
+    missed_branches = other.missed_branches < missed_branches
+                          ? other.missed_branches
+                          : missed_branches;
+    instructions =
+        other.instructions < instructions ? other.instructions : instructions;
+    return *this;
+  }
+  inline performance_counters &operator+=(const performance_counters &other) {
+    cycles += other.cycles;
+    branches += other.branches;
+    missed_branches += other.missed_branches;
+    instructions += other.instructions;
+    return *this;
+  }
+
+  inline performance_counters &operator/=(double numerator) {
+    cycles /= numerator;
+    branches /= numerator;
+    missed_branches /= numerator;
+    instructions /= numerator;
+    return *this;
+  }
+};
+
+inline performance_counters operator-(const performance_counters &a,
+                                      const performance_counters &b) {
+  return performance_counters(a.cycles - b.cycles, a.branches - b.branches,
+                              a.missed_branches - b.missed_branches,
+                              a.instructions - b.instructions);
+}
+
+void setup_performance_counters();
+
+extern performance_counters get_counters();
+#endif
+#endif
