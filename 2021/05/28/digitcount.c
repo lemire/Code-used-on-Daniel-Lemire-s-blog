05cf40cea2999da64bf90c1a93d5c11bf47c14e1
Daniel Lemire <lemire@gmail.com>
2021-05-28 21:18:09 -0400

    Added java code.

--- 2021/05/28/digitcount.c
+++ 2021/05/28/digitcount.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int int_log2(uint32_t x) { return 31 - __builtin_clz(x|1); }
 
@@ -7,7 +8,6 @@ int int_log2(uint32_t x) { return 31 - __builtin_clz(x|1); }
 int digit_count(uint32_t x) {
   int l2 = int_log2(x);
   int ans = ((77*l2)>>8);
-
   static uint32_t table[] = {9,      99,      999,      9999,
                              99999, 999999, 9999999, 99999999, 
                              999999999, 0xFFFFFFFF};
@@ -16,7 +16,7 @@ int digit_count(uint32_t x) {
 }
 
 int slow_digit_count(uint32_t x) {
-    if(x == 0){return 1;}
+    if(x == 0) { return 1;}
     static uint32_t table[] = {1,      10,      100,      1000,      10000,
                              100000, 1000000, 10000000, 100000000, 1000000000};
     int ans = 9;
@@ -27,9 +27,10 @@ int slow_digit_count(uint32_t x) {
 
 int main() {
   for(uint32_t val = 1; val != 0; val++) {
-      if(slow_digit_count(val) != digit_count_small_constants(val)) {
-          printf("%u %d %d \n", val, slow_digit_count(val), digit_count_small_constants(val));
-          break;
+      if(slow_digit_count(val) != digit_count(val)) {
+          printf("%u %d %d \n", val, slow_digit_count(val), digit_count(val));
+            return EXIT_FAILURE;
       }
   }
+  return EXIT_SUCCESS;
 }
