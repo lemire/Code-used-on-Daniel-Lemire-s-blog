b4035d4fe191a0574ceb8e9f361bc1d3ff628f36
Daniel Lemire <lemire@gmail.com>
2021-07-10 11:01:15 -0400

    Adding deflate

--- 2021/06/30/script.sh
+++ 2021/06/30/script.sh
@@ -8,6 +8,10 @@ gzip -k -f twitter.json
 zstd -q -f twitter.json
 lz4 -q -k -f --fast twitter.json
 ls -al twitter*
+
+git clone https://github.com/ebiggers/libdeflate.git && cd libdeflate/ &&  make && cd ..
+
+time for i in {1..1000} ; do  libdeflate/gunzip -k -f twitter.json.gz; done
 time for i in {1..1000} ; do  lz4 -q -f -d  twitter.json.lz4 ; done
 time for i in {1..1000} ; do  zstd -q -f -d  twitter.json.zst ; done
 time for i in {1..1000} ; do  gunzip -k -f twitter.json.gz; done
