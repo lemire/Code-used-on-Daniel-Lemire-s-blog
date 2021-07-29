cb7f626df029ce2f92d049a81b6945ae37cf8115
Daniel Lemire <lemire@gmail.com>
2021-07-29 16:38:53 -0400

    Typo.

--- 2021/07/29/memuse.cpp
+++ 2021/07/29/memuse.cpp
@@ -9,7 +9,7 @@ extern "C" {
 int main() {
   size_t pagesize = sysconf(_SC_PAGESIZE);
   constexpr size_t N = 100000000;
-  char *buffer = new char[100000000]; // allocate 10MB
+  char *buffer = new char[N]; // allocate 100MB
   for (size_t i = 0; i < N; i++) {
     buffer[i] = 1;
     if ((i % (N / 100) == 0) || (i == N - 1)) {
