b2978ff1f35fe188259f8b4bfddb0d4e5556acfe
Daniel Lemire <lemire@gmail.com>
2021-11-11 15:35:13 -0500

    New post.

---
+++ 2021/11/11/check.py
@@ -0,0 +1,32 @@
+try:
+    import z3
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', '--user', 'z3-solver'])
+    import z3
+
+y = z3.BitVec("y", 32)
+s = z3.Solver()
+s.add( ( 1 + y ) / 2 >= y )
+if(s.check() == z3.sat):
+    model = s.model()
+    print(model)
+
+
+y = z3.BitVec("y", 8)
+s = z3.Solver()
+s.add( ( 1 + y ) / 2 >= y )
+s.add( y > 0 )
+
+if(s.check() == z3.sat):
+    model = s.model()
+    print(model)
+
+
+s = z3.Solver()
+s.add( ( 1 + y ) / 2 >= y )
+s.add( y > 1 )
+
+if(s.check() == z3.sat):
+    model = s.model()
+    print(model)
