21302193e37ff573218b5aa338279d5b06cf3c42
Daniel Lemire <lemire@gmail.com>
2021-11-17 17:45:40 -0500

    Saving...

---
+++ 2021/11/17/convert.cpp
@@ -0,0 +1,197 @@
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <vector>
+
+uint64_t nano() {
+  return std::chrono::duration_cast<::std::chrono::nanoseconds>(
+             std::chrono::steady_clock::now().time_since_epoch())
+      .count();
+}
+template <typename PROCEDURE>
+double bench(PROCEDURE f, uint64_t threshold = 200'000'000) {
+  uint64_t start = nano();
+  uint64_t finish = start;
+  size_t count{0};
+  for (; finish - start < threshold;) {
+    count += f();
+    finish = nano();
+  }
+  return double(finish - start) / count;
+}
+
+void to_string_linear(uint64_t x, char *out) {
+  out[0] = x / 1000000000000000;
+  x %= 1000000000000000;
+  out[1] = x / 100000000000000;
+  x %= 100000000000000;
+  out[2] = x / 10000000000000;
+  x %= 10000000000000;
+  out[3] = x / 1000000000000;
+  x %= 1000000000000;
+  out[4] = x / 100000000000;
+  x %= 100000000000;
+  out[5] = x / 10000000000;
+  x %= 10000000000;
+  out[6] = x / 1000000000;
+  x %= 1000000000;
+  out[7] = x / 100000000;
+  x %= 100000000;
+  out[8] = x / 10000000;
+  x %= 10000000;
+  out[9] = x / 1000000;
+  x %= 1000000;
+  out[10] = x / 100000;
+  x %= 100000;
+  out[11] = x / 10000;
+  x %= 10000;
+  out[12] = x / 1000;
+  x %= 1000;
+  out[13] = x / 100;
+  x %= 100;
+  out[14] = x / 10;
+  x %= 10;
+  out[15] = x;
+}
+
+// take a 16-digit integer, < 10000000000000000,
+// and write it out.
+void to_string_tree(uint64_t x, char *out) {
+  uint64_t top = x / 100000000;
+  uint64_t bottom = x % 100000000;
+  //
+  uint64_t toptop = top / 10000;
+  uint64_t topbottom = top % 10000;
+  uint64_t bottomtop = bottom / 10000;
+  uint64_t bottombottom = top % 10000;
+  //
+  uint64_t toptoptop = toptop / 100;
+  uint64_t toptopbottom = toptop % 100;
+
+  uint64_t topbottomtop = topbottom / 100;
+  uint64_t topbottombottom = topbottom % 100;
+
+  uint64_t bottomtoptop = bottomtop / 100;
+  uint64_t bottomtopbottom = bottomtop % 100;
+
+  uint64_t bottombottomtop = bottombottom / 100;
+  uint64_t bottombottombottom = bottombottom % 100;
+  //
+  out[0] = toptoptop / 10;
+  out[1] = toptoptop % 10;
+  out[2] = toptopbottom / 10;
+  out[3] = toptopbottom % 10;
+  out[4] = topbottomtop / 10;
+  out[5] = topbottomtop % 10;
+  out[6] = topbottombottom / 10;
+  out[7] = topbottombottom % 10;
+  out[8] = bottomtoptop / 10;
+  out[9] = bottomtoptop % 10;
+  out[10] = bottomtopbottom / 10;
+  out[11] = bottomtopbottom % 10;
+  out[12] = bottombottomtop / 10;
+  out[13] = bottombottomtop % 10;
+  out[14] = bottombottombottom / 10;
+  out[15] = bottombottombottom % 10;
+}
+
+void to_string_tree_table(uint64_t x, char *out) {
+  static const char table[200] = {
+      0x30, 0x30, 0x30, 0x31, 0x30, 0x32, 0x30, 0x33, 0x30, 0x34, 0x30, 0x35,
+      0x30, 0x36, 0x30, 0x37, 0x30, 0x38, 0x30, 0x39, 0x31, 0x30, 0x31, 0x31,
+      0x31, 0x32, 0x31, 0x33, 0x31, 0x34, 0x31, 0x35, 0x31, 0x36, 0x31, 0x37,
+      0x31, 0x38, 0x31, 0x39, 0x32, 0x30, 0x32, 0x31, 0x32, 0x32, 0x32, 0x33,
+      0x32, 0x34, 0x32, 0x35, 0x32, 0x36, 0x32, 0x37, 0x32, 0x38, 0x32, 0x39,
+      0x33, 0x30, 0x33, 0x31, 0x33, 0x32, 0x33, 0x33, 0x33, 0x34, 0x33, 0x35,
+      0x33, 0x36, 0x33, 0x37, 0x33, 0x38, 0x33, 0x39, 0x34, 0x30, 0x34, 0x31,
+      0x34, 0x32, 0x34, 0x33, 0x34, 0x34, 0x34, 0x35, 0x34, 0x36, 0x34, 0x37,
+      0x34, 0x38, 0x34, 0x39, 0x35, 0x30, 0x35, 0x31, 0x35, 0x32, 0x35, 0x33,
+      0x35, 0x34, 0x35, 0x35, 0x35, 0x36, 0x35, 0x37, 0x35, 0x38, 0x35, 0x39,
+      0x36, 0x30, 0x36, 0x31, 0x36, 0x32, 0x36, 0x33, 0x36, 0x34, 0x36, 0x35,
+      0x36, 0x36, 0x36, 0x37, 0x36, 0x38, 0x36, 0x39, 0x37, 0x30, 0x37, 0x31,
+      0x37, 0x32, 0x37, 0x33, 0x37, 0x34, 0x37, 0x35, 0x37, 0x36, 0x37, 0x37,
+      0x37, 0x38, 0x37, 0x39, 0x38, 0x30, 0x38, 0x31, 0x38, 0x32, 0x38, 0x33,
+      0x38, 0x34, 0x38, 0x35, 0x38, 0x36, 0x38, 0x37, 0x38, 0x38, 0x38, 0x39,
+      0x39, 0x30, 0x39, 0x31, 0x39, 0x32, 0x39, 0x33, 0x39, 0x34, 0x39, 0x35,
+      0x39, 0x36, 0x39, 0x37, 0x39, 0x38, 0x39, 0x39,
+  };
+  uint64_t top = x / 100000000;
+  uint64_t bottom = x % 100000000;
+  //
+  uint64_t toptop = top / 10000;
+  uint64_t topbottom = top % 10000;
+  uint64_t bottomtop = bottom / 10000;
+  uint64_t bottombottom = top % 10000;
+  //
+  uint64_t toptoptop = toptop / 100;
+  uint64_t toptopbottom = toptop % 100;
+
+  uint64_t topbottomtop = topbottom / 100;
+  uint64_t topbottombottom = topbottom % 100;
+
+  uint64_t bottomtoptop = bottomtop / 100;
+  uint64_t bottomtopbottom = bottomtop % 100;
+
+  uint64_t bottombottomtop = bottombottom / 100;
+  uint64_t bottombottombottom = bottombottom % 100;
+  //
+  memcpy(out, &table[2 * toptoptop], 2);
+  memcpy(out + 2, &table[2 * toptopbottom], 2);
+  memcpy(out + 4, &table[2 * topbottomtop], 2);
+  memcpy(out + 6, &table[2 * topbottombottom], 2);
+  memcpy(out + 8, &table[2 * bottomtoptop], 2);
+  memcpy(out + 10, &table[2 * bottomtopbottom], 2);
+  memcpy(out + 12, &table[2 * bottombottomtop], 2);
+  memcpy(out + 14, &table[2 * bottombottombottom], 2);
+}
+
+int main() {
+  std::vector<uint64_t> data;
+  for (size_t i = 0; i < 50000; i++) {
+    data.push_back(rand() % 10000000000000000);
+  }
+  char *buf = new char[data.size() * 16];
+  auto linear_approach = [&data, buf]() -> size_t {
+    char *b = buf;
+    for (auto val : data) {
+      to_string_linear(val, b);
+      b += 16;
+    }
+    return data.size();
+  };
+  auto tree_approach = [&data, buf]() -> size_t {
+    char *b = buf;
+    for (auto val : data) {
+      to_string_tree(val, b);
+      b += 16;
+    }
+    return data.size();
+  };
+  auto tree_table_approach = [&data, buf]() -> size_t {
+    char *b = buf;
+    for (auto val : data) {
+      to_string_tree_table(val, b);
+      b += 16;
+    }
+    return data.size();
+  };
+  for (size_t i = 0; i < 3; i++) {
+    std::cout << "linear ";
+    std::cout << bench(linear_approach) << std::endl;
+    std::cout << "tree  ";
+    std::cout << bench(tree_approach) << std::endl;
+    std::cout << "treet ";
+
+    std::cout << bench(tree_table_approach) << std::endl;
+    std::cout << std::endl;
+  }
+  delete[] buf;
+}
---
+++ 2021/11/17/table.py
@@ -0,0 +1,2 @@
+for i in range(100):
+    print(hex((i/10)+0x30)+","+hex((i%10)+0x30)+",")
