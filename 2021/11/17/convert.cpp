f6e4303aa461553f37093e4b5bf38238882cc287
Daniel Lemire <lemire@gmail.com>
2021-11-17 19:13:54 -0500

    SSE2.

--- 2021/11/17/convert.cpp
+++ 2021/11/17/convert.cpp
@@ -153,6 +153,79 @@ void to_string_tree_table(uint64_t x, char *out) {
   memcpy(out + 14, &table[2 * bottombottombottom], 2);
 }
 
+#ifdef __SSE2__
+// mula
+#include <x86intrin.h>
+void to_string_sse2(uint64_t v, char *out) {
+
+    // v is 16-digit number = abcdefghijklmnop
+
+    const __m128i div_10000 = _mm_set1_epi32(DIV_10000);
+    const __m128i mul_10000 = _mm_set1_epi32(10000);
+    const int div_10000_shift = 45;
+
+    const __m128i div_100   = _mm_set1_epi16(DIV_100);
+    const __m128i mul_100   = _mm_set1_epi16(100);
+    const int div_100_shift = 3;
+
+    const __m128i div_10    = _mm_set1_epi16(DIV_10);
+    const __m128i mul_10    = _mm_set1_epi16(10);
+
+    const __m128i ascii0    = _mm_set1_epi8('0');
+
+	// can't be easliy done in SSE
+	const uint32_t a = v / 100000000; // 8-digit number: abcdefgh
+	const uint32_t b = v % 100000000; // 8-digit number: ijklmnop
+
+    //                [ 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 ]
+    // x            = [       0       |      ijklmnop |       0       |      abcdefgh ]
+    __m128i x = _mm_set_epi64x(b, a);
+
+	// x div 10^4   = [       0       |          ijkl |       0       |          abcd ]
+    __m128i x_div_10000;
+    x_div_10000 = _mm_mul_epu32(x, div_10000);
+    x_div_10000 = _mm_srli_epi64(x_div_10000, div_10000_shift);
+
+    // x mod 10^4   = [       0       |          mnop |       0       |          efgh ]
+    __m128i x_mod_10000;
+    x_mod_10000 = _mm_mul_epu32(x_div_10000, mul_10000);
+    x_mod_10000 = _mm_sub_epi32(x, x_mod_10000);
+
+    // y            = [          mnop |          ijkl |          efgh |          abcd ]
+    __m128i y = _mm_or_si128(x_div_10000, _mm_slli_epi64(x_mod_10000, 32));
+    
+    // y_div_100    = [   0   |    mn |   0   |    ij |   0   |    ef |   0   |    ab ]
+    __m128i y_div_100;
+    y_div_100 = _mm_mulhi_epu16(y, div_100);
+    y_div_100 = _mm_srli_epi16(y_div_100, div_100_shift);
+
+    // y_mod_100    = [   0   |    op |   0   |    kl |   0   |    gh |   0   |    cd ]
+    __m128i y_mod_100;
+    y_mod_100 = _mm_mullo_epi16(y_div_100, mul_100);
+    y_mod_100 = _mm_sub_epi16(y, y_mod_100);
+
+    // z            = [    mn |    op |    ij |    kl |    ef |    gh |    ab |    cd ]
+    __m128i z = _mm_or_si128(y_div_100, _mm_slli_epi32(y_mod_100, 16));
+
+    // z_div_10     = [ 0 | m | 0 | o | 0 | i | 0 | k | 0 | e | 0 | g | 0 | a | 0 | c ]
+    __m128i z_div_10 = _mm_mulhi_epu16(z, div_10);
+
+    // z_mod_10     = [ 0 | n | 0 | p | 0 | j | 0 | l | 0 | f | 0 | h | 0 | b | 0 | d ]
+    __m128i z_mod_10;
+    z_mod_10 = _mm_mullo_epi16(z_div_10, mul_10);
+    z_mod_10 = _mm_sub_epi16(z, z_mod_10);
+
+    // interleave z_mod_10 and z_div_10 -
+    // tmp          = [ m | n | o | p | i | j | k | l | e | f | g | h | a | b | c | d ]
+    __m128i tmp = _mm_or_si128(z_div_10, _mm_slli_epi16(z_mod_10, 8));
+
+    // convert to ascii
+    tmp = _mm_add_epi8(tmp, ascii0);
+
+    // and save result
+    _mm_storeu_si128((__m128i*)out, tmp);
+}
+#endif 
 void to_string_tree_bigtable(uint64_t x, char *out) {
   #include "bigtable.h"
 
@@ -207,6 +280,16 @@ int main() {
     }
     return data.size();
   };
+#ifdef __SSE2__
+  auto sse2_approach = [&data, buf]() -> size_t {
+    char *b = buf;
+    for (auto val : data) {
+      to_string_sse2(val, b);
+      b += 16;
+    }
+    return data.size();
+  };
+#endif
   for (size_t i = 0; i < 3; i++) {
     std::cout << "linear ";
     std::cout << bench(linear_approach) << std::endl;
@@ -218,6 +301,11 @@ int main() {
     std::cout << "treebt";
 
     std::cout << bench(tree_bigtable_approach) << std::endl;
+#ifdef __SSE2__
+    std::cout << "sse2  ";
+
+    std::cout << bench(sse2_approach) << std::endl;
+#endif
     std::cout << std::endl;
   }
   delete[] buf;
