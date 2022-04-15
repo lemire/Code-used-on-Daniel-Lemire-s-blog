2fd8725bd4e4feab009a989f41fcdfa98ac57503
Daniel Lemire <lemire@gmail.com>
2022-04-15 10:54:19 -0400

    Variance.

--- 2022/04/13/numbers.cpp
+++ 2022/04/13/numbers.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main() {
-    float x1 = 10000;
-    float x2 = 10002;
+    float x1 = 100000000;
+    float x2 = 100000002;
     float avg = (x1+x2)/2;
     std::cout << ((x1-avg)*(x1-avg) + (x2-avg)*(x2-avg))/2 << std::endl;
-    double dx1 = 10000;
-    double dx2 = 10002;
+    double dx1 = 100000000;
+    double dx2 = 100000002;
     double davg = (dx1+dx2)/2;
     std::cout << ((dx1-davg)*(dx1-davg) + (dx2-davg)*(dx2-davg))/2 << std::endl;
 }
