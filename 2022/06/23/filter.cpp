ea5e41d35fb9e33f0405712e42f8211d2f8439d4
Daniel Lemire <daniel@lemire.me>
2022-06-23 17:54:45 +0000

    Cleaning

--- 2022/06/23/filter.cpp
+++ 2022/06/23/filter.cpp
@@ -5,6 +5,7 @@
 #include <arm_sve.h>
 #include <iostream>
 #include <vector>
+#include <random>
 void remove_negatives(const int32_t *input, int64_t count, int32_t *output) {
   int64_t i = 0;
   int64_t j = 0;
@@ -30,11 +31,23 @@ void remove_negatives_scalar(const int32_t *input, int64_t count, int32_t *outpu
     }
   }
 }
+
+void remove_negatives_scalar_branchless(const int32_t *input, int64_t count, int32_t *output) {
+  int64_t i = 0;
+  int64_t j = 0;
+  for(; i < count; i++) {
+    output[j] = input[i];
+    j += (input[i] >= 0);
+  }
+}
 int main() {
   std::vector<int32_t> x;
-  size_t size = 1000;
+  size_t size = 50000;
+  std::random_device                  rand_dev;
+  std::mt19937                        generator(rand_dev());
+  std::uniform_int_distribution<int32_t>  distr(-1000,1000);
   for (size_t i = 0; i < size; i++) {
-    x.push_back(1 - i + 100 * (i & 1));
+    x.push_back(distr(generator));
   }
   std::vector<int32_t> y;
   y.resize(x.size());
@@ -49,8 +62,18 @@ int main() {
     linux_events.start();
     remove_negatives_scalar(x.data(), x.size(), y.data());
     linux_events.end(results);
-    std::cout << "cycles/int " << results[0] / 1000. << " ";
-    std::cout << "instructions/int " << results[1] / 1000. << " ";
+    std::cout << "cycles/int " << double(results[0]) / size << " ";
+    std::cout << "instructions/int " << double(results[1]) / size << " ";
+    std::cout << "instructions/cycle " << double(results[1]) / results[0]
+              << std::endl;
+  }
+  std::cout << "scalar_branchless" << std::endl;
+  for (size_t i = 0; i < 10; i++) {
+    linux_events.start();
+    remove_negatives_scalar_branchless(x.data(), x.size(), y.data());
+    linux_events.end(results);
+    std::cout << "cycles/int " << double(results[0]) / size << " ";
+    std::cout << "instructions/int " << double(results[1]) / size << " ";
     std::cout << "instructions/cycle " << double(results[1]) / results[0]
               << std::endl;
   }
@@ -59,8 +82,8 @@ int main() {
     linux_events.start();
     remove_negatives(x.data(), x.size(), y.data());
     linux_events.end(results);
-    std::cout << "cycles/int " << results[0] / 1000. << " ";
-    std::cout << "instructions/int " << results[1] / 1000. << " ";
+    std::cout << "cycles/int " << double(results[0]) / size << " ";
+    std::cout << "instructions/int " << double(results[1]) / size << " ";
     std::cout << "instructions/cycle " << double(results[1]) / results[0]
               << std::endl;
   }
