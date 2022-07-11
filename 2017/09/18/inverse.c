0439f2fa757bc01d1b2b24e37498d6d10dab7e55
Daniel Lemire <lemire@gmail.com>
2022-07-11 09:41:18 -0400

    Adding 16-bit integers.

--- 2017/09/18/inverse.c
+++ 2017/09/18/inverse.c
@@ -4,24 +4,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static inline uint16_t f16(uint16_t x, uint16_t y) { return y * (2 - y * x); }
+
+static uint16_t findInverse16(uint16_t x) {
+  uint32_t y = (3 * x) ^ 2; // 5 bits
+  y = f16(x, y); // 10 bits
+  y = f16(x, y); // 20 bits
+  return y;
+}
+
 static inline uint32_t f32(uint32_t x, uint32_t y) { return y * (2 - y * x); }
 
 static uint32_t findInverse32(uint32_t x) {
-  uint32_t y = (3 * x) ^ 2;
-  y = f32(x, y);
-  y = f32(x, y);
-  y = f32(x, y);
+  uint32_t y = (3 * x) ^ 2; // 5 bits
+  y = f32(x, y); // 10 bits
+  y = f32(x, y); // 20 bits
+  y = f32(x, y); // 30 bits
   return y;
 }
 
 static inline uint64_t f64(uint64_t x, uint64_t y) { return y * (2 - y * x); }
 
 static uint64_t findInverse64(uint64_t x) {
-  uint64_t y = (3 * x) ^ 2;
-  y = f64(x, y);
-  y = f64(x, y);
-  y = f64(x, y);
-  y = f64(x, y);
+  uint64_t y = (3 * x) ^ 2; // 5 bits
+  y = f64(x, y); // 10 bits
+  y = f64(x, y); // 20 bits
+  y = f64(x, y); // 40 bits
+  y = f64(x, y); // 80 bits
   return y;
 }
 
@@ -41,5 +50,9 @@ int main(int argc, char **argv) {
   uint32_t inverse32 = findInverse32(val32);
   assert(inverse32 * val32 == 1);
   printf("The 32-bit inverse of %u is %u (%x) \n", val32, inverse32, inverse32);
+  uint16_t val16 = (uint16_t)val64;
+  uint32_t inverse16 = findInverse16(val16);
+  assert((uint16_t)(inverse16 * val16) == 1);
+  printf("The 16-bit inverse of %u is %u (%x) \n", val16, inverse16, inverse16);
   return EXIT_SUCCESS;
 }
