7a2eeb0c6be874c83031930af57639ab265421bb
Daniel Lemire <lemire@gmail.com>
2022-10-11 13:21:57 -0400

    Saving.

---
+++ 2022/10/11/count_comparisons.cpp
@@ -0,0 +1,89 @@
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <algorithm>
+
+double random(double from, double to) {
+  thread_local std::mt19937 gen(std::random_device{}());
+  thread_local std::uniform_real_distribution<double> dist;
+  return dist(gen, std::uniform_real_distribution<>::param_type{from, to});
+}
+
+size_t counter = 0;
+
+int compare(const void *a, const void *b) {
+  double x, y;
+  memcpy(&x, a, sizeof(x));
+  memcpy(&y, b, sizeof(y));
+  counter++;
+  int result = (x < y) ? -1 : ((y > x) ? 0 : 1);
+  return result;
+}
+
+bool comparestl(const double x, const double y) {
+  counter++;
+
+    return x < y;
+}
+
+
+int main() {
+  for (size_t k = 5; k <=20; k++) {
+    printf("N = 2** %zu\n ", k);
+
+    size_t N = size_t(1) << k;
+    double *buffer = new double[N];
+    double *original = new double[N];
+    for (size_t i = 0; i < N; i++) {
+      original[i] = random(0, 1);
+    }
+    for (size_t i = 0; i < N; i++) {
+      buffer[i] = original[i];
+    }
+    counter = 0;
+    qsort((void *)buffer, N, sizeof(double), compare);
+    printf("qsort comparisons %f\n ", float(counter)/N);
+
+    for (size_t i = 1; i < N; i++) {
+        if(buffer[i] < buffer[i-1]) { printf("bug\n"); }
+    }
+    for (size_t i = 0; i < N; i++) {
+      buffer[i] = original[i];
+    }
+    counter = 0;
+    heapsort((void *)buffer, N, sizeof(double), compare);
+    printf("heapsort comparisons %f\n ", float(counter)/N);
+
+    for (size_t i = 1; i < N; i++) {
+        if(buffer[i] < buffer[i-1]) { printf("bug\n"); }
+    }
+    for (size_t i = 0; i < N; i++) {
+      buffer[i] = original[i];
+    }
+    counter = 0;
+    mergesort((void *)buffer, N, sizeof(double), compare);
+    printf("mergesort comparisons %f\n ", float(counter)/N);
+
+    for (size_t i = 1; i < N; i++) {
+        if(buffer[i] < buffer[i-1]) { printf("bug\n"); }
+    }
+    for (size_t i = 0; i < N; i++) {
+      buffer[i] = original[i];
+    }
+    counter = 0;
+    std::sort(buffer, buffer+N, comparestl);
+    printf("std::sort comparisons %f\n ", float(counter)/N);
+
+    for (size_t i = 1; i < N; i++) {
+        if(buffer[i] < buffer[i-1]) { printf("bug\n"); }
+    }
+
+    delete[] buffer;
+    delete[] original;
+  }
+  return EXIT_SUCCESS;
+}
