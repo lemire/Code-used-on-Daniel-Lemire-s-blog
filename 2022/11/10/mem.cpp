c892adb8003d577910abe940fb20c5296a3b2a2f
Daniel Lemire <daniel@lemire.me>
2022-11-10 09:57:57 -0500

    Memory usage.

---
+++ 2022/11/10/mem.cpp
@@ -0,0 +1,24 @@
+
+#include "nadeau.h"
+#include <vector>
+#include <iostream>
+
+void print_pages() {
+    static size_t pagesize = sysconf(_SC_PAGESIZE);
+    int64_t bytes = getCurrentRSS();
+    assert((bytes % pagesize) == 0);
+    size_t pages = bytes / pagesize;
+    std::cout << "page size: " << pagesize << "\t";
+    std::cout << "bytes: " << bytes << "\t";
+    std::cout << "pages: " << pages << std::endl;
+}
+
+int main() {
+  print_pages();
+  std::vector<int> v1(10);
+  print_pages();
+  std::vector<int> v2(1000000);
+  print_pages();
+
+  return EXIT_SUCCESS;
+}
