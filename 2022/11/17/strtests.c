0f5778b64878f613e9c93849a8f5eee660b05977
Daniel Lemire <daniel@lemire.me>
2022-11-17 15:15:46 +0000

    Saving.

---
+++ 2022/11/17/strtests.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fenv.h>
+
+int main(int argc, char ** args) {
+    if(argc == 1) {
+        printf("Please pass a number as a string as a parameter\n");
+        return EXIT_SUCCESS;
+    }
+    int modes[4] = {FE_UPWARD, FE_DOWNWARD, FE_TOWARDZERO, FE_TONEAREST};
+    for(int i = 0; i < 4; i++) {
+       fesetround(modes[i]);
+       double result = strtod(args[1], NULL);
+       printf("%g \n", result);
+    }
+    return EXIT_SUCCESS;
+}
