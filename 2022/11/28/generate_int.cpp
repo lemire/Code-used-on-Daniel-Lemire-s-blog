1a95c6b659ec7a17eb6e9cb6382caac153c6b998
Daniel Lemire <daniel@lemire.me>
2022-11-28 12:12:47 -0500

    Updating.

---
+++ 2022/11/28/generate_int.cpp
@@ -0,0 +1,18 @@
+
+#include <fstream>
+#include <random>
+
+int main() {
+  size_t N = 10000;
+  double *x = new double[N];
+  std::random_device rd;
+  std::default_random_engine eng(rd());
+  std::uniform_int_distribution<int64_t> distr(-127,127);
+  for (size_t i = 0; i < N; i++) {
+    x[i] = distr(eng);
+  }
+  std::ofstream wf("testint.dat", std::ios::out | std::ios::binary);
+  wf.write((char *)x, sizeof(int64_t) * N);
+  wf.close();
+  delete[] x;
+}
