aa8857b8dd60502cf992f2578184ed8c2e5721ff
Daniel Lemire <daniel@lemire.me>
2023-01-30 12:36:03 -0500

    Adding string_view.

--- 2023/01/30/populate_optional.cpp
+++ 2023/01/30/populate_optional.cpp
@@ -9,6 +9,7 @@
 #include <random>
 #include <string>
 #include <vector>
+#include <string_view>
 
 uint64_t nano() {
   return std::chrono::duration_cast<::std::chrono::nanoseconds>(
@@ -38,10 +39,11 @@ template <bool shortstrings> std::vector<std::string> populate(size_t length) {
 
 std::vector<std::string> data;
 std::vector<std::optional<std::string>> outdata;
+std::vector<std::optional<std::string_view>> outdata_view;
 std::vector<std::string*> outdataptr;
 
-template <bool shortstrings> std::tuple<double, double, double> simulation(size_t N) {
-  double t1, t2, t3;
+template <bool shortstrings> std::tuple<double, double, double, double> simulation(const size_t N) {
+  double t1, t2, t3, t4;
 
   {
     data = populate<shortstrings>(N);
@@ -76,31 +78,45 @@ template <bool shortstrings> std::tuple<double, double, double> simulation(size_
     uint64_t finish = nano();
     t3 = double(finish - start) / N;
   }
-  return {t1, t2, t3};
+  {
+    data = populate<shortstrings>(N);
+    outdata_view = std::vector<std::optional<std::string_view>>(N);
+
+    uint64_t start = nano();
+    for (size_t i = 0; i < N; i++) {
+      outdata_view[i] = std::string_view(data[i]);
+    }
+    uint64_t finish = nano();
+    t4 = double(finish - start) / N;
+  }
+  return {t1, t2, t3, t4};
 }
 
 template <bool shortstrings> void demo() {
   double avg1 = 0;
   double avg2 = 0;
   double avg3 = 0;
+  double avg4 = 0;
 
   size_t times = 100;
 
   for (size_t i = 0; i < times; i++) {
-    auto [t1, t2, t3] = simulation<shortstrings>(131072);
+    auto [t1, t2, t3, t4] = simulation<shortstrings>(131072);
     avg1 += t1;
     avg2 += t2;
     avg3 += t3;
+    avg4 += t4;
   }
   avg1 /= times;
   avg2 /= times;
   avg3 /= times;
+  avg4 /= times;
 
-  std::cout << avg1 << " " << avg2 << " " << avg3 << std::endl;
+  std::cout << avg1 << " " << avg2 << " " << avg3 << " " << avg4 << std::endl;
 }
 
 int main() {
-  puts("We report ns/string (first copy, then move, then pointer).\n");
+  puts("We report ns/string (first copy, then move, then pointer, the string_view).\n");
   puts("short strings:");
   demo<true>();
   puts("long strings:");
