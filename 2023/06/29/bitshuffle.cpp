45da13241ded2329b3aaa80cc584d7448e2a8551
Daniel Lemire <dlemire@lemire.me>
2023-06-29 17:42:06 -0400

    Even better.

--- 2023/06/29/bitshuffle.cpp
+++ 2023/06/29/bitshuffle.cpp
@@ -21,6 +21,13 @@ uint64_t bit_shuffle(uint64_t w, uint8_t indexes[64]) {
   return _cvtmask64_u64(_mm512_movepi8_mask(as_vec_register_shuf));
 }
 
+// shuffle the bits within w
+uint64_t faster_bit_shuffle(uint64_t w, uint8_t indexes[64]) {
+  __m512i as_vec_register = _mm512_set1_epi64(w);
+  __mmask64 as_mask = _mm512_bitshuffle_epi64_mask(as_vec_register, _mm512_loadu_si512(indexes));
+  return _cvtmask64_u64(as_mask);
+}
+
 int main() {
   uint64_t w = 0xabcdef0123456789;
   uint8_t indexes[64] = {63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51,
@@ -32,4 +39,6 @@ int main() {
   std::cout << slow_bit_shuffle(w, indexes) << std::endl;
   std::cout << bit_shuffle(w, indexes) << std::endl;
   std::cout << bit_shuffle(bit_shuffle(w, indexes), indexes) << std::endl;
+  std::cout << faster_bit_shuffle(w, indexes) << std::endl;
+  std::cout << faster_bit_shuffle(faster_bit_shuffle(w, indexes), indexes) << std::endl;
 }
