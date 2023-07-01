d62f9ae3cfc9d059a253593db7a805eb50f28086
Daniel Lemire <dlemire@lemire.me>
2023-07-01 17:07:06 -0400

    Saving.

--- 2023/07/01/include/sse_date.h
+++ 2023/07/01/include/sse_date.h
@@ -7,12 +7,16 @@
 // Convert a string in the form YYYYMMDDHHmmSS to an integer (stored in
 // destination) representing the time in second since epoch. The function
 // returns true upon success.
+// Because we use an unsigned 32-bit integer, we can represent all time between
+// 1970 and 2106. We cannot represent time before 1970.
 bool parse_time(const char *date_string, uint32_t *time_in_second);
 
 // Convert a string in the form YYYYMMDDHHmmSS to an integer (stored in
 // destination) representing the time in second since epoch. The function
 // returns true upon success. This function may read up to 16 bytes in
 // date_string but will only process the first 14 bytes.
+// Because we use an unsigned 32-bit integer, we can represent all time between
+// 1970 and 2106. We cannot represent time before 1970.
 bool sse_parse_time(const char *date_string, uint32_t *time_in_second);
 
 #endif // SSE_INET_ATON_H
