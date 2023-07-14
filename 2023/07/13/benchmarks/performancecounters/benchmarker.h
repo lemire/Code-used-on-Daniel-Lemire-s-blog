2e62da5720c7bee3ee84ca2687e92b404050211f
Daniel Lemire <dlemire@lemire.me>
2023-07-14 13:52:03 -0400

    Saving...

--- 2023/07/13/benchmarks/performancecounters/benchmarker.h
+++ 2023/07/13/benchmarks/performancecounters/benchmarker.h
@@ -5,7 +5,7 @@
 event_collector collector;
 
 template <class function_type> 
-event_aggregate bench(const function_type& function, size_t min_repeat = 10, size_t min_time_ns = 1000000000) {
+event_aggregate bench(const function_type& function, size_t min_repeat = 10, size_t min_time_ns = 1000000000, size_t max_repeat = 1000000) {
     event_aggregate aggregate{};
     size_t N = min_repeat;
     if(N == 0) { N = 1; }
@@ -16,7 +16,7 @@ event_aggregate bench(const function_type& function, size_t min_repeat = 10, siz
       std::atomic_thread_fence(std::memory_order_release);
       event_count allocate_count = collector.end();
       aggregate << allocate_count;
-      if((i+1 == N) && (aggregate.total_elapsed_ns() < min_time_ns)) {
+      if((i+1 == N) && (aggregate.total_elapsed_ns() < min_time_ns) && (N<max_repeat)) {
         N *= 10;
       }
     }
