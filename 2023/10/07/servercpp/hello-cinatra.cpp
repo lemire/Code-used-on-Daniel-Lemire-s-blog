990175bfd17a9d12cd6bc10b1ca09f1339ebe479
Daniel Lemire <dlemire@lemire.me>
2023-10-13 10:26:40 -0400

    Moving

--- 2023/10/07/servercpp/hello-cinatra.cpp
+++ 2023/10/07/servercpp/hello-cinatra.cpp
@@ -3,6 +3,7 @@ using namespace cinatra;
 
 int main() {
 	int max_thread_num = std::thread::hardware_concurrency();
+	if(max_thread_num > 8) { max_thread_num = 8; }
 	http_server server(max_thread_num);
 	server.listen("0.0.0.0", "8080");
 	server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
