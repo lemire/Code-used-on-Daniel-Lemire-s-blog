6333098471f551dd4f71f99fbc0706a3fccb7dbf
Daniel Lemire <daniel@lemire.me>
2023-10-12 22:06:59 -0400

    Adding.

---
+++ 2023/10/servercpp/hello-libhv.cpp
@@ -0,0 +1,40 @@
+#include "hv/HttpServer.h"
+using namespace hv;
+
+int main() {
+    HttpService router;
+    router.GET("/ping", [](HttpRequest* req, HttpResponse* resp) {
+        return resp->String("pong");
+    });
+
+    router.GET("/data", [](HttpRequest* req, HttpResponse* resp) {
+        static char data[] = "0123456789";
+        return resp->Data(data, 10);
+    });
+
+    router.GET("/paths", [&router](HttpRequest* req, HttpResponse* resp) {
+        return resp->Json(router.Paths());
+    });
+
+    router.GET("/get", [](HttpRequest* req, HttpResponse* resp) {
+        resp->json["origin"] = req->client_addr.ip;
+        resp->json["url"] = req->url;
+        resp->json["args"] = req->query_params;
+        resp->json["headers"] = req->headers;
+        return 200;
+    });
+
+    router.POST("/echo", [](const HttpContextPtr& ctx) {
+        return ctx->send(ctx->body(), ctx->type());
+    });
+
+    HttpServer server(&router);
+
+    server.setPort(8080);
+    server.setThreadNum(8);
+    printf(" threads: %d\n", server.worker_threads);
+
+    server.run();
+    
+    return 0;
+}
