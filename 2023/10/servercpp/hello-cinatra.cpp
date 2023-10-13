de2962dc0e200c19bbace85cb3a46e5b997bf7c7
Daniel Lemire <daniel@lemire.me>
2023-10-13 10:06:01 -0400

    Update

---
+++ 2023/10/servercpp/hello-cinatra.cpp
@@ -0,0 +1,14 @@
+#include "cinatra.hpp"
+using namespace cinatra;
+
+int main() {
+	int max_thread_num = std::thread::hardware_concurrency();
+	http_server server(max_thread_num);
+	server.listen("0.0.0.0", "8080");
+	server.set_http_handler<GET, POST>("/", [](request& req, response& res) {
+		res.set_status_and_content(status_type::ok, "hello world");
+	});
+
+	server.run();
+	return 0;
+}
