cd6465eff9fd27cc3961f15b661d7c372a9244c0
Daniel Lemire <daniel@lemire.me>
2023-11-29 16:08:28 -0500

    Update parseuint8.h

--- 2023/11/28/include/parseuint8.h
+++ 2023/11/28/include/parseuint8.h
@@ -3,8 +3,18 @@
 #include <cstdint>
 #include <cstddef>
 
+
+// Parses ASCII numbers between 0 and 255 with validation.
+
+// These functions return a Boolean indicating whether an 8-bit positive value
+// could be parse (between 0 and 255 inclusively). The parsed value is stored
+// in 'num'. You must provide the exact length of the number in bytes in the variable 'len' (1, 2, 3).
+
+// parse_uint8_swar is not correct and left as a reference, reads 4 bytes at str with every call
 int parse_uint8_swar(const char *str, size_t len, uint8_t *num);
+// parse_uint8_fastswar is correct and fast, reads 4 bytes at str with every call
 int parse_uint8_fastswar(const char *str, size_t len, uint8_t *num);
+// parse_uint8_fromchars is correct 
 int parse_uint8_fromchars(const char *str, size_t len, uint8_t *num);
 int parse_uint8_naive(const char *str, size_t len, uint8_t *num);
 #endif // ESCAPE_H
