927b2bba4d7b13ad9820f30de8111ce1bf10c0f8
Daniel Lemire <daniel@lemire.me>
2023-12-07 23:18:39 -0500

    saving.

--- 2023/12/07/treemem.mjs
+++ 2023/12/07/treemem.mjs
@@ -5,6 +5,8 @@ if (global.gc) {
   console.log('Garbage collection unavailable. Pass --expose-gc '
   + 'when launching node to enable forced garbage collection.');
 }
+await new Promise((resolve) => setTimeout(resolve, 500));
+
 let rss = process.memoryUsage().rss
 
 console.log(`usage ${Math.round(rss/ 1024 * 100) / 100} kB`);
