839ccf7c9e501a0706950bdb81d0351f058b8157
Daniel Lemire <daniel@lemire.me>
2024-02-04 15:10:03 -0500

    saving.

--- 2024/02/04/benchmarks/benchmark.cpp
+++ 2024/02/04/benchmarks/benchmark.cpp
@@ -56,6 +56,49 @@ size_t karprabin_rolling(const char *data, size_t len, size_t N, uint32_t B, uin
   return counter;
 }
 
+size_t karprabin_rolling4(const char *data, size_t len, size_t N, uint32_t B, uint32_t target) {
+  size_t counter = 0;
+  uint32_t BtoN = 1;
+  for(size_t i = 0; i < N; i++) {
+    BtoN *= B;
+  }
+  uint32_t hash = 0;
+  for(size_t i = 0; i < N; i++) {
+    hash = hash * B + data[i];
+  }
+  if (hash == target) {
+    counter++;
+  }
+  size_t i = N;
+  for(; i + 3 < len; i+=4) {
+    hash = hash * B + data[i] - BtoN * data[i-N];
+    if (hash == target) {
+      counter++;
+    }
+    hash = hash * B + data[i+1] - BtoN * data[i+1-N];
+    if (hash == target) {
+      counter++;
+    }
+    hash = hash * B + data[i+2] - BtoN * data[i+2-N];
+    if (hash == target) {
+      counter++;
+    }
+    hash = hash * B + data[i+3] - BtoN * data[i+3-N];
+    if (hash == target) {
+      counter++;
+    }
+  }
+
+  for(; i < len; i++) {
+    hash = hash * B + data[i] - BtoN * data[i-N];
+    // karprabin_hash(data+i-N+1, N, B) == hash
+    if (hash == target) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
 void pretty_print(size_t volume, size_t bytes, std::string name,
                   event_aggregate agg) {
   printf("%-40s : ", name.c_str());
@@ -90,6 +133,9 @@ int main(int argc, char **argv) {
   volatile size_t counter = 0;
   for(size_t window = 8; window <= 32; window *= 2) {
     printf("window = %zu\n", window);
+      pretty_print(1, volume, "karprabin_rolling4", bench([&data, &counter, &window]() {
+      counter += karprabin_rolling4(data.get(), N, window, 31, 0);
+    }));
     pretty_print(1, volume, "karprabin_rolling", bench([&data, &counter, &window]() {
       counter += karprabin_rolling(data.get(), N, window, 31, 0);
     }));
