aed8b51efbb5acedb7a19f0cbe8088714395a0d1
Tyler Kennedy <tk@tkte.ch>
2024-03-12 17:17:49 -0400

    Purge filesystem caches between each invocation
    I'm operating under the potentially incorrect assumption that you're running these tests on Linux.

--- 2024/03/12/script.sh
+++ 2024/03/12/script.sh
@@ -1,13 +1,24 @@
 echo "node"
 node --version
+echo 3 > /proc/sys/vm/drop_caches
 node test1.js
+echo 3 > /proc/sys/vm/drop_caches
 node test2.js
+echo 3 > /proc/sys/vm/drop_caches
 node test3.js
+echo 3 > /proc/sys/vm/drop_caches
 node test4.js
+echo 3 > /proc/sys/vm/drop_caches
 node test5.js
+echo 3 > /proc/sys/vm/drop_caches
 echo "Bun"
+echo 3 > /proc/sys/vm/drop_caches
 bun run test1.js
+echo 3 > /proc/sys/vm/drop_caches
 bun run test2.js
+echo 3 > /proc/sys/vm/drop_caches
 bun run test3.js
+echo 3 > /proc/sys/vm/drop_caches
 bun run test4.js
+echo 3 > /proc/sys/vm/drop_caches
 bun run test5.js
