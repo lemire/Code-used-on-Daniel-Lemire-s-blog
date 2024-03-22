d99bb6517550dace035c40a4c8a4777e1b7791d3
Daniel Lemire <daniel@lemire.me>
2024-03-22 11:16:23 -0400

    saving

--- 2024/03/22/recursive_lambda.cpp
+++ 2024/03/22/recursive_lambda.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <functional>
 
+auto return1 = [](int n) -> int { return 1; };
 /*
 //  error: variable 'fact' declared with deduced type 'auto' cannot appear in its own initializer
-auto fact = [](int n) -> int {
+auto fact1 = [](int n) -> int {
     if (n == 0) {
         return 1;
     } else {
@@ -18,6 +20,14 @@ std::function<int(int)> fact = [](int n) -> int {
     }
 };
 
+
+int (*factorial)(int) = [](int n) -> int {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+};
 void print(int(*f)(int)) {
     for(int k = 1; k < 10; ++k) {
         std::cout << "Factorial of " << k << " is " << f(k) << std::endl;
@@ -25,9 +35,38 @@ void print(int(*f)(int)) {
 }
 
 
+int (*lfactorial)(int) =  [](int n)  -> int {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * lfactorial(n - 1);
+    }
+
+};
+
+int factorialc(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int functionc() {
+    return factorialc(10);
+}
+
+
+int functionl() {
+    return lfactorial(10);
+}
+
 int main() {
     int n = 5;
     std::cout << "Factorial of " << n << " is " << fact(n) << std::endl;
+    //print(fact); // NOT OK
+
+    print(factorial); // OK
     print([](int n) -> int { return fact(n); }); // OK 
     return EXIT_SUCCESS;
 }
