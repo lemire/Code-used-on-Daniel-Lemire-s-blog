b002262184f7162f1d8e942fea2b24f1fee70fff
Daniel Lemire <daniel@lemire.me>
2024-04-14 10:15:55 -0400

    saving..

--- 2024/04/11/wrap2.py
+++ 2024/04/11/wrap2.py
@@ -2,16 +2,15 @@ M = 2
 B = 2**8
 
 x = 0
-power = 0
 limit = M
 for i in range(10):
     print("i", i, "x", x/B**i, "power", x, "/", B**i)
-    limit -= power
     limit *= B**2
     power = 0
     x*=B
     while power + 2*x + 1 <= limit:
         power += 2*x + 1
         x += 1
+    limit -= power
 
 print(x/B**(10))
