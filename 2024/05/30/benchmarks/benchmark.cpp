0cfa6f429efcbf2f1ce6942e9dad9e9df17692e8
Daniel Lemire <daniel@lemire.me>
2024-05-30 21:40:04 -0400

    saving

---
+++ 2024/05/30/benchmarks/benchmark.cpp
@@ -0,0 +1,65 @@
+
+#include "performancecounters/benchmarker.h"
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <random>
+#include <string>
+
+#include "needsescaping.h"
+
+void pretty_print(size_t volume, size_t bytes, std::string name,
+                  event_aggregate agg) {
+  printf("%-40s : ", name.c_str());
+  printf(" %5.2f GB/s ", bytes / agg.fastest_elapsed_ns());
+  printf(" %5.1f Ma/s ", volume * 1000.0 / agg.fastest_elapsed_ns());
+  printf(" %5.2f ns/d ", agg.fastest_elapsed_ns() / volume);
+  if (collector.has_events()) {
+    printf(" %5.2f GHz ", agg.fastest_cycles() / agg.fastest_elapsed_ns());
+    printf(" %5.2f c/d ", agg.fastest_cycles() / volume);
+    printf(" %5.2f i/d ", agg.fastest_instructions() / volume);
+    printf(" %5.2f c/b ", agg.fastest_cycles() / bytes);
+    printf(" %5.2f i/b ", agg.fastest_instructions() / bytes);
+    printf(" %5.2f i/c ", agg.fastest_instructions() / agg.fastest_cycles());
+  }
+  printf("\n");
+}
+
+
+int main(int argc, char **argv) {
+  std::vector<std::string> data = {"Hello, World!", "La vie est belle", "C++ is fun", "I love programming", "I love algorithms", "I love data structures", "I love mathematics", "I love computer science", "I love software engineering", "I love machine learning", "I love artificial intelligence", "I love deep learning", "I love neural networks", "I love convolutional neural networks", "I love recurrent neural networks", "I love long short-term memory networks", "I love generative adversarial networks", "I love reinforcement learning", "I love unsupervised learning", "I love supervised learning", "I love semi-supervised learning", "I love self-supervised learning", "I love transfer learning", "I love multi-task learning", "I love meta-learning", "I love few-shot learning", "I love zero-shot learning", "I love one shot",
+  "John went to town", "Melville", "Johanna", "Can't we just get along", "What about China?", "Hello, World!", "La vie est belle", "C++ is fun", "I love programming", "I love algorithms", "I love data structures", "I love mathematics", "I love computer science", "I love software engineering", "I love machine learning", "I love artificial intelligence", "I love deep learning", "I love neural networks", "I love convolutional neural networks", "I love recurrent neural networks", "I love long short-term memory networks", "I love generative adversarial networks", "I love reinforcement learning", "I love unsupervised learning", "I love supervised learning", "I love semi-supervised learning", "I love self-supervised learning", "I love transfer learning", "I love multi-task learning", "I love meta-learning", "I love few-shot learning", "I love zero-shot learning", "I love one shot",
+  "John went to town", "Melville", "Johanna", "Can't we just get along", "What about China?",
+  "Hello, World!", "La vie est belle", "C++ is fun", "I love programming", "I love algorithms", "I love data structures", "I love mathematics", "I love computer science", "I love software engineering", "I love machine learning", "I love artificial intelligence", "I love deep learning", "I love neural networks", "I love convolutional neural networks", "I love recurrent neural networks", "I love long short-term memory networks", "I love generative adversarial networks", "I love reinforcement learning", "I love unsupervised learning", "I love supervised learning", "I love semi-supervised learning", "I love self-supervised learning", "I love transfer learning", "I love multi-task learning", "I love meta-learning", "I love few-shot learning", "I love zero-shot learning", "I love one shot",
+  "John went to town", "Melville", "Johanna", "Can't we just get along", "What about China?"};
+  size_t volume = 0;
+  for (const std::string &s : data) {
+    volume += s.size();
+  }
+  volatile uint64_t counter = 0;
+  pretty_print(data.size(), volume, "simple_needs_escaping",
+               bench([&data,&counter]() {
+                 for (const std::string &s : data) {
+                  counter += simple_needs_escaping(s);
+                 }
+               }));
+  pretty_print(data.size(), volume, "branchless_needs_escaping",
+               bench([&data,&counter]() {
+                 for (const std::string &s : data) {
+                  counter += branchless_needs_escaping(s);
+                 }
+               }));
+  pretty_print(data.size(), volume, "table_needs_escaping",
+               bench([&data,&counter]() {
+                 for (const std::string &s : data) {
+                  counter += table_needs_escaping(s);
+                 }
+               }));
+
+  pretty_print(data.size(), volume, "simd_needs_escaping",
+               bench([&data,&counter]() {
+                 for (const std::string &s : data) {
+                  counter += simd_needs_escaping(s);
+                 }
+               }));
+}
---
+++ 2024/05/30/include/needsescaping.h
@@ -0,0 +1,79 @@
+#include <array>
+#include <cstdint>
+static constexpr std::array<uint8_t, 256> json_quotable_character =
+    []() constexpr {
+      std::array<uint8_t, 256> result{};
+      for (int i = 0; i < 32; i++) {
+        result[i] = 1;
+      }
+      for (int i : {'"', '\\'}) {
+        result[i] = 1;
+      }
+      return result;
+    }();
+
+constexpr inline bool simple_needs_escaping(std::string_view v) {
+  for (char c : v) {
+    if (uint8_t(c) < 32 | c == '"' | c == '\\') {
+      return true;
+    }
+  }
+  return false;
+}
+
+constexpr inline bool branchless_needs_escaping(std::string_view v) {
+  bool b = false;
+  for (char c : v) {
+    b |= (uint8_t(c) < 32 | c == '"' | c == '\\');
+  }
+  return b;
+}
+
+constexpr inline bool table_needs_escaping(std::string_view view) {
+  uint8_t needs = 0;
+  for (uint8_t c : view) {
+    needs |= json_quotable_character[c];
+  }
+  return needs;
+}
+
+#if defined(__SSE2__) || defined(__x86_64__) || defined(__x86_64) || \
+    (defined(_M_AMD64) || defined(_M_X64) ||                         \
+     (defined(_M_IX86_FP) && _M_IX86_FP == 2))
+#define HAS_SSE2 1
+#endif
+
+
+#if defined(__aarch64__) || defined(_M_ARM64)
+#define HAS_NEON 1
+#endif
+
+#if HAS_NEON
+#include <arm_neon.h>
+
+inline bool simd_needs_escaping(std::string_view view) {
+  if (view.size() < 16) {
+    return branchless_needs_escaping(view);
+  }
+  size_t i = 0;
+  static uint8_t rnt_array[16] = {1, 0, 34,  0, 0, 0,  0, 0,
+                                  0, 0, 0, 0, 92, 0, 0, 0};
+  const uint8x16_t rnt = vld1q_u8(rnt_array);
+  uint8x16_t running{0};
+  for (; i + 15 < view.size(); i += 16) {
+    uint8x16_t word = vld1q_u8((const uint8_t*)view.data() + i);
+    running = vorrq_u8(running, vceqq_u8(vqtbl1q_u8(rnt, word), word));
+    running = vorrq_u8(running, vcltq_u8(word, vdupq_n_u8(32)));
+  }
+  if (i < view.size()) {
+    uint8x16_t word =
+        vld1q_u8((const uint8_t*)view.data() + view.length() - 16);
+    running = vorrq_u8(running, vceqq_u8(vqtbl1q_u8(rnt, word), word));
+    running = vorrq_u8(running, vcltq_u8(word, vdupq_n_u8(32)));
+  }
+  return vmaxvq_u32(vreinterpretq_u32_u8(running)) != 0;
+}
+
+#elif HAS_SSE2
+#include <emmintrin.h>
+#endif
