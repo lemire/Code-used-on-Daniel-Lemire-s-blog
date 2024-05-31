bb68b4d0d8c9a38fa325b5e6add4160ab35acf6e
Daniel Lemire <daniel@lemire.me>
2024-05-30 22:37:48 -0400

    minor update

--- 2024/05/30/benchmarks/benchmark.cpp
+++ 2024/05/30/benchmarks/benchmark.cpp
@@ -25,41 +25,134 @@ void pretty_print(size_t volume, size_t bytes, std::string name,
   printf("\n");
 }
 
-
 int main(int argc, char **argv) {
-  std::vector<std::string> data = {"Hello, World!", "La vie est belle", "C++ is fun", "I love programming", "I love algorithms", "I love data structures", "I love mathematics", "I love computer science", "I love software engineering", "I love machine learning", "I love artificial intelligence", "I love deep learning", "I love neural networks", "I love convolutional neural networks", "I love recurrent neural networks", "I love long short-term memory networks", "I love generative adversarial networks", "I love reinforcement learning", "I love unsupervised learning", "I love supervised learning", "I love semi-supervised learning", "I love self-supervised learning", "I love transfer learning", "I love multi-task learning", "I love meta-learning", "I love few-shot learning", "I love zero-shot learning", "I love one shot",
-  "John went to town", "Melville", "Johanna", "Can't we just get along", "What about China?", "Hello, World!", "La vie est belle", "C++ is fun", "I love programming", "I love algorithms", "I love data structures", "I love mathematics", "I love computer science", "I love software engineering", "I love machine learning", "I love artificial intelligence", "I love deep learning", "I love neural networks", "I love convolutional neural networks", "I love recurrent neural networks", "I love long short-term memory networks", "I love generative adversarial networks", "I love reinforcement learning", "I love unsupervised learning", "I love supervised learning", "I love semi-supervised learning", "I love self-supervised learning", "I love transfer learning", "I love multi-task learning", "I love meta-learning", "I love few-shot learning", "I love zero-shot learning", "I love one shot",
-  "John went to town", "Melville", "Johanna", "Can't we just get along", "What about China?",
-  "Hello, World!", "La vie est belle", "C++ is fun", "I love programming", "I love algorithms", "I love data structures", "I love mathematics", "I love computer science", "I love software engineering", "I love machine learning", "I love artificial intelligence", "I love deep learning", "I love neural networks", "I love convolutional neural networks", "I love recurrent neural networks", "I love long short-term memory networks", "I love generative adversarial networks", "I love reinforcement learning", "I love unsupervised learning", "I love supervised learning", "I love semi-supervised learning", "I love self-supervised learning", "I love transfer learning", "I love multi-task learning", "I love meta-learning", "I love few-shot learning", "I love zero-shot learning", "I love one shot",
-  "John went to town", "Melville", "Johanna", "Can't we just get along", "What about China?"};
+  std::vector<std::string> data = {"Hello, World!",
+                                   "La vie est belle",
+                                   "C++ is fun",
+                                   "I love programming",
+                                   "I love algorithms",
+                                   "I love data structures",
+                                   "I love mathematics",
+                                   "I love computer science",
+                                   "I love software engineering",
+                                   "I love machine learning",
+                                   "I love artificial intelligence",
+                                   "I love deep learning",
+                                   "I love neural networks",
+                                   "I love convolutional neural networks",
+                                   "I love recurrent neural networks",
+                                   "I love long short-term memory networks",
+                                   "I love generative adversarial networks",
+                                   "I love reinforcement learning",
+                                   "I love unsupervised learning",
+                                   "I love supervised learning",
+                                   "I love semi-supervised learning",
+                                   "I love self-supervised learning",
+                                   "I love transfer learning",
+                                   "I love multi-task learning",
+                                   "I love meta-learning",
+                                   "I love few-shot learning",
+                                   "I love zero-shot learning",
+                                   "I love one shot",
+                                   "John went to town",
+                                   "Melville",
+                                   "Johanna",
+                                   "Can't we just get along",
+                                   "What about China?",
+                                   "Hello, World!",
+                                   "La vie est belle",
+                                   "C++ is fun",
+                                   "I love programming",
+                                   "I love algorithms",
+                                   "I love data structures",
+                                   "I love mathematics",
+                                   "I love computer science",
+                                   "I love software engineering",
+                                   "I love machine learning",
+                                   "I love artificial intelligence",
+                                   "I love deep learning",
+                                   "I love neural networks",
+                                   "I love convolutional neural networks",
+                                   "I love recurrent neural networks",
+                                   "I love long short-term memory networks",
+                                   "I love generative adversarial networks",
+                                   "I love reinforcement learning",
+                                   "I love unsupervised learning",
+                                   "I love supervised learning",
+                                   "I love semi-supervised learning",
+                                   "I love self-supervised learning",
+                                   "I love transfer learning",
+                                   "I love multi-task learning",
+                                   "I love meta-learning",
+                                   "I love few-shot learning",
+                                   "I love zero-shot learning",
+                                   "I love one shot",
+                                   "John went to town",
+                                   "Melville",
+                                   "Johanna",
+                                   "Can't we just get along",
+                                   "What about China?",
+                                   "Hello, World!",
+                                   "La vie est belle",
+                                   "C++ is fun",
+                                   "I love programming",
+                                   "I love algorithms",
+                                   "I love data structures",
+                                   "I love mathematics",
+                                   "I love philosophy",
+                                   "I love software engineering",
+                                   "I dislike machine learning",
+                                   "I love artificial intelligence",
+                                   "I love deep learning",
+                                   "I love neural networks",
+                                   "I love convolutional neural networks",
+                                   "I love recurrent neural networks",
+                                   "I love long short-term memory networks",
+                                   "I love generative adversarial networks",
+                                   "I love reinforcement learning",
+                                   "I love unsupervised learning",
+                                   "I love supervised learning",
+                                   "I love semi-supervised learning",
+                                   "I love self-supervised learning",
+                                   "I love transfer learning",
+                                   "I love multi-task learning",
+                                   "I love meta-learning",
+                                   "I love few-shot learning",
+                                   "I love zero-shot learning",
+                                   "I dislike one shot",
+                                   "Peter went to town",
+                                   "Marie",
+                                   "Johanna",
+                                   "Can't we just get fight?",
+                                   "What about Tokyo?"};
   size_t volume = 0;
   for (const std::string &s : data) {
     volume += s.size();
   }
   volatile uint64_t counter = 0;
   pretty_print(data.size(), volume, "simple_needs_escaping",
-               bench([&data,&counter]() {
+               bench([&data, &counter]() {
                  for (const std::string &s : data) {
-                  counter += simple_needs_escaping(s);
+                   counter += simple_needs_escaping(s);
                  }
                }));
   pretty_print(data.size(), volume, "branchless_needs_escaping",
-               bench([&data,&counter]() {
+               bench([&data, &counter]() {
                  for (const std::string &s : data) {
-                  counter += branchless_needs_escaping(s);
+                   counter += branchless_needs_escaping(s);
                  }
                }));
   pretty_print(data.size(), volume, "table_needs_escaping",
-               bench([&data,&counter]() {
+               bench([&data, &counter]() {
                  for (const std::string &s : data) {
-                  counter += table_needs_escaping(s);
+                   counter += table_needs_escaping(s);
                  }
                }));
 
   pretty_print(data.size(), volume, "simd_needs_escaping",
-               bench([&data,&counter]() {
+               bench([&data, &counter]() {
                  for (const std::string &s : data) {
-                  counter += simd_needs_escaping(s);
+                   counter += simd_needs_escaping(s);
                  }
                }));
 }
--- 2024/05/30/include/needsescaping.h
+++ 2024/05/30/include/needsescaping.h
@@ -2,19 +2,20 @@
 #include <cstdint>
 static constexpr std::array<uint8_t, 256> json_quotable_character =
     []() constexpr {
-      std::array<uint8_t, 256> result{};
-      for (int i = 0; i < 32; i++) {
-        result[i] = 1;
-      }
-      for (int i : {'"', '\\'}) {
-        result[i] = 1;
-      }
-      return result;
-    }();
+  std::array<uint8_t, 256> result{};
+  for (int i = 0; i < 32; i++) {
+    result[i] = 1;
+  }
+  for (int i : {'"', '\\'}) {
+    result[i] = 1;
+  }
+  return result;
+}
+();
 
 constexpr inline bool simple_needs_escaping(std::string_view v) {
   for (char c : v) {
-    if (uint8_t(c) < 32 | c == '"' | c == '\\') {
+    if ((uint8_t(c) < 32) | (c == '"') | (c == '\\')) {
       return true;
     }
   }
@@ -24,7 +25,7 @@ constexpr inline bool simple_needs_escaping(std::string_view v) {
 constexpr inline bool branchless_needs_escaping(std::string_view v) {
   bool b = false;
   for (char c : v) {
-    b |= (uint8_t(c) < 32 | c == '"' | c == '\\');
+    b |= ((uint8_t(c) < 32) | (c == '"') | (c == '\\'));
   }
   return b;
 }
@@ -37,12 +38,11 @@ constexpr inline bool table_needs_escaping(std::string_view view) {
   return needs;
 }
 
-#if defined(__SSE2__) || defined(__x86_64__) || defined(__x86_64) || \
-    (defined(_M_AMD64) || defined(_M_X64) ||                         \
+#if defined(__SSE2__) || defined(__x86_64__) || defined(__x86_64) ||           \
+    (defined(_M_AMD64) || defined(_M_X64) ||                                   \
      (defined(_M_IX86_FP) && _M_IX86_FP == 2))
 #define HAS_SSE2 1
 #endif
-
 
 #if defined(__aarch64__) || defined(_M_ARM64)
 #define HAS_NEON 1
@@ -53,21 +53,21 @@ constexpr inline bool table_needs_escaping(std::string_view view) {
 
 inline bool simd_needs_escaping(std::string_view view) {
   if (view.size() < 16) {
-    return branchless_needs_escaping(view);
+    return simple_needs_escaping(view);
   }
   size_t i = 0;
-  static uint8_t rnt_array[16] = {1, 0, 34,  0, 0, 0,  0, 0,
-                                  0, 0, 0, 0, 92, 0, 0, 0};
+  static uint8_t rnt_array[16] = {1, 0, 34, 0, 0,  0, 0, 0,
+                                  0, 0, 0,  0, 92, 0, 0, 0};
   const uint8x16_t rnt = vld1q_u8(rnt_array);
   uint8x16_t running{0};
   for (; i + 15 < view.size(); i += 16) {
-    uint8x16_t word = vld1q_u8((const uint8_t*)view.data() + i);
+    uint8x16_t word = vld1q_u8((const uint8_t *)view.data() + i);
     running = vorrq_u8(running, vceqq_u8(vqtbl1q_u8(rnt, word), word));
     running = vorrq_u8(running, vcltq_u8(word, vdupq_n_u8(32)));
   }
   if (i < view.size()) {
     uint8x16_t word =
-        vld1q_u8((const uint8_t*)view.data() + view.length() - 16);
+        vld1q_u8((const uint8_t *)view.data() + view.length() - 16);
     running = vorrq_u8(running, vceqq_u8(vqtbl1q_u8(rnt, word), word));
     running = vorrq_u8(running, vcltq_u8(word, vdupq_n_u8(32)));
   }
@@ -76,4 +76,31 @@ inline bool simd_needs_escaping(std::string_view view) {
 
 #elif HAS_SSE2
 #include <emmintrin.h>
+
+inline bool simd_needs_escaping(std::string_view view) {
+  if (view.size() < 16) {
+    return simple_needs_escaping(view);
+  }
+  size_t i = 0;
+  __m128i running = _mm_setzero_si128();
+  for (; i + 15 < view.size(); i += 16) {
+    __m128i word = _mm_loadu_si128((const __m128i *)(view.data() + i));
+    running = _mm_or_si128(running, _mm_cmpeq_epi8(word, _mm_set1_epi8(34)));
+    running = _mm_or_si128(running, _mm_cmpeq_epi8(word, _mm_set1_epi8(92)));
+    running = _mm_or_si128(
+        running, _mm_cmpeq_epi8(_mm_subs_epu8(word, _mm_set1_epi8(31)),
+                                _mm_setzero_si128()));
+  }
+  if (i < view.size()) {
+    __m128i word =
+        _mm_loadu_si128((const __m128i *)(view.data() + view.length() - 16));
+    running = _mm_or_si128(running, _mm_cmpeq_epi8(word, _mm_set1_epi8(34)));
+    running = _mm_or_si128(running, _mm_cmpeq_epi8(word, _mm_set1_epi8(92)));
+    running = _mm_or_si128(
+        running, _mm_cmpeq_epi8(_mm_subs_epu8(word, _mm_set1_epi8(31)),
+                                _mm_setzero_si128()));
+  }
+  return _mm_movemask_epi8(running) != 0;
+}
+
 #endif
