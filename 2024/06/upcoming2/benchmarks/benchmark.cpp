11e0aafcdb5e246696e6bcf84e35c7d027a4b03d
Daniel Lemire <daniel@lemire.me>
2024-06-03 09:45:54 -0400

    Updating

---
+++ 2024/06/upcoming2/benchmarks/benchmark.cpp
@@ -0,0 +1,58 @@
+
+#include "performancecounters/benchmarker.h"
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <random>
+#include <string>
+
+#include "matrix_multiply.h"
+
+void pretty_print(size_t, size_t doubles, std::string name,
+                  event_aggregate agg) {
+  printf("%-40s : ", name.c_str());
+  printf(" %5.2f Gvalue/s ", doubles / agg.fastest_elapsed_ns());
+  printf(" %5.2f ns/value ", agg.fastest_elapsed_ns() / doubles);
+
+  if (collector.has_events()) {
+    printf(" %5.2f GHz ", agg.fastest_cycles() / agg.fastest_elapsed_ns());
+    printf(" %5.2f c/value ", agg.fastest_cycles() / doubles);
+    printf(" %5.2f i/value ", agg.fastest_instructions() / doubles);
+    printf(" %5.2f i/c ", agg.fastest_instructions() / agg.fastest_cycles());
+  }
+  printf("\n");
+}
+
+int main(int argc, char **argv) {
+  size_t size = 100;
+  size_t volume = size * size * size;
+  Matrix<double> a(size, size);
+  Matrix<double> b(size, size);
+  Matrix<double> c = allocate_multiply(a, b);
+  pretty_print(size, volume, "multiply_ikj",
+               bench([&]() { multiply_ikj(a, b, c); }));
+  pretty_print(size, volume, "multiply_ijk",
+               bench([&]() { multiply_ijk(a, b, c); }));
+  pretty_print(size, volume, "multiply_kij",
+               bench([&]() { multiply_kij(a, b, c); }));
+  pretty_print(size, volume, "multiply_kji",
+               bench([&]() { multiply_kji(a, b, c); }));
+  pretty_print(size, volume, "multiply_jki",
+               bench([&]() { multiply_jki(a, b, c); }));
+  pretty_print(size, volume, "multiply_jik",
+               bench([&]() { multiply_jik(a, b, c); }));
+
+  pretty_print(size, volume, "multiply_ikj_safe",
+               bench([&]() { multiply_ikj_safe(a, b, c); }));
+  pretty_print(size, volume, "multiply_ijk_safe",
+               bench([&]() { multiply_ijk_safe(a, b, c); }));
+  pretty_print(size, volume, "multiply_kij_safe",
+               bench([&]() { multiply_kij_safe(a, b, c); }));
+  pretty_print(size, volume, "multiply_kji_safe",
+               bench([&]() { multiply_kji_safe(a, b, c); }));
+  pretty_print(size, volume, "multiply_jki_safe",
+               bench([&]() { multiply_jki_safe(a, b, c); }));
+  pretty_print(size, volume, "multiply_jik_safe",
+               bench([&]() { multiply_jik_safe(a, b, c); }));
+  return EXIT_SUCCESS;
+}
---
+++ 2024/06/upcoming2/src/matrix_multiply.h
@@ -0,0 +1,163 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+
+template <typename T> struct Matrix {
+  Matrix(size_t rows, size_t cols)
+      : data(new T[rows * cols]), rows(rows), cols(cols) {}
+  std::unique_ptr<T[]> data;
+  size_t rows;
+  size_t cols;
+  T &operator()(size_t i, size_t j) { return data.get()[i * cols + j]; }
+  const T &operator()(size_t i, size_t j) const {
+    return data.get()[i * cols + j];
+  }
+  T &at(size_t i, size_t j) {
+    if (i >= rows || j >= cols)
+      throw std::out_of_range("Matrix index out of range");
+    return data.get()[i * cols + j];
+  }
+  const T &at(size_t i, size_t j) const {
+    if (i >= rows || j >= cols)
+      throw std::out_of_range("Matrix index out of range");
+    return data.get()[i * cols + j];
+  }
+};
+
+template <typename T>
+Matrix<T> allocate_multiply(const Matrix<T> &a, const Matrix<T> &b) {
+  return Matrix<T>(a.rows, b.cols);
+}
+
+template <typename T>
+void multiply_ikj(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t i = 0; i < a.rows; i++) {
+    for (size_t k = 0; k < a.cols; k++) {
+      for (size_t j = 0; j < b.cols; j++) {
+        c(i, j) += a(i, k) * b(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_ijk(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t i = 0; i < a.rows; i++) {
+    for (size_t k = 0; k < a.cols; k++) {
+      for (size_t j = 0; j < b.cols; j++) {
+        c(i, j) += a(i, k) * b(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_kij(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t k = 0; k < a.cols; k++) {
+    for (size_t i = 0; i < a.rows; i++) {
+      for (size_t j = 0; j < b.cols; j++) {
+        c(i, j) += a(i, k) * b(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_kji(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t k = 0; k < a.cols; k++) {
+    for (size_t j = 0; j < b.cols; j++) {
+      for (size_t i = 0; i < a.rows; i++) {
+        c(i, j) += a(i, k) * b(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_jki(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t j = 0; j < b.cols; j++) {
+    for (size_t k = 0; k < a.cols; k++) {
+      for (size_t i = 0; i < a.rows; i++) {
+        c(i, j) += a(i, k) * b(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_jik(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t j = 0; j < b.cols; j++) {
+    for (size_t i = 0; i < a.rows; i++) {
+      for (size_t k = 0; k < a.cols; k++) {
+        c(i, j) += a(i, k) * b(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_ikj_safe(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t i = 0; i < a.rows; i++) {
+    for (size_t k = 0; k < a.cols; k++) {
+      for (size_t j = 0; j < b.cols; j++) {
+        c.at(i, j) += a.at(i, k) * b.at(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_ijk_safe(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t i = 0; i < a.rows; i++) {
+    for (size_t k = 0; k < a.cols; k++) {
+      for (size_t j = 0; j < b.cols; j++) {
+        c.at(i, j) += a.at(i, k) * b.at(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_kij_safe(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t k = 0; k < a.cols; k++) {
+    for (size_t i = 0; i < a.rows; i++) {
+      for (size_t j = 0; j < b.cols; j++) {
+        c.at(i, j) += a.at(i, k) * b.at(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_kji_safe(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t k = 0; k < a.cols; k++) {
+    for (size_t j = 0; j < b.cols; j++) {
+      for (size_t i = 0; i < a.rows; i++) {
+        c.at(i, j) += a.at(i, k) * b.at(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_jki_safe(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t j = 0; j < b.cols; j++) {
+    for (size_t k = 0; k < a.cols; k++) {
+      for (size_t i = 0; i < a.rows; i++) {
+        c.at(i, j) += a.at(i, k) * b.at(k, j);
+      }
+    }
+  }
+}
+
+template <typename T>
+void multiply_jik_safe(const Matrix<T> &a, const Matrix<T> &b, Matrix<T> &c) {
+  for (size_t j = 0; j < b.cols; j++) {
+    for (size_t i = 0; i < a.rows; i++) {
+      for (size_t k = 0; k < a.cols; k++) {
+        c.at(i, j) += a.at(i, k) * b.at(k, j);
+      }
+    }
+  }
+}
