9ed8c5753422ac8c998dd94c54517f1aca79798f
Daniel Lemire <dlemire@lemire.me>
2024-06-27 15:17:03 -0400

    saving

--- extra/nodejs/base64/ciboire.mjs
+++ extra/nodejs/base64/ciboire.mjs
@@ -1,8 +1,13 @@
 import { bench, run } from "mitata";
+const size = 1024 * 1024 * 8;
+
+let base64Input = Buffer.alloc(size, "latin1").toString("base64");
+let base64From = Buffer.from (base64Input, "base64");
+console.log(base64Input);
+Buffer.from(base64Input, "base64");
+base64From.toString("base64");
 function makeBenchmark(size, isToString) {
-  const base64Input = Buffer.alloc(size, "latin1").toString("base64");
-  const base64From = Buffer.from (base64Input, "base64");
-  if (!isToString)
+ if (!isToString)
     bench(`Buffer. from(${size} bytes, 'base64')`, () => {
       Buffer.from(base64Input, "base64");
     });
