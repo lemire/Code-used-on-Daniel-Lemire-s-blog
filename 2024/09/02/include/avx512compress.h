5ae9635e2b093b14ab31988d999d3953de8aae6f
Daniel Lemire <dlemire@lemire.me>
2024-09-02 18:11:02 -0400

    saving

--- 2024/09/02/include/avx512compress.h
+++ 2024/09/02/include/avx512compress.h
@@ -15,10 +15,10 @@ void to_float16(uint16_t *dst, const double *src, size_t length) {
         __m512d src_vec = _mm512_loadu_pd(&src[i]);
 
         // Convert to 16-bit floats with rounding
-        __m128h dst_vec = _mm512_cvt_roundpd_ph(src_vec, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
+        __m128bh dst_vec = _mm256_cvtneps_pbh(_mm512_cvt_roundpd_ps(src_vec, _MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
 
         // Store the result
-        _mm_storeu_si128((__m128i*)&dst[i], _mm_castph_si128(dst_vec));
+        _mm_storeu_si128((__m128i*)&dst[i], *(__m128i*)&dst_vec);
     }
 
     // Handle remaining elements
@@ -30,10 +30,10 @@ void to_float16(uint16_t *dst, const double *src, size_t length) {
         __m512d src_vec = _mm512_maskz_loadu_pd(mask, &src[i]);
 
         // Convert to 16-bit floats with rounding
-        __m128h dst_vec = _mm512_cvt_roundpd_ph(src_vec, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
+        __m128bh dst_vec = _mm256_cvtneps_pbh(_mm512_cvt_roundpd_ps(src_vec, _MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
 
         // Store the result with masking
-        _mm_mask_storeu_epi16(&dst[i], mask, _mm_castph_si128(dst_vec));
+        _mm_mask_storeu_epi16(&dst[i], mask, *(__m128i*)&dst_vec);
     }
 }
 
@@ -48,7 +48,7 @@ void from_float16(double *dst, const uint16_t *src, size_t length) {
         __m128i src_vec = _mm_loadu_si128((__m128i*)&src[i]);
 
         // Convert to double-precision floats
-        __m512d dst_vec = _mm512_cvtph_pd(_mm_castsi128_ph(src_vec));
+        __m512d dst_vec = _mm512_cvtps_pd(_mm256_cvtpbh_ps(*(__m128bh*)&src_vec));
 
         // Store the result
         _mm512_storeu_pd(&dst[i], dst_vec);
@@ -63,7 +63,7 @@ void from_float16(double *dst, const uint16_t *src, size_t length) {
         __m128i src_vec = _mm_maskz_loadu_epi16(mask, &src[i]);
 
         // Convert to double-precision floats
-        __m512d dst_vec = _mm512_cvtph_pd(_mm_castsi128_ph (src_vec));
+        __m512d dst_vec = _mm512_cvtps_pd(_mm256_cvtpbh_ps( *(__m128bh*)&src_vec));
 
         // Store the result with masking
         _mm512_mask_storeu_pd(&dst[i], mask, dst_vec);
