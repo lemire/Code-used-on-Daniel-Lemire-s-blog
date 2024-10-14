b5e78feda8cb6ceb197936d2f32f609703fdf642
Daniel Lemire <daniel@lemire.me>
2024-10-14 18:15:23 -0400

    saving

--- 2024/10/14/src/main/java/me/lemire/MyBenchmark.java
+++ 2024/10/14/src/main/java/me/lemire/MyBenchmark.java
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package me.lemire;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -48,28 +17,28 @@ import java.util.concurrent.ThreadLocalRandom;
 @Warmup(iterations = 5, time = 1)
 @Fork(value = 2)
 public class MyBenchmark {
-	private static final byte[] silly_table1;
+    private static final byte[] silly_table1;
 
-	static {
-		silly_table1 = new byte[256];
-		silly_table1['\\'] = '\\';
-	}
-	private static final byte[] silly_table2;
+    static {
+        silly_table1 = new byte[256];
+        silly_table1['\\'] = '\\';
+    }
+    private static final byte[] silly_table2;
 
-	static {
-		silly_table2 = new byte[256];
-		silly_table2['\\'] = '\\';
-		silly_table2['\n'] = 'n';
-	}
+    static {
+        silly_table2 = new byte[256];
+        silly_table2['\\'] = '\\';
+        silly_table2['\n'] = 'n';
+    }
 
-	private static final byte[] silly_table3;
+    private static final byte[] silly_table3;
 
-	static {
-		silly_table3 = new byte[256];
-		silly_table3['\\'] = '\\';
-		silly_table3['\n'] = 'n';
-		silly_table3['\t'] = 't';
-	}
+    static {
+        silly_table3 = new byte[256];
+        silly_table3['\\'] = '\\';
+        silly_table3['\n'] = 'n';
+        silly_table3['\t'] = 't';
+    }
 
     public static int replaceBackslash1(char[] original, char[] newArray) {
         int index = 0;
@@ -81,21 +50,21 @@ public class MyBenchmark {
                 newArray[index++] = c;
             }
         }
-		return index;
+        return index;
     }
 
     public static int replaceBackslashTable1(char[] original, char[] newArray) {
         int index = 0;
         for (char c : original) {
             byte b = silly_table1[c%256];
-			if (c < 256 &&  b != 0) {
-				newArray[index++] = '\\';
-				newArray[index++] = (char)b;
-			} else {
-				newArray[index++] = c;
-			}
+            if (c < 256 &&  b != 0) {
+                newArray[index++] = '\\';
+                newArray[index++] = (char)b;
+            } else {
+                newArray[index++] = c;
+            }
         }
-		return index;
+        return index;
     }
 
 
@@ -112,21 +81,21 @@ public class MyBenchmark {
                 newArray[index++] = c;
             }
         }
-		return index;
+        return index;
     }
 
     public static int replaceBackslashTable2(char[] original, char[] newArray) {
         int index = 0;
         for (char c : original) {
             byte b = silly_table2[c%256];
-			if (c < 256 &&  b > 0) {
-				newArray[index++] = '\\';
-				newArray[index++] = (char)b;
-			} else {
-				newArray[index++] = c;
-			}
+            if (c < 256 &&  b > 0) {
+                newArray[index++] = '\\';
+                newArray[index++] = (char)b;
+            } else {
+                newArray[index++] = c;
+            }
         }
-		return index;
+        return index;
     }
 
     public static int replaceBackslash3(char[] original, char[] newArray) {
@@ -145,77 +114,77 @@ public class MyBenchmark {
                 newArray[index++] = c;
             }
         }
-		return index;
+        return index;
     }
 
     public static int replaceBackslashTable3(char[] original, char[] newArray) {
         int index = 0;
         for (char c : original) {
             byte b = silly_table2[c%256];
-			if (c < 256 && b > 0) {
-				newArray[index++] = '\\';
-				newArray[index++] = (char)b;
-			} else {
-				newArray[index++] = c;
-			}
+            if (c < 256 && b > 0) {
+                newArray[index++] = '\\';
+                newArray[index++] = (char)b;
+            } else {
+                newArray[index++] = c;
+            }
         }
-		return index;
+        return index;
     }
 
-	@State(Scope.Benchmark)
-	public static class BenchmarkState {
-		@Param({"65536"})
-		public int size;
-		public char[] inputstring;
-		public char[] outputstring;
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        @Param({"65536"})
+        public int size;
+        public char[] inputstring;
+        public char[] outputstring;
 
 
-		@Setup(Level.Trial)
-		public void setUp() {
-			ThreadLocalRandom random = ThreadLocalRandom.current();
-			inputstring = new char[size];
-			int count = size;
-			for(int k = 0; k < size; k++) {
-				inputstring[k] = (char)random.nextInt(0, 256);
-				if(silly_table3[inputstring[k]] > 0) {
-					count++;
-				}
-			}
-			outputstring = new char[count];
-		}
+        @Setup(Level.Trial)
+        public void setUp() {
+            ThreadLocalRandom random = ThreadLocalRandom.current();
+            inputstring = new char[size];
+            int count = size;
+            for(int k = 0; k < size; k++) {
+                inputstring[k] = (char)random.nextInt(0, 256);
+                if(silly_table3[inputstring[k]] > 0) {
+                    count++;
+                }
+            }
+            outputstring = new char[count];
+        }
 
-		@Setup(Level.Invocation)
-		public void perInvocation(){
-		}
-	}
+        @Setup(Level.Invocation)
+        public void perInvocation(){
+        }
+    }
 
-	@Benchmark
-	public void benchReplaceBackslash1(Blackhole blackhole, BenchmarkState state) {
-		blackhole.consume(replaceBackslash1(state.inputstring, state.outputstring));
-	}
+    @Benchmark
+    public void benchReplaceBackslash1(Blackhole blackhole, BenchmarkState state) {
+        blackhole.consume(replaceBackslash1(state.inputstring, state.outputstring));
+    }
 
-	@Benchmark
-	public void benchReplaceBackslashTable1(Blackhole blackhole, BenchmarkState state) {
-		blackhole.consume(replaceBackslashTable1(state.inputstring, state.outputstring));
-	}
-	@Benchmark
-	public void benchReplaceBackslash2(Blackhole blackhole, BenchmarkState state) {
-		blackhole.consume(replaceBackslash2(state.inputstring, state.outputstring));
-	}
+    @Benchmark
+    public void benchReplaceBackslashTable1(Blackhole blackhole, BenchmarkState state) {
+        blackhole.consume(replaceBackslashTable1(state.inputstring, state.outputstring));
+    }
+    @Benchmark
+    public void benchReplaceBackslash2(Blackhole blackhole, BenchmarkState state) {
+        blackhole.consume(replaceBackslash2(state.inputstring, state.outputstring));
+    }
 
-	@Benchmark
-	public void benchReplaceBackslashTable2(Blackhole blackhole, BenchmarkState state) {
-		blackhole.consume(replaceBackslashTable2(state.inputstring, state.outputstring));
-	}
+    @Benchmark
+    public void benchReplaceBackslashTable2(Blackhole blackhole, BenchmarkState state) {
+        blackhole.consume(replaceBackslashTable2(state.inputstring, state.outputstring));
+    }
 
-	@Benchmark
-	public void benchReplaceBackslash3(Blackhole blackhole, BenchmarkState state) {
-		blackhole.consume(replaceBackslash3(state.inputstring, state.outputstring));
-	}
+    @Benchmark
+    public void benchReplaceBackslash3(Blackhole blackhole, BenchmarkState state) {
+        blackhole.consume(replaceBackslash3(state.inputstring, state.outputstring));
+    }
 
-	@Benchmark
-	public void benchReplaceBackslashTable3(Blackhole blackhole, BenchmarkState state) {
-		blackhole.consume(replaceBackslashTable3(state.inputstring, state.outputstring));
-	}
+    @Benchmark
+    public void benchReplaceBackslashTable3(Blackhole blackhole, BenchmarkState state) {
+        blackhole.consume(replaceBackslashTable3(state.inputstring, state.outputstring));
+    }
 
 }
