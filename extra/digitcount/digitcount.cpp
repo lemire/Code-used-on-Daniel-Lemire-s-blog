c62c170b7534435663b03ce902b4964f114372ee
Daniel Lemire <daniel@lemire.me>
2025-01-07 15:11:33 -0500

    saving.

--- extra/digitcount/digitcount.cpp
+++ extra/digitcount/digitcount.cpp
@@ -43,22 +43,106 @@ int digit_count(uint64_t x) {
   return y + 1;
 }
 
+
+
+int faster_digit_count(uint64_t x) {
+static uint64_t table[64][2] = {
+    { 0x01, 0xfffffffffffffff6ULL },
+    { 0x01, 0xfffffffffffffff6ULL },
+    { 0x01, 0xfffffffffffffff6ULL },
+    { 0x01, 0xfffffffffffffff6ULL },
+    { 0x02, 0xffffffffffffff9cULL },
+    { 0x02, 0xffffffffffffff9cULL },
+    { 0x02, 0xffffffffffffff9cULL },
+    { 0x03, 0xfffffffffffffc18ULL },
+    { 0x03, 0xfffffffffffffc18ULL },
+    { 0x03, 0xfffffffffffffc18ULL },
+    { 0x04, 0xffffffffffffd8f0ULL },
+    { 0x04, 0xffffffffffffd8f0ULL },
+    { 0x04, 0xffffffffffffd8f0ULL },
+    { 0x04, 0xffffffffffffd8f0ULL },
+    { 0x05, 0xfffffffffffe7960ULL },
+    { 0x05, 0xfffffffffffe7960ULL },
+    { 0x05, 0xfffffffffffe7960ULL },
+    { 0x06, 0xfffffffffff0bdc0ULL },
+    { 0x06, 0xfffffffffff0bdc0ULL },
+    { 0x06, 0xfffffffffff0bdc0ULL },
+    { 0x07, 0xffffffffff676980ULL },
+    { 0x07, 0xffffffffff676980ULL },
+    { 0x07, 0xffffffffff676980ULL },
+    { 0x07, 0xffffffffff676980ULL },
+    { 0x08, 0xfffffffffa0a1f00ULL },
+    { 0x08, 0xfffffffffa0a1f00ULL },
+    { 0x08, 0xfffffffffa0a1f00ULL },
+    { 0x09, 0xffffffffc4653600ULL },
+    { 0x09, 0xffffffffc4653600ULL },
+    { 0x09, 0xffffffffc4653600ULL },
+    { 0x0a, 0xfffffffdabf41c00ULL },
+    { 0x0a, 0xfffffffdabf41c00ULL },
+    { 0x0a, 0xfffffffdabf41c00ULL },
+    { 0x0a, 0xfffffffdabf41c00ULL },
+    { 0x0b, 0xffffffe8b7891800ULL },
+    { 0x0b, 0xffffffe8b7891800ULL },
+    { 0x0b, 0xffffffe8b7891800ULL },
+    { 0x0c, 0xffffff172b5af000ULL },
+    { 0x0c, 0xffffff172b5af000ULL },
+    { 0x0c, 0xffffff172b5af000ULL },
+    { 0x0d, 0xfffff6e7b18d6000ULL },
+    { 0x0d, 0xfffff6e7b18d6000ULL },
+    { 0x0d, 0xfffff6e7b18d6000ULL },
+    { 0x0d, 0xfffff6e7b18d6000ULL },
+    { 0x0e, 0xffffa50cef85c000ULL },
+    { 0x0e, 0xffffa50cef85c000ULL },
+    { 0x0e, 0xffffa50cef85c000ULL },
+    { 0x0f, 0xfffc72815b398000ULL },
+    { 0x0f, 0xfffc72815b398000ULL },
+    { 0x0f, 0xfffc72815b398000ULL },
+    { 0x10, 0xffdc790d903f0000ULL },
+    { 0x10, 0xffdc790d903f0000ULL },
+    { 0x10, 0xffdc790d903f0000ULL },
+    { 0x10, 0xffdc790d903f0000ULL },
+    { 0x11, 0xfe9cba87a2760000ULL },
+    { 0x11, 0xfe9cba87a2760000ULL },
+    { 0x11, 0xfe9cba87a2760000ULL },
+    { 0x12, 0xf21f494c589c0000ULL },
+    { 0x12, 0xf21f494c589c0000ULL },
+    { 0x12, 0xf21f494c589c0000ULL },
+    { 0x13, 0x7538dcfb76180000ULL },
+    { 0x13, 0x7538dcfb76180000ULL },
+    { 0x13, 0x7538dcfb76180000ULL },
+    { 0x13, 0x7538dcfb76180000ULL },
+};
+  int log = int_log2(x);
+  uint64_t low = table[log][1];
+  uint64_t high = table[log][0];
+  return (x+low < low) + high;
+}
+
 int main() {
   size_t counter = 0;
   printf("%" PRIu64 " \n", 10000000000000000000ULL);
   for (uint64_t i = 1;; i *= 10) {
     ++counter;
     printf("%" PRIu64 " %d == %zu\n", i - 1, digit_count(i - 1), counter - 1);
-
     printf("%" PRIu64 " %d == %zu\n", i, digit_count(i), counter);
     printf("%" PRIu64 " %d == %zu\n", i + 1, digit_count(i + 1), counter);
-
+    if(digit_count(i) != faster_digit_count(i)){
+      printf("error %llu %d %d\n", i, digit_count(i), faster_digit_count(i));
+      return -1;
+    }
+    if(digit_count(i+1) != faster_digit_count(i+1)){
+      printf("error %llu %d %d\n", i+1, digit_count(i+1), faster_digit_count(i+1));
+      return -1;
+    }
+    if(digit_count(i-1) != faster_digit_count(i-1)){
+      printf("error %llu %d %d\n", i-1, digit_count(i-1), faster_digit_count(i-1));
+      return -1;
+    }
     if (i * 10 < i) {
       break;
     }
   }
-  printf("%" PRIu64 " %d\n", 0xffffffffffffffff,
-         digit_count(0xffffffffffffffff));
+  printf("success\n");
 
   return 0;
 }
---
+++ extra/digitcount/script.py
@@ -0,0 +1,52 @@
+import math, sys
+def log2(n: int) -> int:
+  return int(math.log2(n | 1))
+
+def log10(n: int) -> int:
+  return int(math.log10(n | 1))
+
+def f32(i):
+    d = log10(2**i)
+    # max(0, _) to avoid overflow
+    return ((d + 1) << 32) + max(0, ((1 << 32) - 10**(d + 1)))
+
+print([hex(f32(j)) for j in range(0, 32)])
+
+
+def digit_count32(n: int) -> int:
+  assert 0 <= n < 2**32, n
+  return (n + f32(log2(n))) >> 32
+number = 1
+while (number < 2**32):
+    assert(digit_count32(number) == len(str(number)))
+    number *= 10
+
+def f64(i):
+    d = log10(2**i)
+    # max(0, _) to avoid overflow
+    return ((d + 1) << 64) + max(0, ((1 << 64) - 10**(d + 1)))
+
+
+def count_digit64(x):
+    return (x + f64(log2(x))) // 2**64
+number = 1
+
+
+while (number < 2**64):
+    print("===")
+    print(count_digit64(number), len(str(number)), number)
+    assert(len(str(number)) == count_digit64(number))
+    print(count_digit64(number-1), len(str(number-1)), number-1)
+    assert(len(str(number-1)) == count_digit64(number-1))
+    print(count_digit64(number+1), len(str(number+1)), number+1)
+    assert(len(str(number+1)) == count_digit64(number+1))
+    number *= 10
+
+
+print([[hex(f64(j)//2**64), hex(f64(j)%2**64)] for j in range(0, 64)])
+
+table = [[f64(j) // 2**64, f64(j) % 2**64] for j in range(0, 64)]
+print("uint64_t table[64][2] = {")
+for pair in table:
+    print(f"    {{ 0x{hex(pair[0])[2:]:0>2}, 0x{hex(pair[1])[2:]:0>16}ULL }},")
+print("};")
