8b473d8e845983091b83339f1c5098a14a6ab9fd
Daniel Lemire <daniel@lemire.me>
2025-04-06 15:34:27 -0400

    various fixes

--- 2025/04/06/shuffle_test.go
+++ 2025/04/06/shuffle_test.go
@@ -3,80 +3,9 @@ package main
 // Nevin Brackett-Rozinsky, Daniel Lemire, Batched Ranged Random Integer Generation, Software: Practice and Experience 55 (1), 2024.
 // Daniel Lemire, Fast Random Integer Generation in an Interval, ACM Transactions on Modeling and Computer Simulation, 29 (1), 2019.
 import (
-	"math/bits"
-	"math/rand/v2"
+	"math/rand"
 	"testing"
 )
-
-func shuffleBatch2(storage []uint64) {
-	i := uint64(len(storage))
-	for ; i > 1<<30; i-- {
-		partialShuffle64b(storage, i, 1, i)
-	}
-	bound := uint64(1) << 60
-	for ; i > 1; i -= 2 {
-		bound = partialShuffle64b(storage, i, 2, bound)
-	}
-}
-
-func shuffleBatch23456(storage []uint64) {
-	i := uint64(len(storage))
-	for ; i > 1<<30; i-- {
-		partialShuffle64b(storage, i, 1, i)
-	}
-	bound := uint64(1) << 60
-	for ; i > 1<<19; i -= 2 {
-		bound = partialShuffle64b(storage, i, 2, bound)
-	}
-	bound = uint64(1) << 57
-	for ; i > 1<<14; i -= 3 {
-		bound = partialShuffle64b(storage, i, 3, bound)
-	}
-	bound = uint64(1) << 56
-	for ; i > 1<<11; i -= 4 {
-		bound = partialShuffle64b(storage, i, 4, bound)
-	}
-	bound = uint64(1) << 55
-	for ; i > 1<<9; i -= 5 {
-		bound = partialShuffle64b(storage, i, 5, bound)
-	}
-	bound = uint64(1) << 54
-	for ; i > 6; i -= 6 {
-		bound = partialShuffle64b(storage, i, 6, bound)
-	}
-	if i > 1 {
-		partialShuffle64b(storage, i, i-1, 720)
-	}
-}
-
-func partialShuffle64b(result []uint64, n, k, bound uint64) uint64 {
-	r := rand.Uint64()
-
-	for i := uint64(0); i < k; i++ {
-		hi, lo := bits.Mul64(n-i, r)
-		r = lo
-		result[i] = hi
-	}
-
-	if r < bound {
-		bound = n
-		for i := uint64(1); i < k; i++ {
-			bound *= (n - i)
-		}
-		t := (-bound) % bound
-
-		for r < t {
-			r = rand.Uint64()
-			for i := uint64(0); i < k; i++ {
-				hi, lo := bits.Mul64(n-i, r)
-				r = lo
-				result[i] = hi
-			}
-		}
-	}
-
-	return bound
-}
 
 func BenchmarkShuffleStandard10K(b *testing.B) {
 	size := 10000
@@ -117,7 +46,4 @@ func BenchmarkShuffleBatch23456_10K(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		shuffleBatch23456(data)
 	}
-}
-
-func main() {
 }
