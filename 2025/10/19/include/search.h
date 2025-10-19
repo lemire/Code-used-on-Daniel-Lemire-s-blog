b5b6c02069c26bd2eadd15309292460f6b5debb8
Daniel Lemire <daniel@lemire.me>
2025-10-19 19:47:00 -0400

    saving

--- 2025/10/19/include/search.h
+++ 2025/10/19/include/search.h
@@ -13,11 +13,11 @@ concept map_like = std::ranges::range<T> && requires {
           std::pair<const typename T::key_type, typename T::mapped_type>>
    && std::convertible_to<typename T::mapped_type, size_t>;
 
-size_t at_index(map_like auto const& index_map, size_t idx) {
+auto at_index(map_like auto& index_map, size_t idx) {
   size_t count = 0;
-  for (const auto &pair : index_map) {
+  for (const auto &[_, value] : index_map) {
     if(count == idx)
-      return static_cast<size_t>(pair.second);
+      return value;
     count++;
   }
   throw std::out_of_range("Index out of range");
@@ -56,30 +56,33 @@ size_t at_index_cache(map_like auto const& index_map, size_t idx) {
 }
 
 
-size_t at_index_thread_local_cache(map_like auto const& index_map, size_t idx) {
-  using Iterator = decltype(index_map.begin());
+auto at_index_thread_local_cache(map_like auto& index_map, 
+    size_t idx) {
+  using iterator = decltype(index_map.begin());
   struct Cache {
-    Iterator last_iterator;
-    size_t last_index = static_cast<size_t>(-1);
+    iterator last_iterator;
+    size_t last_index = -1;
   };
   thread_local Cache cache;
-  if (idx == cache.last_index + 1 && cache.last_iterator != index_map.end()) {
-    ++cache.last_iterator;
+  if (idx == cache.last_index + 1 
+      && cache.last_iterator != index_map.end()) {
+    cache.last_iterator++;
     cache.last_index = idx;
     if (cache.last_iterator != index_map.end()) {
-      return static_cast<size_t>(cache.last_iterator->second);
+      return cache.last_iterator->second;
     } else {
       throw std::out_of_range("Index out of range");
     }
   } else {
     cache.last_iterator = index_map.begin();
-    cache.last_index = static_cast<size_t>(-1);
+    cache.last_index = -1;
     size_t count = 0;
-    for (auto it = index_map.begin(); it != index_map.end(); ++it) {
+    for (auto it = index_map.begin(); 
+        it != index_map.end(); ++it) {
       if (count == idx) {
         cache.last_iterator = it;
         cache.last_index = idx;
-        return static_cast<size_t>(it->second);
+        return it->second;
       }
       count++;
     }
