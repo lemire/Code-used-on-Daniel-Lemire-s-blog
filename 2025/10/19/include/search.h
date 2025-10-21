4f40480826263f8f28a78ef0c59472b1b9495ed2
Daniel Lemire <daniel@lemire.me>
2025-10-20 23:10:52 -0400

    saving.

--- 2025/10/19/include/search.h
+++ 2025/10/19/include/search.h
@@ -9,7 +9,7 @@ concept map_like = std::ranges::range<T> && requires {
   typename T::key_type;
   typename T::mapped_type;
   typename T::value_type;
-} && std::same_as<typename T::value_type, 
+} && std::same_as<typename T::value_type,
           std::pair<const typename T::key_type, typename T::mapped_type>>
    && std::convertible_to<typename T::mapped_type, size_t>;
 
@@ -24,30 +24,33 @@ auto at_index(map_like auto& index_map, size_t idx) {
 }
 
 
-size_t at_index_cache(map_like auto const& index_map, size_t idx) {
+auto at_index_cache(map_like auto const& index_map, size_t idx) {
   using Iterator = decltype(index_map.begin());
   struct Cache {
     Iterator last_iterator;
     size_t last_index = static_cast<size_t>(-1);
+    decltype(&index_map) map_ptr = nullptr;
   };
   static Cache cache;
-  if (idx == cache.last_index + 1 && cache.last_iterator != index_map.end()) {
+  if (cache.map_ptr == &index_map && idx == cache.last_index + 1
+      && cache.last_iterator != index_map.end()) {
     ++cache.last_iterator;
     cache.last_index = idx;
     if (cache.last_iterator != index_map.end()) {
-      return static_cast<size_t>(cache.last_iterator->second);
+      return cache.last_iterator->second;
     } else {
       throw std::out_of_range("Index out of range");
     }
   } else {
     cache.last_iterator = index_map.begin();
     cache.last_index = static_cast<size_t>(-1);
+    cache.map_ptr = &index_map;
     size_t count = 0;
     for (auto it = index_map.begin(); it != index_map.end(); ++it) {
       if (count == idx) {
         cache.last_iterator = it;
         cache.last_index = idx;
-        return static_cast<size_t>(it->second);
+        return it->second;
       }
       count++;
     }
@@ -56,15 +59,17 @@ size_t at_index_cache(map_like auto const& index_map, size_t idx) {
 }
 
 
-auto at_index_thread_local_cache(map_like auto& index_map, 
+auto at_index_thread_local_cache(map_like auto& index_map,
     size_t idx) {
   using iterator = decltype(index_map.begin());
   struct Cache {
     iterator last_iterator;
     size_t last_index = -1;
+    decltype(&index_map) map_ptr = nullptr;
   };
   thread_local Cache cache;
-  if (idx == cache.last_index + 1 
+  if (cache.map_ptr == &index_map
+      && idx == cache.last_index + 1
       && cache.last_iterator != index_map.end()) {
     cache.last_iterator++;
     cache.last_index = idx;
@@ -76,8 +81,9 @@ auto at_index_thread_local_cache(map_like auto& index_map,
   } else {
     cache.last_iterator = index_map.begin();
     cache.last_index = -1;
+    cache.map_ptr = &index_map;
     size_t count = 0;
-    for (auto it = index_map.begin(); 
+    for (auto it = index_map.begin();
         it != index_map.end(); ++it) {
       if (count == idx) {
         cache.last_iterator = it;
